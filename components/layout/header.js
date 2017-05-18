c1d4a77b787f0712f5bd2e4ca8281ff009bb3faf
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-05-18 17:01:11 +0200

    fix header routes

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import { Link } from '../../routes';
 import React from 'react';
 
 export default class Header extends React.Component {
@@ -17,12 +17,12 @@ export default class Header extends React.Component {
             <Link prefetch href="/"><img src="/static/images/logo.png" /></Link>
           </div>
           <ul className="menu">
-            <li><Link prefetch href="/"><a href="/dataset">Datasets</a></Link></li>
-            <li><Link prefetch href="/"><a href="/partners">Partners</a></Link></li>
-            <li><Link prefetch href="/"><a href="/page">Pages</a></Link></li>
-            <li><Link prefetch href="/"><a href="/insights">Insights</a></Link></li>
-            <li><Link prefetch href="/"><a href="/vocabularies">Vocabularies</a></Link></li>
-            <li><Link prefetch href="/"><a href="/logout">Logout</a></Link></li>
+            <li><Link prefetch route="datasets"><a>Datasets</a></Link></li>
+            <li><Link prefetch route="partners"><a>Partners</a></Link></li>
+            <li><Link prefetch route="pages"><a>Pages</a></Link></li>
+            <li><Link prefetch route="insights"><a>Insights</a></Link></li>
+            <li><Link prefetch route="vocabularies"><a>Vocabularies</a></Link></li>
+            <li><Link prefetch route="logout"><a>Logout</a></Link></li>
           </ul>
         </div>
       </header>
--- routes.js
+++ routes.js
@@ -3,7 +3,22 @@
 const nextRoutes = require('next-routes');
 const routes = module.exports = nextRoutes();
 
-// -----DATASET ROUTES--------
+// ----- DATASET --------
 routes.add('datasets', '/admin/datasets', 'dataset');
 routes.add('edit_dataset', '/admin/datasets/:id/edit', 'dataset/edit');
 routes.add('new_dataset', '/admin/datasets/new', 'dataset/new');
+
+// ----- PARTNERS --------
+routes.add('partners', '/admin/datasets', 'dataset');
+
+// ----- PAGES --------
+routes.add('pages', '/admin/datasets', 'dataset');
+
+// ----- INSIGHTS --------
+routes.add('insights', '/admin/datasets', 'dataset');
+
+// ----- VOCABULARIES --------
+routes.add('vocabularies', '/admin/datasets', 'dataset');
+
+// ----- LOGOUT --------
+routes.add('logout', '/admin/datasets', 'dataset');
