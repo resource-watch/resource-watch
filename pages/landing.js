f4f07c69d9991d7b551069ae09c233e0637b5434
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-05-22 16:26:48 +0200

    Add landing page template. Background color for back office pages

---
+++ pages/landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Page from 'components/layout/page';
+
+export default class DatasetIndex extends React.Component {
+
+  render() {
+    return (
+      <Page
+        title="Landing"
+        description="Dataset description..."
+      >
+      Landing
+      </Page>
+    );
+  }
+}
