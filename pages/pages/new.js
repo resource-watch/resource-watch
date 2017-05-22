aa55662f1c9aefc4c2e8aa9f9491d6693f86c06b
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-05-22 17:00:34 +0200

    Add pages pages

--- pages/pages/new.js
+++ pages/pages/new.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import PartnerForm from 'components/partners/form/PartnerForm';
+import PageForm from 'components/pages/form/PageForm';
 import Title from 'components/ui/Title';
 import Page from 'components/layout/page';
 
-export default class PartnerNew extends React.Component {
+export default class PageNew extends React.Component {
 
   render() {
     return (
       <Page
-        title="New partner"
-        description="Edit partner description..."
+        title="New page"
+        description="Edit page description..."
       >
         <div className="row">
           <div className="column small-12">
             <Title className="-huge -p-primary">
               Create Partner
             </Title>
-            <PartnerForm
+            <PageForm
               application={['rw']}
               authorization={process.env.TEMP_TOKEN}
-              onSubmit={() => Router.pushRoute('partners')}
+              onSubmit={() => Router.pushRoute('pages')}
               mode="new"
             />
           </div>
