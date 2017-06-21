22a1f8fb59b6f5f767aa46b796d1516a5a5c478d
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-06-21 16:57:34 +0200

    making some progress on edit profile page;

--- pages/app/myrw/MyRWEditProfile.js
+++ pages/app/myrw/MyRWEditProfile.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import Button from 'components/ui/Button';
+import Checkbox from 'components/form/Checkbox';
 import Page from 'components/app/layout/Page';
+import UserService from 'services/UserService';
 
 
 class MyRWEditProfile extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null
+    };
+
+    this.userService = new UserService({ apiURL: process.env.CONTROL_TOWER_URL });
+  }
+
+  componentWillMount() {
+    this.userService.getLoggedUser().then((response) => {
+      console.log('get logged user: ', response);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
 
   triggerSaveProfile() {
 
   }
 
   render() {
+    const { user } = this.state;
+
     return (
       <Page
         title="My Resource Watch Edit Profile"
@@ -20,6 +42,24 @@ class MyRWEditProfile extends React.Component {
             <div className="column small-12">
               <div className="title-section">
                 <h2>Edit Profile</h2>
+                <Button
+                  properties={{
+                    type: 'button',
+                    className: '-primary -end'
+                  }}
+                  onClick={this.triggerSaveProfile}
+                >
+                  Save
+                </Button>
+              </div>
+              <div className="bottom-section">
+                <Checkbox
+                  properties={{
+                    name: 'delete_account',
+                    label: 'Delete account',
+                    checked: false
+                  }}
+                />
                 <Button
                   properties={{
                     type: 'button',
