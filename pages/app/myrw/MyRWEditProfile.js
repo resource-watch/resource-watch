5f354af82289230766074ef6ff9d73007529c9f0
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-06-21 16:25:21 +0200

    Starting implementation of Edit Profile page

--- pages/app/myrw/MyRWEditProfile.js
+++ pages/app/myrw/MyRWEditProfile.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { Link } from 'routes';
+import Button from 'components/ui/Button';
 import Page from 'components/app/layout/Page';
 
 
 class MyRWEditProfile extends React.Component {
 
-  render() {
+  triggerSaveProfile() {
 
+  }
+
+  render() {
     return (
       <Page
         title="My Resource Watch Edit Profile"
         description="My Resource Watch Edit Profile description"
       >
-        <div className="p-myrw-edit-profile">
-          <div className="c-page">
-
+        <div className="c-page p-myrw-edit-profile">
+          <div className="row">
+            <div className="column small-12">
+              <div className="title-section">
+                <h2>Edit Profile</h2>
+                <Button
+                  properties={{
+                    type: 'button',
+                    className: '-primary -end'
+                  }}
+                  onClick={this.triggerSaveProfile}
+                >
+                  Save
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </Page>
