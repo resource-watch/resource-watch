db69739cb3076dd0e83abc423c9426bebf0694a8
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-06-22 09:25:22 +0200

    Add UserService and finish wireframe of Edit Profile

--- pages/app/myrw/MyRWEditProfile.js
+++ pages/app/myrw/MyRWEditProfile.js
@@ -1,8 +1,32 @@
 import React from 'react';
+
 import Button from 'components/ui/Button';
 import Checkbox from 'components/form/Checkbox';
 import Page from 'components/app/layout/Page';
 import UserService from 'services/UserService';
+import Field from 'components/form/Field';
+import Input from 'components/form/Input';
+
+
+export const FORM_ELEMENTS = {
+  elements: {
+  },
+  validate() {
+    const elements = this.elements;
+    Object.keys(elements).forEach((k) => {
+      elements[k].validate();
+    });
+  },
+  isValid() {
+    const elements = this.elements;
+    const valid = Object.keys(elements)
+      .map(k => elements[k].isValid())
+      .filter(v => v !== null)
+      .every(element => element);
+
+    return valid;
+  }
+};
 
 
 class MyRWEditProfile extends React.Component {
@@ -11,7 +35,7 @@ class MyRWEditProfile extends React.Component {
     super(props);
 
     this.state = {
-      user: null
+      user: {}
     };
 
     this.userService = new UserService({ apiURL: process.env.CONTROL_TOWER_URL });
@@ -27,6 +51,10 @@ class MyRWEditProfile extends React.Component {
 
   triggerSaveProfile() {
 
+  }
+
+  handleFormChange(value) {
+    this.setState(Object.assign(this.state, value));
   }
 
   render() {
@@ -52,14 +80,67 @@ class MyRWEditProfile extends React.Component {
                   Save
                 </Button>
               </div>
-              <div className="bottom-section">
-                <Checkbox
+              <fieldset className="c-field-container">
+                <Field
+                  ref={(c) => { if (c) FORM_ELEMENTS.elements.name = c; }}
+                  onChange={value => this.handleFormChange({ name: value })}
+                  validations={['required']}
                   properties={{
-                    name: 'delete_account',
-                    label: 'Delete account',
-                    checked: false
+                    name: 'name',
+                    label: 'Name',
+                    type: 'text',
+                    required: true,
+                    default: user.name
                   }}
-                />
+                >
+                  {Input}
+                </Field>
+                <Field
+                  ref={(c) => { if (c) FORM_ELEMENTS.elements.email = c; }}
+                  onChange={value => this.handleFormChange({ email: value })}
+                  validations={['required']}
+                  properties={{
+                    name: 'email',
+                    label: 'Email',
+                    type: 'email',
+                    required: true,
+                    default: user.email
+                  }}
+                >
+                  {Input}
+                </Field>
+                <Field
+                  ref={(c) => { if (c) FORM_ELEMENTS.elements.new_password = c; }}
+                  onChange={value => this.handleFormChange({ new_password: value })}
+                  properties={{
+                    name: 'new_password',
+                    label: 'Change password',
+                    type: 'password',
+                    default: user.new_password
+                  }}
+                >
+                  {Input}
+                </Field>
+              </fieldset>
+              <h5>Photo</h5>
+              <div className="photo-container">
+                Add
+              </div>
+              <div className="bottom-section">
+                <div className="delete-account-checkbox">
+                  <Field
+                    ref={(c) => { if (c) FORM_ELEMENTS.elements.wri_funded = c; }}
+                    onChange={value => this.changeMetadata({ deleteAccount: value.checked })}
+                    properties={{
+                      name: 'delete_account',
+                      label: 'Delete account',
+                      checked: false,
+
+                    }}
+                  >
+                    {Checkbox}
+                  </Field>
+                </div>
                 <Button
                   properties={{
                     type: 'button',
