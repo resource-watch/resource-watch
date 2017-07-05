8ffe04e00c799abb7e032973be3356c4ea54093c
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-07-05 16:47:05 +0200

    Save widget modal

---
+++ components/modal/SaveWidgetModal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class SaveWidgetModal extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="save-widget-modal">
+        <div className="container">
+          <h1 className="c-text -header-normal -thin title">Save widget</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+SaveWidgetModal.propTypes = {
+};
+
+
+export default SaveWidgetModal;
