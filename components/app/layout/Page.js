55b43fcb48c197eeda733fd54cdc2ab6ffc8bc1a
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-07-06 12:17:15 +0200

    Add setUser to Page

--- components/app/layout/Page.js
+++ components/app/layout/Page.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import User from 'components/user';
+import PropTypes from 'prop-types';
 import isEmpty from 'lodash/isEmpty';
 
 // Redux
 import withRedux from 'next-redux-wrapper';
 import { initStore } from 'store';
+import { setUser } from 'redactions/user';
+
+// Components
+import User from 'components/user';
 
 class Page extends React.Component {
 
@@ -25,11 +29,19 @@ class Page extends React.Component {
         console.info(err);
       }
     }
+    // Save user in the store
+    this.props.setUser(this.props.user);
   }
 }
 
 Page.propTypes = {
-  user: React.PropTypes.object
+  user: PropTypes.object,
+  // Store
+  setUser: PropTypes.func.isRequired
 };
 
-export default withRedux(initStore, mapDispatchToProps)(Page);
+const mapDispatchToProps = dispatch => ({
+  setUser: (user) => { dispatch(setUser(user)); }
+});
+
+export default withRedux(initStore, null, mapDispatchToProps)(Page);
