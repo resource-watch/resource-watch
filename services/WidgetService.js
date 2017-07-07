2d9f3a2a7bf542fc9122fdf2ee305e01b9357ab4
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-07-07 12:42:33 +0200

    remove log code. Fix widget service token

--- services/WidgetService.js
+++ services/WidgetService.js
@@ -29,7 +29,7 @@ export default class WidgetService {
       body: JSON.stringify(bodyObj),
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
+        'Authorization': token
       }
     })
     .then(response => response.json())
