ad5167cc21c733e63b68094e754d10ebc2344f87
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-07-11 17:00:15 +0200

    first tries to fix it

--- utils/widgets/WidgetHelper.js
+++ utils/widgets/WidgetHelper.js
@@ -86,14 +86,14 @@ export function getDataURL(widgetEditor, tableName, dataset) {
     columns.push(sizeColumn);
   }
 
-  let orderByColumn = orderBy ? [orderBy] : [];
-  if (orderByColumn.length > 0 && value && category && aggregateFunction && orderByColumn[0].name === value.name) {
-    orderByColumn = [{ name: 'y' }];
-  } else if (orderByColumn.length > 0 && value && category && aggregateFunction && orderByColumn[0].name === category.name) {
-    orderByColumn = [{ name: 'x' }];
+  const orderByColumn = orderBy ? [orderBy] : [];
+  if (orderByColumn.length > 0 && orderByColumn[0].name === value.name && aggregateFunction && aggregateFunction !== 'none') {
+    orderByColumn[0].name = `${aggregateFunction}(${value.name})`;
   }
   const sortOrder = orderBy ? orderBy.orderType : 'asc';
   const query = `${getQueryByFilters(tableName, filters, columns, orderByColumn, sortOrder)} LIMIT ${limit}`;
+
+  console.log('query', query);
 
   // TODO: remove the limit
   return `${process.env.WRI_API_URL}/query/${dataset}?sql=${query}`;
