ed68f6eef0eafc5c38bb3b9d527f5c5e1c2ef02c
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-07-12 17:53:31 +0200

    hide size when chart type is not scatter or similar

--- components/widgets/DimensionsContainer.js
+++ components/widgets/DimensionsContainer.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+// Redux
+import withRedux from 'next-redux-wrapper';
+import { initStore } from 'store';
+
+// Components
 import CategoryContainer from 'components/widgets/CategoryContainer';
 import ValueContainer from 'components/widgets/ValueContainer';
 import ColorContainer from 'components/widgets/ColorContainer';
@@ -7,15 +14,28 @@ import SizeContainer from 'components/widgets/SizeContainer';
 class DimensionsContainer extends React.Component {
 
   render() {
+    const { chartType } = this.props.widgetEditor;
+  
     return (
       <div className="c-dimensions-container">
         <CategoryContainer />
         <ValueContainer />
         {false && <ColorContainer />  /* temporal while we have legends*/}
+        { chartType && chartType === 'scatter' &&
         <SizeContainer />
+        }
       </div>
     );
   }
 }
 
-export default DimensionsContainer;
+DimensionsContainer.propTypes = {
+  // Store
+  widgetEditor: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  widgetEditor: state.widgetEditor
+});
+
+export default withRedux(initStore, mapStateToProps, null)(DimensionsContainer);
