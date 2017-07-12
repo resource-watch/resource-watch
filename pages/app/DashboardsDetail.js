a3a1f76d680113589168e5ed729cde19a788c50f
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-07-12 13:34:55 +0100

    Create a DashboardDetail component w/ tweaked redirection for the country dashboard

---
+++ pages/app/DashboardsDetail.js
@@ -0,0 +1,435 @@
+import React from 'react';
+import classnames from 'classnames';
+import isEmpty from 'lodash/isEmpty';
+
+// Router
+import { Router } from 'routes';
+
+// Components
+import Page from 'components/app/layout/Page';
+import Layout from 'components/app/layout/Layout';
+import Title from 'components/ui/Title';
+import Breadcrumbs from 'components/ui/Breadcrumbs';
+import DashboardCard from 'components/app/dashboards/DashboardCard';
+
+// Temporarily hard-code the list of dashboards
+// Needs to be updated so the widgets can be shared between various
+// dashboards instead of copying them.
+/* eslint-disable */
+const DASHBOARDS = [
+  {
+    name: 'Water',
+    slug: 'water',
+    description: 'Topic description lorem ipsum casius tesebe Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odi em sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+    image: 'static/images/dashboards/dashboard-water.jpg',
+    widgets: [
+      {
+        widgetId: '6f01a91e-bb68-4d10-9da4-e48b553193f3',
+        categories: ['Water', 'Cities'],
+      },
+      {
+        name: 'Countries that will experience the greatest increase in projected water stress in the year 2040 if we continue business as usual',
+        categories: ['Water', 'Society'],
+        data: {
+          attributes: {
+            widgetConfig: {
+              "height":300,
+              "padding": {"top": 25,"left": 0,"bottom": 30,"right": 0},
+              "data": [
+                {
+                  "name": "table",
+                  "url": "https://wri-rw.carto.com/api/v2/sql?q=SELECT country, values, iso, row_number() over (order by values desc) as rank FROM aqueduct_water_stress_country_ranking_bau where type = 'all' and year = '2040' and values!=0 order by values desc",
+                  "format": {"type": "json","property": "rows"},
+                  "transform": [{"type": "sort","by": "rank"}]
+                },
+                {
+                  "name": "max",
+                  "source": "table",
+                  "transform": [{"type": "aggregate","summarize": {"rank": ["max"]}}]
+                },
+                {
+                  "name": "head",
+                  "source": "table",
+                  "transform": [
+                    {
+                      "type": "cross",
+                      "with": "max",
+                      "filter": "datum.a.rank===datum.b.max_rank || datum.a.rank<=10"
+                    },
+                    {"type": "sort","by": "a.rank"} 
+                  ]
+                }
+              ],
+              "scales": [
+                {
+                  "name": "bar",
+                  "type": "linear",
+                  "range": "width",
+                  "domain": {"data": "table","field": "values"}
+                },
+                {
+                  "name": "vertical_head",
+                  "type": "ordinal",
+                  "range": "height",
+                  "domain": {
+                    "data": "head",
+                    "field": "a.rank"
+                  }
+                }
+              ],
+              "marks": [
+                {
+                  "type": "text",
+                  "from": {"data": "head"},
+                  "properties": {
+                    "enter": {
+                      "x": {"value": 20},
+                      "y": {"field": "a.rank","scale": "vertical_head"},
+                      "text": {
+                        "template": "{{datum.a.rank}}.- {{datum.a.country}}"
+                      },
+                      "baseline": {"value": "middle"},
+                      "fontSize": {"value": 13},
+                      "fill": {"value": "#555555"},
+                      "align": {"value": "left"}
+                    }
+                  }
+                },
+                {
+                  "name": "head",
+                  "type": "rect",
+                  "from": {"data": "head"},
+                  "properties": {
+                    "enter": {
+                      "x":{"field": {"group": "width"},"mult": 0.35},
+                      "width":{"scale": "bar", "field": "a.values","mult": 0.5},
+                      "y": {
+                        "field": "a.rank",
+                        "scale": "vertical_head",
+                        "offset": -6
+                      },
+                      "height": {"value": 10},
+                      "fillOpacity": {"value": 1}
+                    }
+                  }
+                }   
+              ]
+            }
+          }
+        }
+      },
+      {
+        widgetId: '04b1610b-74ef-413b-b167-3a0169f77b3f',
+        categories: ['Water', 'Food']
+      },
+      {
+        name: '% of power plants exposed to water risk',
+        categories: ['Water', 'Energy']
+      },
+      {
+        name: 'Watersheds are at risk due to deforestation',
+        categories: ['Water', 'Forests'],
+        data: {
+          attributes: {
+            widgetConfig: {
+              "padding": {"top": 0,"left": 0,"right": 0,"bottom": 0},
+              "data": [
+                {
+                  "url": "https://wri-rw.carto.com/api/v2/sql?q=SELECT count(rs_tl_c) x, rs_tl_c as y FROM river_basins where rs_tl_c != 10  group by rs_tl_c",
+                  "name": "table",
+                  "format": {"type": "json","property": "rows"}
+                },
+                {
+                  "from":"table",
+                  "name": "pie",
+                  "transform": [{"type": "pie","field": "x"}]
+                }
+              ],
+              "marks": [
+                {
+                  "from": {
+                    "data": "table",
+                    "transform": [{"type": "pie","field": "x"}]
+                  },
+                  "type": "arc",
+                  "properties": {
+                    "enter": {
+                      "x": {"mult": 0.5,"field": {"group": "width"}},
+                      "y": {"mult": 0.5,"field": {"group": "height"}},
+                      "fill": {"field": "x","scale": "color"},
+                      "stroke": {"value": "white"},
+                      "endAngle": {"field": "layout_end"},
+                      "startAngle": {"field": "layout_start"},
+                      "innerRadius": {"value": 45},
+                      "outerRadius": {"value": 65}
+                    }
+                  }
+                }
+              ],
+              "scales": [
+                {
+                  "name": "color",
+                  "type": "ordinal",
+                  "range": "category20c",
+                  "domain": {"data": "table","field": "x_percent"}
+                }
+              ]
+            }
+          }
+        }
+      },
+      {
+        widgetId: '2528cfdb-fd03-4965-bf77-3550a43b5055',
+        categories: ['Water', 'Forests']
+      },
+      {
+        widgetId: '73c574b9-f9ab-4f77-87be-651ff8dac5fe',
+        categories: ['Water', 'Biodiversity', 'Climate']
+      },
+      {
+        widgetId: '7cdad4dc-0074-4bd4-b3bd-db6ffc4e0180',
+        categories: ['Water', 'Disasters', 'Society'],
+      },
+      {
+        name: 'Surface water is changing over time',
+        categories: ['Water']
+      }
+    ]
+  },
+  /* The following dashboard is not a real one, but instead a link to the old
+   * country dashboard */
+  {
+    name: 'Countries',
+    slug: 'countries',
+    image: 'static/images/dashboards/dashboard-Cities.jpg',
+    widgets: [{}]
+  },
+  {
+    name: 'Cities',
+    slug: 'cities',
+    description: '',
+    image: 'static/images/dashboards/dashboard-Cities.jpg',
+    widgets: []
+  },
+  {
+    name: 'Society',
+    slug: 'society',
+    description: '',
+    image: 'static/images/dashboards/dashboard-Society.jpg',
+    widgets: []
+  },
+  {
+    name: 'Food',
+    slug: 'food',
+    description: '',
+    image: 'static/images/dashboards/dashboard-Food.jpg',
+    widgets: []
+  },
+  {
+    name: 'Energy',
+    slug: 'energy',
+    description: '',
+    image: 'static/images/dashboards/dashboard-Energy.jpg',
+    widgets: []
+  },
+  {
+    name: 'Forests',
+    slug: 'forests',
+    description: '',
+    image: 'static/images/dashboards/dashboard-Forests.jpg',
+    widgets: []
+  },
+  {
+    name: 'Biodiversity',
+    slug: 'biodiversity',
+    description: '',
+    image: 'static/images/dashboards/dashboard-Biodiversity.jpg',
+    widgets: []
+  },
+  {
+    name: 'Climate',
+    slug: 'climate',
+    description: '',
+    image: 'static/images/dashboards/dashboard-Climate.jpg',
+    widgets: []
+  },
+  {
+    name: 'Disasters',
+    slug: 'disasters',
+    description: '',
+    image: 'static/images/dashboards/dashboard-Disasters.jpg',
+    widgets: []
+  }
+];
+/* eslint-enable */
+
+class DashboardsDetail extends Page {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      // List of dashboards
+      dashboards: [],
+      // Pointer to the selected dashboard
+      selectedDashboard: null,
+      // Whether to show all the dashboards or just a few
+      showMore: false
+    };
+  }
+
+  static async getInitialProps(params) {
+    const { query, res } = params;
+    const slug = query.slug;
+
+    // If the slug is "countries" then we make a redirection to the
+    // old country dashboard on the client
+    // We can't do it on the server, it doesn't work, despite the
+    // documentation:
+    // https://github.com/zeit/next.js/wiki/Redirecting-in-%60getInitialProps%60
+    if (!res && slug === 'countries') {
+      // NOTE: This won't probably work on a dev environment
+      history.replaceState(null, null, '/countries');
+    }
+
+    // We always need to return an object
+    return super.getInitialProps(params);
+  }
+
+  componentWillMount() {
+    this.getDashboards();
+  }
+
+  /**
+   * Event handler executed when a different dashboard is selected
+   * @param {string} slug Slug of the selected dashboard
+   */
+  onChangeDashboard(slug, dash) {
+    const dashboards = dash || this.state.dashboards;
+
+    // If we can't find the dashboard with the specified slug, we just
+    // set the first dashboard as the active one
+    const selectedDashboard = dashboards.find(dashboard => dashboard.slug === slug)
+      || dashboards[0];
+
+    this.setState({ selectedDashboard });
+
+    // We update the URL anyway (only on the client)
+    if (typeof window !== 'undefined') {
+      Router.replaceRoute('dashboards_detail', { slug: selectedDashboard.slug });
+    }
+  }
+
+  /**
+   * Fetch the dashboards, save them in the state, set a default
+   * selected dashboard and update the URL
+   */
+  getDashboards() {
+    // We store the dashboards in the state
+    const dashboards = DASHBOARDS;
+    this.setState({ dashboards });
+
+    // We set the dashboard associated with the slug
+    this.onChangeDashboard(this.props.url.query.slug, dashboards);
+  }
+
+  render() {
+    return (
+      <Layout
+        title="Dashboards"
+        description="Resource Watch Dashboards"
+        url={this.props.url}
+        user={this.props.user}
+        pageHeader
+      >
+        <div className="c-page-dashboards">
+
+          {/* PAGE HEADER */}
+          <div className="c-page-header">
+            <div className="l-container">
+              <div className="page-header-content -padding-b-2">
+                <Breadcrumbs items={[{ name: 'Data', route: 'data' }]} />
+                <Title className="-primary -huge page-header-title">Dashboards</Title>
+              </div>
+            </div>
+          </div>
+
+          <div className="info">
+            <div className="row">
+              <div className="column small-12">
+                <ul className="dashboards-list">
+                  {
+                    this.state.dashboards
+                      .map(dashboard => (
+                        <li
+                          className={classnames({
+                            '-active': this.state.selectedDashboard === dashboard,
+                            '-disabled': !dashboard.widgets.length
+                          })}
+                          key={dashboard.slug}
+                          style={{ backgroundImage: `url(/${dashboard.image})` }}
+                        >
+                          <input
+                            type="radio"
+                            name="dashboard"
+                            id={`dashboard-${dashboard.slug}`}
+                            value={dashboard.slug}
+                            checked={this.state.selectedDashboard === dashboard}
+                            disabled={!dashboard.widgets.length}
+                            onChange={e => this.onChangeDashboard(e.target.value)}
+                          />
+                          <label className="content" htmlFor={`dashboard-${dashboard.slug}`}>
+                            {dashboard.name}
+                          </label>
+                        </li>
+                      ))
+                      .slice(0, this.state.showMore ? this.state.dashboards.length : 5)
+                  }
+                  <li className="-toggle">
+                    <button
+                      type="button"
+                      className="content"
+                      onClick={() => this.setState({ showMore: !this.state.showMore })}
+                    >
+                      <span>{ this.state.showMore ? 'Close' : 'More' }</span>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="row">
+              <div className="column small-12 large-7 dashboard-info">
+                <Title className="-extrabig -secondary">{this.state.selectedDashboard.name}</Title>
+                <p className="description">
+                  {this.state.selectedDashboard.description}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="column small-12 widgets-list">
+              {
+                this.state.selectedDashboard.widgets.map(widget =>
+                  !isEmpty(widget) // Only useful for the countries dashboard
+                    && <DashboardCard
+                      key={widget.name || widget.widgetId}
+                      widgetId={widget.widgetId}
+                      categories={widget.categories}
+                      // The following attributes will be deprecated once all the
+                      // widgets are fetched from the API
+                      name={widget.name}
+                      data={widget.data}
+                    />
+                )
+              }
+            </div>
+          </div>
+
+        </div>
+      </Layout>
+    );
+  }
+
+}
+
+export default DashboardsDetail;
