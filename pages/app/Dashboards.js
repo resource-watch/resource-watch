e0d0f0d4b21eaeaf26a3ba97561d37613b67ac3e
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-07-12 15:23:21 +0100

    Simplify the Dashboard component to just list the dashboards

--- pages/app/Dashboards.js
+++ pages/app/Dashboards.js
@@ -9,7 +9,6 @@ import Page from 'components/app/layout/Page';
 import Layout from 'components/app/layout/Layout';
 import Title from 'components/ui/Title';
 import Breadcrumbs from 'components/ui/Breadcrumbs';
-import DashboardCard from 'components/app/dashboards/DashboardCard';
 
 // Temporarily hard-code the list of dashboards
 // Needs to be updated so the widgets can be shared between various
@@ -268,11 +267,7 @@ class Dashboards extends Page {
     super(props);
     this.state = {
       // List of dashboards
-      dashboards: [],
-      // Pointer to the selected dashboard
-      selectedDashboard: null,
-      // Whether to show all the dashboards or just a few
-      showMore: false
+      dashboards: []
     };
   }
 
@@ -281,46 +276,20 @@ class Dashboards extends Page {
   }
 
   /**
-   * Event handler executed when a different dashboard is selected
+   * Event handler executed when a dashboard is selected
    * @param {string} slug Slug of the selected dashboard
    */
-  onChangeDashboard(slug, dash) {
-    const dashboards = dash || this.state.dashboards;
-
-    // If the selected dashboard is the old country dashboard
-    // we redirect the user there
-    if (slug === 'countries') {
-      location.href = 'http://staging.resourcewatch.org/countries';
-      return;
-    }
-
-    // If we can't find the dashboard with the specified slug, we just
-    // set the first dashboard as the active one
-    const selectedDashboard = dashboards.find(dashboard => dashboard.slug === slug)
-      || dashboards[0];
-
-    this.setState({ selectedDashboard });
-
-    // We update the URL anyway (only on the client)
-    if (typeof window !== 'undefined') {
-      Router.replaceRoute('dashboards', { slug: selectedDashboard.slug });
-    }
+  static onChangeDashboard(slug) {
+    Router.pushRoute('dashboards_detail', { slug });
   }
 
   /**
-   * Fetch the dashboards, save them in the state, set a default
-   * selected dashboard and update the URL
+   * Fetch the dashboards and save them in the state
    */
   getDashboards() {
     // We store the dashboards in the state
     const dashboards = DASHBOARDS;
     this.setState({ dashboards });
-
-    // If the default selected dashboard:
-    // - if the URL contains a slug, then we try to set the associated
-    // dashboard
-    // - if not, the first dashboard will be set as default
-    this.onChangeDashboard(this.props.url.query.slug, dashboards);
   }
 
   render() {
@@ -353,7 +322,6 @@ class Dashboards extends Page {
                       .map(dashboard => (
                         <li
                           className={classnames({
-                            '-active': this.state.selectedDashboard === dashboard,
                             '-disabled': !dashboard.widgets.length
                           })}
                           key={dashboard.slug}
@@ -364,54 +332,22 @@ class Dashboards extends Page {
                             name="dashboard"
                             id={`dashboard-${dashboard.slug}`}
                             value={dashboard.slug}
-                            checked={this.state.selectedDashboard === dashboard}
                             disabled={!dashboard.widgets.length}
-                            onChange={e => this.onChangeDashboard(e.target.value)}
+                            onChange={e => Dashboards.onChangeDashboard(e.target.value)}
                           />
                           <label className="content" htmlFor={`dashboard-${dashboard.slug}`}>
                             {dashboard.name}
                           </label>
                         </li>
                       ))
-                      .slice(0, this.state.showMore ? this.state.dashboards.length : 5)
                   }
-                  <li className="-toggle">
-                    <button
-                      type="button"
-                      className="content"
-                      onClick={() => this.setState({ showMore: !this.state.showMore })}
-                    >
-                      <span>{ this.state.showMore ? 'Close' : 'More' }</span>
-                    </button>
-                  </li>
                 </ul>
               </div>
             </div>
             <div className="row">
               <div className="column small-12 large-7 dashboard-info">
-                <Title className="-extrabig -secondary">{this.state.selectedDashboard.name}</Title>
-                <p className="description">
-                  {this.state.selectedDashboard.description}
-                </p>
+                <Title className="-extrabig -secondary">Select a topic to start exploring</Title>
               </div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="column small-12 widgets-list">
-              {
-                this.state.selectedDashboard.widgets.map(widget => (
-                  <DashboardCard
-                    key={widget.name || widget.widgetId}
-                    widgetId={widget.widgetId}
-                    categories={widget.categories}
-                    // The following attributes will be deprecated once all the
-                    // widgets are fetched from the API
-                    name={widget.name}
-                    data={widget.data}
-                  />
-                ))
-              }
             </div>
           </div>
 
