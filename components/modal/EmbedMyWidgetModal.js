ff77a790fc7772a9e6c13f93ec808d0862c41635
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-07-14 09:41:50 +0200

    add minimal styles to embed modal

--- components/modal/EmbedMyWidgetModal.js
+++ components/modal/EmbedMyWidgetModal.js
@@ -24,11 +24,14 @@ class EmbedMyWidgetModal extends React.Component {
   render() {
     const { widgetId } = this.props;
     const url = `http://staging.resourcewatch.org/embed/widget/${widgetId}`;
+    const iframeText = `<iframe srce="${url}"</iframe>`;
     return (
-      <div className="embed-my-widget-modal">
+      <div className="c-embed-my-widget-modal">
         <h1 className="c-text -header-big -thin">Share into my web</h1>
+        <p>You may include this content on your webpage. To do this, copy the following html
+        code and insert it into the source code of your page:</p>
         <div className="url-container">
-          <input ref={(n) => { this.input = n; }} value={url} className="url" readOnly />
+          <input ref={(n) => { this.input = n; }} value={iframeText} className="url" readOnly />
           <button className="c-btn -primary -filled" onClick={() => this.onCopyClick()}>
             Copy
           </button>
