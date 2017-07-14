3fbdc91720ea53227969bf4ce985b6ed84d8ac46
David Inga <david.inga@vizzuality.com>
2017-07-14 14:57:58 +0200

    recovering https

--- components/modal/EmbedMyWidgetModal.js
+++ components/modal/EmbedMyWidgetModal.js
@@ -23,7 +23,7 @@ class EmbedMyWidgetModal extends React.Component {
 
   render() {
     const { widgetId } = this.props;
-    const url = `http://staging.resourcewatch.org/embed/widget/${widgetId}`;
+    const url = `https://staging.resourcewatch.org/embed/widget/${widgetId}`;
     const iframeText = `<iframe src="${url}" width="100%" height="474" frameBorder="0"></iframe>`;
     return (
       <div className="c-embed-my-widget-modal">
