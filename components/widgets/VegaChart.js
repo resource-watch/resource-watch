b0d8111ec2731145cf8b8f9283b4fd427c34e55a
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-07-24 10:56:15 +0100

    Correctly format dates
    The tooltip now uses the same formatter as for the ticks of the charts.

--- components/widgets/VegaChart.js
+++ components/widgets/VegaChart.js
@@ -52,6 +52,24 @@ class VegaChart extends React.Component {
    * @param {object} item Data associated with the hovered mark, if exist
    */
   onMousemove(vegaConfig, visData, x, item) {
+    // Regarding the Vega configuration, check if a column is a date
+    const isDate = (columnName) => { // eslint-disable-line arrow-body-style
+      return vegaConfig
+        && vegaConfig.data[0].format
+        && vegaConfig.data[0].format.parse
+        && vegaConfig.data[0].format.parse[columnName] === 'date';
+    };
+
+    // Return the type of the data passed as argument
+    const getType = (columnName, val) => {
+      // We need to check the type date before the number one
+      // because a date could be a timestamp
+      if (val instanceof Date || isDate(columnName)) return 'date';
+      if (typeof val === 'number') return 'number';
+      return 'string';
+    };
+
+
     // If the cursor is on top of a mark, we display the data
     // associated to that mark
     // The only exception is for the lines because the data
@@ -60,7 +78,18 @@ class VegaChart extends React.Component {
       return this.props.toggleTooltip(true, {
         follow: true,
         children: VegaChartTooltip,
-        childrenProps: { item: item.datum }
+        childrenProps: {
+          item: {
+            x: {
+              type: getType('x', item.datum.x),
+              value: item.datum.x
+            },
+            y: {
+              type: getType('y', item.datum.y),
+              value: item.datum.y
+            }
+          }
+        }
       });
     }
 
@@ -82,27 +111,47 @@ class VegaChart extends React.Component {
     // If not, then we retrieve the x position of the cursor,
     // and display the data of the closest point/line/bar
     let data; // eslint-disable-line no-shadow
-    if (typeof x === 'string') data = visData.find(d => d.x === x);
-    if (typeof x === 'number') {
+    if (getType('x', x) === 'string') data = visData.find(d => d.x === x);
+    if (getType('x', x) === 'number') {
       const bisectDate = bisector(d => d.x).left;
       const i = bisectDate(sortedVisData, x, 1);
       const d0 = visData[i - 1];
       const d1 = visData[i];
       data = (d0 && d1 && (x - d0.x > d1.x - x)) ? d1 : d0;
     }
-    if (x instanceof Date) {
+    if (getType('x', x)) {
+      const timestamp = x.getTime ? x.getTime() : x;
       const bisectDate = bisector(d => d.x).left;
-      const i = bisectDate(sortedVisData, x.getTime(), 1);
+      const i = bisectDate(sortedVisData, timestamp, 1);
       const d0 = visData[i - 1];
       const d1 = visData[i];
-      data = (d0 && d1 && (x.getTime() - d0.x > d1.x - x.getTime())) ? d1 : d0;
+      data = (d0 && d1 && (timestamp - d0.x > d1.x - timestamp)) ? d1 : d0;
     }
 
     if (data) {
       return this.props.toggleTooltip(true, {
         follow: true,
         children: VegaChartTooltip,
-        childrenProps: { item: data }
+        childrenProps: {
+          item: {
+            x: {
+              type: getType('x', x), // Don't use data.x here
+              value: data.x,
+              range: getType('x', x) === 'date'
+                // We don't need to pass the full array
+                ? [sortedVisData[0].x, sortedVisData[sortedVisData.length - 1].x]
+                : []
+            },
+            y: {
+              type: getType('y', data.y),
+              value: data.y,
+              range: getType('y', data.y) === 'date'
+                // We don't need to pass the full array
+                ? [sortedVisData[0].y, sortedVisData[sortedVisData.length - 1].y]
+                : []
+            }
+          }
+        }
       });
     }
 
--- components/widgets/VegaChartTooltip.js
+++ components/widgets/VegaChartTooltip.js
@@ -1,33 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { format } from 'd3';
+import { format, time } from 'd3';
+
+// Helpers
+import { getTimeFormat } from 'utils/widgets/WidgetHelper';
+
+// Global functions
+const getRoundNumber = format('.0f');
+const getSINumber = format('.2s');
 
 class VegaChartTooltip extends React.Component {
 
   getParsedX() {
     const { item } = this.props;
-    if (item && typeof item.x === 'number') {
-      return format('.0f')(item.x);
+    if (!item) return null;
+
+    const { x } = item;
+
+    if (x.type === 'number') {
+      return getRoundNumber(x.value);
+    } else if (x.type === 'date') {
+      const date = new Date(x.value);
+      return time.format(getTimeFormat(x.range))(date);
     }
 
-    if (item && typeof item.x === 'string') {
-      return item.x;
-    }
-
-    return '';
+    return x.value;
   }
 
   getParsedY() {
     const { item } = this.props;
-    if (item && typeof item.y === 'number') {
-      return format('.2s')(item.y);
+    if (!item) return null;
+
+    const { y } = item;
+    if (!y) return null;
+
+    if (y.type === 'number') {
+      return getSINumber(y.value);
+    } else if (y.type === 'date') {
+      const date = new Date(y.value);
+      return time.format(getTimeFormat(y.range))(date);
     }
 
-    if (item && typeof item.y === 'string') {
-      return item.y;
-    }
-
-    return '';
+    return y.value;
   }
 
 
@@ -41,12 +55,17 @@ class VegaChartTooltip extends React.Component {
   }
 }
 
+const columnType = PropTypes.shape({
+  type: PropTypes.oneOf(['number', 'string', 'date']),
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object]),
+  // Range needed to properly display the dates
+  range: PropTypes.arrayOf(PropTypes.number)
+});
+
 VegaChartTooltip.propTypes = {
-  // Define the chart data
   item: PropTypes.shape({
-    // PropTypes.object being a date object
-    x: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object]),
-    y: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.object])
+    x: columnType,
+    y: columnType
   })
 };
 
