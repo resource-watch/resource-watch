d3bd1742bb6d24c2ff501f116335939728467ef8
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-08-01 18:01:26 +0100

    Update the Modal component to execute a callback when closed
    The callback is only executed if the modal is closed due to a user interaction (ESC key, click on the close button or click on the overlay).

--- redactions/modal.js
+++ redactions/modal.js
@@ -4,6 +4,7 @@
 const MODAL_TOGGLE = 'MODAL_TOGGLE';
 const MODAL_SET_OPTIONS = 'MODAL_SET_OPTIONS';
 const MODAL_LOADING = 'MODAL_LOADING';
+const MODAL_EXECUTE_CLOSE_CALLBACK = 'MODAL_EXECUTE_CLOSE_CALLBACK';
 
 // REDUCER
 const initialState = {
@@ -11,7 +12,10 @@ const initialState = {
   options: {
     children: null,
     childrenProps: null,
-    size: ''
+    size: '',
+    // Callback executed if the user closes the modal (user interaction)
+    // (not executed when toggleModal is executed)
+    onCloseModal: null
   },
   loading: false
 };
@@ -24,6 +28,13 @@ export default function (state = initialState, action) {
       return Object.assign({}, state, { options: action.payload });
     case MODAL_LOADING:
       return Object.assign({}, state, { loading: action.payload });
+    case MODAL_EXECUTE_CLOSE_CALLBACK:
+      if (state.options.onCloseModal) {
+        // We make the call asynchronous to avoid blocking the state
+        // from being computed
+        setTimeout(() => state.options.onCloseModal(), 0);
+      }
+      return state;
     default:
       return state;
   }
@@ -40,11 +51,16 @@ export function closeModal() {
   return dispatch => dispatch({ type: MODAL_TOGGLE });
 }
 
-export function toggleModal(open, opts = {}) {
+export function toggleModal(open, opts = {}, userInteraction = false) {
   return (dispatch) => {
     if (open && opts) {
       dispatch({ type: MODAL_SET_OPTIONS, payload: opts });
     }
+
+    if (userInteraction) {
+      dispatch({ type: MODAL_EXECUTE_CLOSE_CALLBACK });
+    }
+
     dispatch({ type: MODAL_TOGGLE, payload: open });
   };
 }
