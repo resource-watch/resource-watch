eba444fdc2dadb5bf3bcdada62796db366bdb366
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-08-07 15:13:29 +0100

    Improve the styles of the legend

--- components/ui/Legend.js
+++ components/ui/Legend.js
@@ -4,7 +4,6 @@ import { SortableContainer, SortableElement, SortableHandle, arrayMove } from 'r
 // Components
 import LegendType from 'components/ui/LegendType';
 import Icon from 'components/ui/Icon';
-import Switch from 'components/ui/Switch';
 import LayerInfoModal from 'components/modal/LayerInfoModal';
 
 const SortableItem = SortableElement(({ value }) => value);
@@ -18,7 +17,7 @@ const DragHandle = SortableHandle(() => (
 const SortableList = SortableContainer(({ items }) => (
   <ul className="legend-list">
     {items.map((value, index) =>
-      <SortableItem key={`item-${value}`} index={index} value={value} />
+      <SortableItem key={value.key} index={index} value={value} />
     )}
   </ul>
 ));
@@ -82,22 +81,44 @@ class Legend extends React.Component {
     this.props.setModalOptions(options);
   }
 
+  /**
+   * Event handler executed when the user clicks the button
+   * to change the opacity of a layer
+   */
+  onClickOpacity() { // eslint-disable-line class-methods-use-this
+    // TODO: implement
+    alert('Not implemented yet'); // eslint-disable-line no-alert
+  }
+
+  /**
+   * Event handler executed when the user clicks the button
+   * to switch the layer for another one of the same dataset
+   */
+  onClickLayers() { // eslint-disable-line class-methods-use-this
+    // TODO: implement
+    alert('Not implemented yet'); // eslint-disable-line no-alert
+  }
+
   getItemsActions(layer) {
     return (
       <div className="item-actions">
-        <button className="info" onClick={() => this.onLayerInfoModal(layer)}>
-          <Icon name="icon-info" className="-smaller" />
+        <button className="layers" onClick={() => this.onClickLayers()} aria-label="Select other layer">
+          <Icon name="icon-layers" />
+        </button>
+        <button className="opacity" onClick={() => this.onClickOpacity()} aria-label="Change opacity">
+          <Icon name="icon-opacity" />
+        </button>
+        { !this.props.readonly && (
+          <button className="toggle" onClick={() => this.onHideLayer(layer.dataset)} aria-label="Toggle the visibility">
+            <Icon name={this.isLayerVisible(layer) ? 'icon-hide' : 'icon-show'} />
+          </button>
+        ) }
+        <button className="info" onClick={() => this.onLayerInfoModal(layer)} aria-label="More information">
+          <Icon name="icon-info" />
         </button>
         { !this.props.readonly
-          && <Switch
-            onChange={() => this.onHideLayer(layer.dataset)}
-            active={!layer.hidden}
-            classNames="-secondary"
-          />
-        }
-        { !this.props.readonly
-          && <button className="close" onClick={() => this.onDeactivateLayer(layer.dataset)}>
-            <Icon name="icon-cross" className="-smaller" />
+          && <button className="close" onClick={() => this.onDeactivateLayer(layer.dataset)} aria-label="Remove">
+            <Icon name="icon-cross" />
           </button>
         }
       </div>
@@ -108,7 +129,7 @@ class Legend extends React.Component {
     // Reverse layers to show first the last one added
     const layersActiveReversed = this.props.layersActive.slice().reverse();
     return layersActiveReversed.map(layer => (
-      <li key={layer.name} className="c-legend-unit">
+      <li key={layer.slug} className="c-legend-unit">
         <div className="legend-info">
           <header className="legend-item-header">
             <h3 className={this.props.className.color}>
@@ -123,16 +144,22 @@ class Legend extends React.Component {
     ));
   }
 
+  /**
+   * Return whether the layer is hidden or not
+   * @param {object} layer
+   * @returns {boolean}
+   */
+  isLayerVisible(layer) {
+    return !this.props.layersHidden.includes(layer.dataset);
+  }
+
   render() {
     return (
       <div className="c-legend-map">
         <div className={`open-legend ${this.state.open ? '-active' : ''}`}>
-          <h1 className="legend-title">
-            Legend
-            <button className="toggle-legend" onClick={() => this.setState({ open: false })}>
-              <Icon name="icon-arrow-down" className="-small" />
-            </button>
-          </h1>
+          <button className="toggle-legend" onClick={() => this.setState({ open: false })}>
+            <Icon name="icon-arrow-down" className="-small" />
+          </button>
           <SortableList
             items={this.getLegendItems()}
             helperClass="c-legend-unit -sort"
