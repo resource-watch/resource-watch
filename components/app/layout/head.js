b3ca21f46fc78f81247476eebeb9c60cb00f7df5
David Inga <david.inga@vizzuality.com>
2017-08-08 11:52:46 +0200

    fixing style for head element

--- components/app/layout/head.js
+++ components/app/layout/head.js
@@ -8,10 +8,10 @@ import styles from 'css/index.scss';
 export default class Head extends React.Component {
 
   static getStyles() {
-    if (process.env.NODE_ENV === 'production') {
-      // In production, serve pre-built CSS file from /styles/{version}/main.css
-      return <link rel="stylesheet" type="text/css" href={`/styles/${Package.version}/index.css`} />;
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //   // In production, serve pre-built CSS file from /styles/{version}/main.css
+    //   return <link rel="stylesheet" type="text/css" href={`/styles/${Package.version}/index.css`} />;
+    // }
     // In development, serve CSS inline (with live reloading) with webpack
     // NB: Not using dangerouslySetInnerHTML will cause problems with some CSS
     return <style dangerouslySetInnerHTML={{ __html: styles }} />;
