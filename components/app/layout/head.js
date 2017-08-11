056f88286ccd1e7c6f4131ce0904060c3d9607e7
David Inga <david.inga@vizzuality.com>
2017-08-11 12:30:14 +0200

    no sass compilation

--- components/app/layout/head.js
+++ components/app/layout/head.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import HeadNext from 'next/head';
-
-// Styles
-import styles from 'css/index.scss';
+// import Package from '../../../package.json';
 
 export default class Head extends React.Component {
 
@@ -14,7 +12,7 @@ export default class Head extends React.Component {
     // }
     // In development, serve CSS inline (with live reloading) with webpack
     // NB: Not using dangerouslySetInnerHTML will cause problems with some CSS
-    return <style dangerouslySetInnerHTML={{ __html: styles }} />;
+    return <style dangerouslySetInnerHTML={{ __html: require('css/index.scss') }} />;
   }
 
   render() {
