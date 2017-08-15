e5bb3eb2376522adf849f1c82549fdbfceffe636
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-08-15 15:59:57 +0200

    Add vocabularies selectors

--- pages/app/Explore.js
+++ pages/app/Explore.js
@@ -232,25 +232,33 @@ class Explore extends Page {
                 </div>
               </div>
               <div className="search-container">
-                <div className="row collapse">
-                  <div className="column small-12 medium-6">
-                    <SearchInput
-                      onSearch={this.handleFilterDatasetsSearch}
-                      input={{
-                        value: search && search.value,
-                        placeholder: 'Search dataset'
-                      }}
-                    />
-                  </div>
-                  <div className="column small-12 medium-6">
-                    <CustomSelect
-                      options={this.state.vocabularies}
-                      onValueChange={this.handleFilterDatasetsIssue}
-                      placeholder="Select issue"
-                      value={issue && issue.length > 0 && issue[0].value}
-                    />
-                  </div>
-                </div>
+                <SearchInput
+                  onSearch={this.handleFilterDatasetsSearch}
+                  input={{
+                    value: search && search.value,
+                    placeholder: 'Search dataset'
+                  }}
+                />
+              </div>
+              <div className="filters-container">
+                <CustomSelect
+                  options={this.state.vocabularies}
+                  onValueChange={this.handleFilterDatasetsIssue}
+                  placeholder="Topics"
+                  value={issue && issue.length > 0 && issue[0].value}
+                />
+                <CustomSelect
+                  options={this.state.vocabularies}
+                  onValueChange={this.handleFilterDatasetsIssue}
+                  placeholder="Geographies"
+                  value={issue && issue.length > 0 && issue[0].value}
+                />
+                <CustomSelect
+                  options={this.state.vocabularies}
+                  onValueChange={this.handleFilterDatasetsIssue}
+                  placeholder="Data types"
+                  value={issue && issue.length > 0 && issue[0].value}
+                />
               </div>
 
               <DatasetListHeader
