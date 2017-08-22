9d41df228a6a0cbd7f1c9fffd3d9b2b75f193f7b
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-08-22 22:00:25 +0200

    Dashboards: wysiwyg styles

--- components/dashboards/wysiwyg/ToolbarWidgetBtn.js
+++ components/dashboards/wysiwyg/ToolbarWidgetBtn.js
@@ -1,39 +1,132 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { toggleModal } from 'redactions/modal';
+import { AtomicBlockUtils } from 'draft-js';
 
-import ToolbarWidgetModal from 'components/dashboards/wysiwyg/ToolbarWidgetModal';
+import { toastr } from 'react-redux-toastr';
+
+// Services
+import WidgetsService from 'services/WidgetsService';
+
+// Components
+import TetherComponent from 'react-tether';
 
 class ToolbarWidgetBtn extends React.Component {
   static propTypes = {
-    toggleModal: PropTypes.func
+    editorState: PropTypes.object,
+    onChange: PropTypes.func
   };
 
-  openModal = () => {
-    this.props.toggleModal(true, {
-      children: ToolbarWidgetModal,
-      childrenProps: this.props
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: false,
+      widgets: [],
+      widget: null
+    };
+
+    this.widgetsService = new WidgetsService();
+  }
+
+  componentWillMount() {
+    this.widgetsService.fetchAllData({})
+      .then((widgets) => {
+        this.setState({ widgets });
+      })
+      .catch((err) => {
+        toastr.error(err);
+      });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.onScreenClick);
+  }
+
+  /**
+   * UI EVENTS
+   * - onChange
+   * - onToggleDropdown
+   * - onScreenClick
+  */
+  onChange = (id) => {
+    const { editorState } = this.props;
+    const entityKey = editorState
+      .getCurrentContent()
+      .createEntity('EMBEDDED_LINK', 'MUTABLE', { src: `/embed/widget/${id}`, height: 390, width: 500 })
+      .getLastCreatedEntityKey();
+
+    const newEditorState = AtomicBlockUtils.insertAtomicBlock(
+      editorState,
+      entityKey,
+      ' ',
+    );
+
+    this.props.onChange(newEditorState);
+    this.onToggleDropdown(false);
+  }
+
+  // This function is debounced. If you don't do that insane things will happen
+  onToggleDropdown = (to) => {
+    this.setState({
+      active: to || !this.state.active
     });
-  };
+  }
+
+  onScreenClick = (e) => {
+    const el = document.querySelector('.c-tooltip');
+    const clickOutside = el && el.contains && !el.contains(e.target);
+
+    if (clickOutside) {
+      this.onToggleDropdown(false);
+    }
+  }
 
   render() {
     return (
-      <button
-        type="button"
-        className="c-button -primary -compressed"
-        onClick={this.openModal}
-      >
-        Add widget
-      </button>
+      <div className="rdw-custom-wrapper">
+        <TetherComponent
+          attachment="top center"
+          constraints={[{
+            to: 'window'
+          }]}
+          targetOffset="10px 0"
+          classes={{
+            element: 'c-tooltip -arrow-top'
+          }}
+        >
+          {/* First child: This is what the item will be tethered to */}
+          <button
+            type="button"
+            className="c-button -secondary -compressed"
+            onClick={this.onToggleDropdown}
+          >
+            Add widget
+          </button>
+
+          {/* Second child: If present, this item will be tethered to the the first child */}
+          {this.state.active &&
+            <div className="c-widget-tooltip-list">
+              <div className="tooltip-content">
+                <ul className="tooltip-list">
+                  {this.state.widgets.map((w) => {
+                    return (
+                      <li
+                        key={w.id}
+                        onClick={() => { this.onChange(w.id); }}
+                      >
+                        {w.name}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          }
+        </TetherComponent>
+      </div>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-  toggleModal
-}, dispatch);
-
-export default connect(null, mapDispatchToProps)(ToolbarWidgetBtn);
+export default ToolbarWidgetBtn;
