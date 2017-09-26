0fda676050cb1ebb82321402b2bb61f7c77d3dba
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-09-26 12:56:05 +0100

    Display the aliases of the fields in the editor whenever possible

--- components/widgets/editor/ui/CategoryContainer.js
+++ components/widgets/editor/ui/CategoryContainer.js
@@ -51,6 +51,7 @@ class CategoryContainer extends React.Component {
           {category &&
             <ColumnBox
               name={category.name}
+              alias={category.alias}
               type={category.type}
               closable
               configurable={category.type === 'number'}
--- components/widgets/editor/ui/ColorContainer.js
+++ components/widgets/editor/ui/ColorContainer.js
@@ -56,6 +56,7 @@ class ColorContainer extends React.Component {
           {color &&
             <ColumnBox
               name={color.name}
+              alias={color.alias}
               type={color.type}
               closable
               configurable
