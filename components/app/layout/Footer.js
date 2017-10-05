447a17c58a62f0309c2fcacf2781eec5502c8d0b
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-10-05 19:52:23 +0200

    Footer updated

--- components/app/layout/Footer.js
+++ components/app/layout/Footer.js
@@ -17,7 +17,9 @@ const data = [
 
 const about = [
   { name: 'About', route: 'about' },
-  { name: 'Partners', route: 'about_partners' }
+  { name: 'Partners', route: 'about_partners' },
+  { name: 'Terms of Service' },
+  { name: 'Privacy' }
 ];
 
 const insights = [
@@ -90,13 +92,25 @@ class Footer extends React.Component {
           </div>
         </div>
 
-        <div className="footer-terms">
+        <div className="footer-main">
+          <CompoundMenu items={menuData} />
+        </div>
+
+        <div className="footer-lower">
           <div className="l-container">
             <div className="row">
               <div className="column small-12">
-                <div className="terms">
-                  <p>Terms of Service — Privacy</p>
-                  <p>© 2017 - Resource Watch</p>
+                <div className="footer-container">
+                  <div className="footer-item">
+                    <a href="http://www.wri.org/" target="_blank" rel="noreferrer noopener">
+                      <img src="/static/images/wri-logo.svg" alt="WRI logo" />
+                    </a>
+                  </div>
+                  <div className="footer-item">
+                    <p className="-bold">World Resources Institute</p>
+                    <p>10 G Street NE Suite 800, Washington, DC 20002, USA</p>
+                    <p>Phone +1(202) 729-7600    |    Fax: +1 (202) 720 7610</p>
+                  </div>
                 </div>
               </div>
             </div>
