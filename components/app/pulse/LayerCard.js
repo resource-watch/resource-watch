8897917792c622bf8a8bbd43cf93a1bca8d40386
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-10-06 15:49:00 +0200

    add widget to layer card in planet pulse

--- components/app/pulse/LayerCard.js
+++ components/app/pulse/LayerCard.js
@@ -6,82 +6,121 @@ import { Link, Router } from 'routes';
 
 // Redux
 import { connect } from 'react-redux';
+import { setSimilarWidgets } from 'redactions/pulse';
 
 // Components
 import Legend from 'components/app/pulse/Legend';
 // import Spinner from 'components/ui/Spinner';
 import DatasetWidgetChart from 'components/app/explore/DatasetWidgetChart';
 
-function LayerCard(props) {
-  const { layerActive, layerPoints, similarDatasets } = props.pulse;
+// Services
+import WidgetService from 'services/WidgetService';
 
-  const className = classNames({
-    'c-layer-card': true,
-    '-hidden': layerActive === null
-  });
+import { LAYERS_PLANET_PULSE } from 'utils/layers/pulse_layers';
 
-  const datasetId = (layerActive !== null) ? layerActive.attributes.dataset : null;
+class LayerCard extends React.Component {
+  componentWillReceiveProps(nextProps) {
+    this.loadWidgets(nextProps);
+  }
 
-  const similarWidgets = similarDatasets && similarDatasets
-    .map(dataset => dataset.attributes.widget
-      .find(widget => widget.attributes.default === true))
-    .filter(value => typeof value !== 'undefined');
+  loadWidgets(nextProps) {
+    const { pulse } = nextProps;
+    const layerActive = pulse.layerActive && pulse.layerActive.id;
 
-  return (
-    <div className={className}>
-      <h3>{layerActive && layerActive.attributes.name}</h3>
-      <p>{layerActive && layerActive.attributes.description}</p>
-      {layerPoints && layerPoints.length > 0 &&
-        <p>Number of objects: {layerPoints.length}</p>
+    if (layerActive) {
+      let found = false;
+      for (let i = 0; i < LAYERS_PLANET_PULSE.length && !found; i++) {
+        found = LAYERS_PLANET_PULSE[i].layers.find(obj => obj.id === layerActive);
       }
-      <Legend
-        layerActive={layerActive}
-        className={{ color: '-dark' }}
-      />
-      { datasetId &&
-        <Link
-          route={'explore_detail'}
-          params={{ id: datasetId }}
-        >
-          <a className="link_button" >Explore the data</a>
-        </Link>
+      if (found) {
+        const { widgets } = found;
+        if (widgets && widgets.length && widgets.length > 0) {
+          const widgetService = new WidgetService(widgets[0], { apiURL: process.env.WRI_API_URL });
+          widgetService.fetchData().then((response) => {
+            this.props.setSimilarWidgets([response]);
+          });
+        }
+      } else {
+        this.props.setSimilarWidgets([]);
       }
-      {similarWidgets &&
-        <div>
-          <h5>Similar content</h5>
-          <div className="similar-widgets">
-            <div className="row list">
-              {similarWidgets.map(widget =>
-                (<div
-                  key={widget.id}
-                  className="widget-card"
-                  onClick={() => Router.pushRoute('explore_detail', { id: widget.attributes.dataset })}
-                  role="button"
-                  tabIndex={-1}
-                >
-                  <p>{widget.attributes.name}</p>
-                  <DatasetWidgetChart
-                    widget={widget.attributes}
-                    mode="thumbnail"
-                  />
-                </div>
-                ))
-              }
+    }
+  }
+
+  render() {
+    const { layerActive, layerPoints, similarWidgets } = this.props.pulse;
+
+    const className = classNames({
+      'c-layer-card': true,
+      '-hidden': layerActive === null
+    });
+
+    const datasetId = (layerActive !== null) ? layerActive.attributes.dataset : null;
+
+    return (
+      <div className={className}>
+        <h3>{layerActive && layerActive.attributes.name}</h3>
+        <p>{layerActive && layerActive.attributes.description}</p>
+        {layerPoints && layerPoints.length > 0 &&
+          <p>Number of objects: {layerPoints.length}</p>
+        }
+        <Legend
+          layerActive={layerActive}
+          className={{ color: '-dark' }}
+        />
+        {similarWidgets &&
+          <div>
+            <h5>Similar content</h5>
+            <div className="similar-widgets">
+              <div className="row list">
+                {similarWidgets.map(widget =>
+                  (<div
+                    key={widget.id}
+                    className="widget-card"
+                    onClick={() => Router.pushRoute('explore_detail', { id: widget.attributes.dataset })}
+                    role="button"
+                    tabIndex={-1}
+                  >
+                    <div className="widget-title">
+                      {widget.attributes.name}
+                    </div>
+                    <DatasetWidgetChart
+                      widget={widget.attributes}
+                      mode="thumbnail"
+                    />
+                  </div>
+                  ))
+                }
+              </div>
             </div>
           </div>
-        </div>
-      }
-    </div>
-  );
+        }
+        { datasetId &&
+          <Link
+            route={'explore_detail'}
+            params={{ id: datasetId }}
+          >
+            <a className="link_button" >Explore the data</a>
+          </Link>
+        }
+      </div>
+    );
+  }
 }
 
 LayerCard.propTypes = {
   // PROPS
-  pulse: PropTypes.object
+  pulse: PropTypes.object,
+
+  // Actions
+  setSimilarWidgets: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
   pulse: state.pulse
 });
 
-export default connect(mapStateToProps, null)(LayerCard);
+const mapDispatchToProps = dispatch => ({
+  setSimilarWidgets: (widgets) => { dispatch(setSimilarWidgets(widgets)); }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LayerCard);
