resource-watch/resource-watch

commit 83b78c46a23b9e356d80bf6a7b0130946ab73551

Only save the name of the band in the widgetConfig
We would previously save the alias and the description too but these can change at any time.

components/app/myrw/widgets/pages/new.js
@@ -129,7 +129,7 @@ class WidgetsNew extends React.Component {
         {
           paramsConfig: {
             visualizationType,
-            band,
+            band: { name: band.name },
             limit,
             value,
             category,

components/modal/SaveWidgetModal.js
@@ -113,7 +113,7 @@ class SaveWidgetModal extends React.Component {
             chartType,
             filters,
             areaIntersection,
-            band,
+            band: { name: band.name },
             layer: layer && layer.id
           }
         },

components/app/myrw/widgets/pages/edit.js
@@ -150,7 +150,7 @@ class WidgetsEdit extends React.Component {
         {
           paramsConfig: {
             visualizationType,
-            band,
+            band: { name: band.name },
             limit,
             value,
             category,

commit 955df29c506f1f67da6fc02284589202ceeabff0

Fix several bugs related to the widget creation
* In My RW, the user wouldn't be able to create a widget due to a regression from 83b78c4
* In the admin section, the band wouldn't be saved properly due to a regression from f2f5071

components/app/myrw/widgets/pages/new.js
@@ -129,7 +129,7 @@ class WidgetsNew extends React.Component {
         {
           paramsConfig: {
             visualizationType,
-            band: { name: band.name },
+            band: band && { name: band.name },
             limit,
             value,
             category,

components/widgets/editor/modal/SaveWidgetModal.js
@@ -115,7 +115,7 @@ class SaveWidgetModal extends React.Component {
             chartType,
             filters,
             areaIntersection,
-            band,
+            band: band && { name: band.name },
             layer: layer && layer.id
           }
         },