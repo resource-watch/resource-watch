a9e4fc66a3c64f386b264ddcbc7d7e7ab07a8804
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-10-10 16:39:05 +0200

    Terms and privacy added

---
+++ pages/app/Terms.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import renderHTML from 'react-render-html';
+
+import withRedux from 'next-redux-wrapper';
+import { initStore } from 'store';
+import { getStaticData } from 'redactions/static_pages';
+import { setUser } from 'redactions/user';
+import { setRouter } from 'redactions/routes';
+
+import Page from 'components/app/layout/Page';
+import Layout from 'components/app/layout/Layout';
+
+class GetInvolved extends Page {
+  static async getInitialProps({ asPath, pathname, query, req, store, isServer }) {
+    const { user } = isServer ? req : store.getState();
+    const url = { asPath, pathname, query };
+    store.dispatch(setUser(user));
+    store.dispatch(setRouter(url));
+    await store.dispatch(getStaticData('terms-of-service'));
+    return { isServer, user, url };
+  }
+
+  render() {
+    const { data } = this.props;
+    const styles = {};
+
+    if (!data) return null;
+
+    if (data && data.photo) {
+      styles.backgroundImage = `url(${process.env.STATIC_SERVER_URL}${data.photo.cover})`;
+    }
+
+    return (
+      <Layout
+        title="Terms of service"
+        description="Terms of service description"
+        url={this.props.url}
+        user={this.props.user}
+        className="l-static"
+      >
+        <section className="l-content">
+          <header className="l-content-header">
+            <div className="cover" style={styles}>
+              <div className="row">
+                <div className="column small-12">
+                  <div className="content">
+                    <h1>{data.title}</h1>
+                    <p>{data.summary}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </header>
+          <div className="l-content-body">
+            {!!data.content &&
+              <div className="l-container">
+                <article>
+                  <div className="row align-center">
+                    <div className="column small-12 medium-8">
+                      <div className="c-terms">
+                        {renderHTML(data.content || '')}
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </div>
+            }
+          </div>
+        </section>
+      </Layout>
+    );
+  }
+}
+
+GetInvolved.propTypes = {
+  data: PropTypes.object,
+  getStaticData: PropTypes.func
+};
+
+const mapStateToProps = state => ({
+  data: state.staticPages['terms-of-service']
+});
+
+const mapDispatchToProps = dispatch => ({
+  getStaticData: (slug, ref) => {
+    dispatch(getStaticData(slug, ref));
+  }
+});
+
+export default withRedux(initStore, mapStateToProps, mapDispatchToProps)(GetInvolved);
