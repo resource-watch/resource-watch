7033e2dcd3af04f76b0fd27868eb996b44dfb81a
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-10-11 09:01:00 +0200

    move cautions metadata section to a lower position

--- pages/app/ExploreDetail.js
+++ pages/app/ExploreDetail.js
@@ -413,13 +413,6 @@ class ExploreDetail extends Page {
                   </div>
                 ) : null}
 
-                {cautions ? (
-                  <div className="l-section-mod">
-                    <h3>Cautions</h3>
-                    <p>{formattedCautions}</p>
-                  </div>
-                ) : null}
-
                 {metadataInfo && metadataInfo.citation ? (
                   <div className="l-section-mod">
                     <h3>Citation</h3>
@@ -452,6 +445,13 @@ class ExploreDetail extends Page {
                   <div className="l-section-mod">
                     <h3>Frequency of updates</h3>
                     <p>{metadataInfo && metadataInfo.frequency_of_updates}</p>
+                  </div>
+                ) : null}
+
+                {cautions ? (
+                  <div className="l-section-mod">
+                    <h3>Cautions</h3>
+                    <p>{formattedCautions}</p>
                   </div>
                 ) : null}
 
