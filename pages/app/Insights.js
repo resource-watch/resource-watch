c58cee2a1a90e3db889a4fe2ea052dcabaf0b8b1
David Inga <david.inga@vizzuality.com>
2017-10-11 17:48:46 +0200

    changed route for insights detail

--- pages/app/Insights.js
+++ pages/app/Insights.js
@@ -21,12 +21,12 @@ class Insights extends Page {
         className="-alt"
         background={c.background}
         clickable
-        route={`/insights/${c.slug}`}
+        route={`/blog/${c.slug}`}
       >
         <div>
           <h4>{c.tag}</h4>
           <h3>
-            <Link route={`/insights/${c.slug}`}>
+            <Link route={`/blog/${c.slug}`}>
               <a>{c.title}</a>
             </Link>
           </h3>
