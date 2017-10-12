ed7525f555f8886bb4ee5cc9098ca335b12aa831
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-10-12 09:49:55 +0100

    Improve the map embed layout

--- pages/app/embed/EmbedMap.js
+++ pages/app/embed/EmbedMap.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 // Redux
 import withRedux from 'next-redux-wrapper';
@@ -15,6 +16,7 @@ import EmbedLayout from 'components/app/layout/EmbedLayout';
 import Spinner from 'components/widgets/editor/ui/Spinner';
 import Map from 'components/widgets/editor/map/Map';
 import Legend from 'components/widgets/editor/ui/Legend';
+import Icon from 'components/widgets/editor/ui/Icon';
 
 // Utils
 import LayerManager from 'components/widgets/editor/helpers/LayerManager';
@@ -33,12 +35,38 @@ class EmbedMap extends Page {
     return !/localhost|staging.resourcewatch.org/.test(this.props.referer);
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpened: false
+    };
+  }
+
   componentDidMount() {
     this.props.getWidget(this.props.url.query.id);
   }
 
+  getModal() {
+    const { widget } = this.props;
+    return (
+      <div className="widget-modal">
+        { !widget.attributes.description &&
+          <p>No additional information is available</p>
+        }
+
+        { widget.attributes.description && (
+          <div>
+            <h4>Description</h4>
+            <p>{widget.attributes.description}</p>
+          </div>
+        ) }
+      </div>
+    );
+  }
+
   render() {
     const { widget, loading, layerGroups } = this.props;
+    const { modalOpened } = this.state;
 
     if (loading) {
       return (
@@ -59,41 +87,50 @@ class EmbedMap extends Page {
         description={`${widget.attributes.description || ''}`}
       >
         <div className="c-embed-widget">
-          <div className="visualization">
-            <div className="widget-title">
+          <div className="widget-title">
+            <a href={`/data/explore/${widget.attributes.dataset}`} target="_blank" rel="noopener noreferrer">
               <h4>{widget.attributes.name}</h4>
-            </div>
-            <div className="widget-content">
-              <Map
-                LayerManager={LayerManager}
-                mapConfig={mapConfig}
-                layerGroups={layerGroups}
-              />
-
-              <Legend
-                layerGroups={layerGroups}
-                className={{ color: '-dark' }}
-                toggleLayerGroupVisibility={
-                  layerGroup => this.props.toggleLayerGroupVisibility(layerGroup)
-                }
-                setLayerGroupsOrder={() => {}}
-                setLayerGroupActiveLayer={() => {}}
-                interactionDisabled
-                expanded={false}
-              />
-            </div>
-            <p className="widget-description">
-              {widget.attributes.description}
-            </p>
+            </a>
+            <button
+              aria-label={`${modalOpened ? 'Close' : 'Open'} information modal`}
+              onClick={() => this.setState({ modalOpened: !modalOpened })}
+            >
+              <Icon name={`icon-${modalOpened ? 'cross' : 'info'}`} className="c-icon -small" />
+            </button>
           </div>
-          { this.isLoadedExternally() &&
-            <img
-              className="embed-logo"
-              height={21}
-              width={129}
-              src={'/static/images/logo-embed.png'}
-              alt="Resource Watch"
-            /> }
+
+          <div className={classnames('widget-content', { '-external': this.isLoadedExternally() })}>
+            <Map
+              LayerManager={LayerManager}
+              mapConfig={mapConfig}
+              layerGroups={layerGroups}
+            />
+
+            <Legend
+              layerGroups={layerGroups}
+              className={{ color: '-dark' }}
+              toggleLayerGroupVisibility={
+                layerGroup => this.props.toggleLayerGroupVisibility(layerGroup)
+              }
+              setLayerGroupsOrder={() => {}}
+              setLayerGroupActiveLayer={() => {}}
+              interactionDisabled
+              expanded={false}
+            />
+
+            { modalOpened && this.getModal() }
+          </div>
+          { this.isLoadedExternally() && (
+            <div className="widget-footer">
+              <a href="/" target="_blank" rel="noopener noreferrer">
+                <img
+                  className="embed-logo"
+                  src={'/static/images/logo-embed.png'}
+                  alt="Resource Watch"
+                />
+              </a>
+            </div>
+          ) }
         </div>
       </EmbedLayout>
     );
