3efe2ea278fd748612528c98f676baa27c5c6cf3
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-10-30 12:10:59 +0100

    Add env variable to next config

--- next.config.js
+++ next.config.js
@@ -56,7 +56,8 @@ module.exports = {
         'process.env.WRI_API_URL': JSON.stringify(process.env.WRI_API_URL),
         'process.env.STATIC_SERVER_URL': JSON.stringify(process.env.STATIC_SERVER_URL),
         'process.env.ADD_SEARCH_KEY': JSON.stringify(process.env.ADD_SEARCH_KEY),
-        'process.env.TRANSIFEX_LIVE_API': JSON.stringify(process.env.TRANSIFEX_LIVE_API)
+        'process.env.TRANSIFEX_LIVE_API': JSON.stringify(process.env.TRANSIFEX_LIVE_API),
+        'process.env.BING_MAPS_API_KEY': JSON.stringify(process.env.BING_MAPS_API_KEY)
       })
     );
 
