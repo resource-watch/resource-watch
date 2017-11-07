e4dcd050fc7a7df605e30051563ecef68e6a9ef0
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-11-07 14:48:02 +0000

    Fix a direct mutation of the store that affected the sorting

--- selectors/explore/filterDatasets.js
+++ selectors/explore/filterDatasets.js
@@ -24,7 +24,7 @@ const getSortedDatasets = (datasets, sorting) => {
     return datasets;
   }
 
-  return datasets.sort((a, b) => {
+  return [...datasets].sort((a, b) => {
     const aPos = sorting.datasets.indexOf(a.id);
     const bPos = sorting.datasets.indexOf(b.id);
 
