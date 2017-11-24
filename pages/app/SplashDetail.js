9d306211fc6cf633c3e23deda3320e6066115887
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-11-24 13:21:27 +0100

    active-inactive hotspots

--- pages/app/SplashDetail.js
+++ pages/app/SplashDetail.js
@@ -40,6 +40,8 @@ const PANORAMAS = [
             id: 'coral_bleaching_on_the_rise',
             position: '10 2 -10',
             text: 'Since the 1980s, warmer ocean temperatures have contributed to more frequent and more harmful coral bleaching events around the world. From 1980 to 1997, some 370 incidents of coral bleaching and disease were reported. From 1998 to 2010, that number exploded to more than 3,700. (This jump mostly reflects the destructive effect of rising ocean temperatures, though it should be noted that growing awareness, monitoring, and communication of coral bleaching as an issue also played a role.)',
+            image: '../../static/images/splash/coral_bleaching_on_the_rise.png',
+            imageSelected: '../../static/images/splash/coral_bleaching_on_the_rise_active.png',
             widget: <iframe src="https://staging.resourcewatch.org/embed/widget/a47df7b0-0cdb-4fc2-a877-7c4324c1d6fa" width="100%" height="474" frameBorder="0"></iframe>
           }
         ]
@@ -243,10 +245,8 @@ class SplashDetail extends Page {
                 id={elem.id}
                 position={elem.position}
                 key={elem.title}
-                geometry="primitive: plane; height: 1; width: 1"
-                material="shader: flat; src: #markerSelected; transparent: true"
-                event-set__enter="_event: mouseenter; scale: 1.2 1.2 1"
-                event-set__leave="_event: mouseleave; scale: 1 1 1"
+                geometry="primitive: plane; height: 4; width: 6"
+                material={`shader: flat; src: ${(selectedHotspot && selectedHotspot.id === elem.id) ? elem.imageSelected : elem.image}; transparent: true`}
               />
             ))}
 
