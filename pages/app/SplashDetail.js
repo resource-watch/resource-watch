d8e8d401d86464465f7ede444b196a7b5c7cb502
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-11-28 15:11:47 +0100

    Add markup for hotspot 1;

--- pages/app/SplashDetail.js
+++ pages/app/SplashDetail.js
@@ -41,10 +41,35 @@ const PANORAMAS = [
             id: 'coral_bleaching_on_the_rise',
             position: '0 2 -12',
             rotation: "0 0 0",
-            text: `Since the 1980s, warmer ocean temperatures have contributed to more frequent and more harmful coral bleaching events around the world. From 1980 to 1997, some 370 incidents of coral bleaching and disease were reported. From 1998 to 2010, that number exploded to more than 3,700. (This jump mostly reflects the destructive effect of rising ocean temperatures, though it should be noted that growing awareness, monitoring, and communication of coral bleaching as an issue also played a role.)\n The spike in 1998, which represents the first global coral bleaching event, was abnormally high; but, as the graph shows, the frequency and severity of coral bleaching events dramatically increased in the following years as well. What happened in 2002 and 2005?\nWarm ocean temperatures in 2002 caused widespread bleaching, hitting the area around the Great Barrier Reef the hardest. More than half of the Great Barrier Reef’s corals bleached that year. Another spike in 2005 was attributed to an underwater heat wave in the Caribbean, leaving up to 40 percent of reefs bleached there. \nGlobal coral bleaching events\nFirst Event (1998): In 1997, an unusually strong El Niño followed by a La Niña dramatically changed global ocean surface temperatures. Exacerbated by the general effects of global warming, these temperature changes led to the first mass coral bleaching in 1998. An estimated 16 percent of coral died in 1998 alone. Second Event (2010): 2010 set a new record for the hottest year, surpassing 1998. Warm ocean temperatures and a moderate El Niño caused another global bleaching event, affecting reefs in every ocean basin. \nThird Event (2014-2017): This has been the longest and most widespread global coral bleaching event to date. From 2014 to 2017, coral reefs around the world experienced higher-than-normal ocean temperatures, fueled by a partial El Niño in 2014-15 and worsened by a strong El Niño and a La Niña in the following years. Reefs in American Samoa (the location of Airport Reef, seen here) and the Great Barrier Reef were heavily affected in 2015 and again in early 2017, leaving the majority of coral bleached.`,
+            markup:
+              <div>
+                <p>
+                  Since the 1980s, warmer ocean temperatures have contributed to more frequent and more harmful coral bleaching events around the world. From 1980 to 1997, some 370 incidents of coral bleaching and disease were reported. From 1998 to 2010, that number exploded to more than 3,700. (This jump mostly reflects the destructive effect of rising ocean temperatures, though it should be noted that growing awareness, monitoring, and communication of coral bleaching as an issue also played a role.)
+                </p>
+                <p>
+                  The spike in 1998, which represents the first global coral bleaching event, was abnormally high; but, as the graph shows, the frequency and severity of coral bleaching events dramatically increased in the following years as well.
+                </p>
+                <iframe src="https://staging.resourcewatch.org/embed/widget/a47df7b0-0cdb-4fc2-a877-7c4324c1d6fa" width="100%" height="474" frameBorder="0"></iframe>
+                <h3>
+                  What happened in 2002 and 2005?
+                </h3>
+                <p>
+                  Warm ocean temperatures in 2002 caused widespread bleaching, hitting the area around the Great Barrier Reef the hardest. <a href="https://link.springer.com/article/10.1007/s00338-003-0353-y" target="_blank">More than half</a> of the Great Barrier Reef’s corals bleached that year.
+                </p>
+                <p>
+                  Another spike in 2005 was attributed to an underwater heat wave in the Caribbean, leaving <a href="https://www.coris.noaa.gov/activities/caribbean_rpt/SCRBH2005_rpt.pdf" target="_blank">up to 40 percent</a> of reefs bleached there.
+                </p>
+                <h3>
+                  Global coral bleaching events
+                </h3>
+                <ul>
+                  <li><strong>First Event (1998):</strong> In 1997, an unusually strong El Niño followed by a La Niña dramatically changed global ocean surface temperatures. Exacerbated by the general effects of global warming, these temperature changes led to the first mass coral bleaching in 1998. An estimated 16 percent of coral died in 1998 alone.</li>
+                  <li><strong>Second Event (2010):</strong> 2010 set a new record for the hottest year, surpassing 1998. Warm ocean temperatures and a moderate El Niño caused another global bleaching event, affecting reefs in every ocean basin.</li>
+                  <li><strong>Third Event (2014-2017):</strong> This has been the longest and most widespread global coral bleaching event to date. From 2014 to 2017, coral reefs around the world experienced higher-than-normal ocean temperatures, fueled by a partial <a href="https://coralreefwatch.noaa.gov/satellite/analyses_guidance/global_coral_bleaching_2014-17_status.php" target="_blank">El Niño in 2014-15</a> and worsened by a strong El Niño and a La Niña in the following years. Reefs in American Samoa (the location of Airport Reef, seen here) and the Great Barrier Reef were heavily affected in 2015 and again in early 2017, leaving the <a href="http://www.noaa.gov/media-release/us-coral-reefs-facing-warming-waters-increased-bleaching" target="_blank">majority of coral</a> bleached.</li>
+                </ul>
+              </div>,
             image: '../../static/images/splash/coral_bleaching_on_the_rise.png',
-            imageSelected: '../../static/images/splash/coral_bleaching_on_the_rise_active.png',
-            widget: <iframe src="https://staging.resourcewatch.org/embed/widget/a47df7b0-0cdb-4fc2-a877-7c4324c1d6fa" width="100%" height="474" frameBorder="0"></iframe>
+            imageSelected: '../../static/images/splash/coral_bleaching_on_the_rise_active.png'
           },
           {
             title: 'Coral bleaching up close',
@@ -211,9 +236,8 @@ class SplashDetail extends Page {
             <div className="detail-container">
               <h2>{selectedHotspot.title}</h2>
               <div className="text-container">
-                {selectedHotspot.text}
+                {selectedHotspot.markup}
               </div>
-              {selectedHotspot.widget}
             </div>
           </div>
         }
