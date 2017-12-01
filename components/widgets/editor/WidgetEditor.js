resource-watch/resource-watch

commit dc184c935709d2d636e86093d709a3c818d0d6d1

Prevent the editor from breaking when restoring an outdated widget
If the editor would be restored with a widget whose configuration contains columns that don't exist anymore, it would try to render it before checking if it's correct leading to errors.

A flag has been added in the state to monitor the "initialization" of the editor before rendering any visualization.

components/widgets/editor/WidgetEditor.js
@@ -98,6 +98,7 @@ const DEFAULT_STATE = {
   hasGeoInfo: false, // Whether the dataset includes geographical information
   tableName: null, // Name of the table
   chartLoading: false, // Whether the chart is loading its data/rendering
+  initializing: false, // Flag to prevent rendering the vis before the end of the init
 
   // CHART CONFIG
   chartConfig: null, // Vega chart configuration
@@ -208,7 +209,8 @@ class WidgetEditor extends React.Component {
       && this.props.widgetEditor.visualizationType !== 'table'
       && this.props.widgetEditor.visualizationType !== 'map'
       && this.props.widgetEditor.visualizationType !== 'embed'
-      && (hasChangedWidgetEditor || previousState.tableName !== this.state.tableName)) {
+      && (hasChangedWidgetEditor || previousState.tableName !== this.state.tableName)
+      && !this.state.initializing) {
       this.fetchChartConfig();
     }
   }
@@ -757,7 +759,8 @@ class WidgetEditor extends React.Component {
       layersLoaded: false,
       layersError: false,
       jiminyLoaded: false,
-      jiminyError: false
+      jiminyError: false,
+      initializing: true
     }, () => {
       Promise.all([this.getFields(), this.getLayers()])
         .then(() => this.getJiminy())
@@ -792,7 +795,8 @@ class WidgetEditor extends React.Component {
             // up to date (for example, the aliases)
             this.checkEditorRestoredState();
           }
-        });
+        })
+        .then(() => this.setState({ initializing: false }));
     });
   }
 

commit 26525f083ed344e52f427b3bc310eb5efb153b1e

Fix an issue where the editor would be frozen while restoring a widget
Regression from dc184c935709d2d636e86093d709a3c818d0d6d1

components/widgets/editor/WidgetEditor.js
@@ -209,7 +209,8 @@ class WidgetEditor extends React.Component {
       && this.props.widgetEditor.visualizationType !== 'table'
       && this.props.widgetEditor.visualizationType !== 'map'
       && this.props.widgetEditor.visualizationType !== 'embed'
-      && (hasChangedWidgetEditor || previousState.tableName !== this.state.tableName)
+      && (hasChangedWidgetEditor || previousState.tableName !== this.state.tableName
+        || previousState.initializing !== this.state.initializing)
       && !this.state.initializing) {
       this.fetchChartConfig();
     }