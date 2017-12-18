bc64e96ff4cdb0b66b25716356d0a822f3a62870
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-12-18 11:17:40 +0100

    Add missing third hotspot

--- utils/splash/Panoramas.js
+++ utils/splash/Panoramas.js
@@ -14,7 +14,10 @@ export const PANORAMAS = [
         name: 'bleached',
         label: 'Bleached',
         image: 'https://s3.amazonaws.com/wri-api-backups/resourcewatch/staging/images/bleached-optimized.jpg',
-        intro: <p>What might resemble a beautiful snowfall is actually a destructive stress response known as coral bleaching, which occurred in Airport Reef in 2015. Prolonged exposure to warmer ocean temperatures can cause corals to expel their symbiotic algae (which gives color to corals and nourishes them through photosynthesis), leaving the corals’ white skeletons visible. Some corals are able to bounce back from a bleaching event if water temperatures decrease fast enough. In a warming ocean, however, <a href="https://www.coralcoe.org.au/media-releases/two-thirds-of-great-barrier-reef-hit-by-back-to-back-mass-coral-bleaching" target="_blank">corals will have less time to recover</a> between bleaching events, and widespread die-off could occur.\nPhoto date: February 2, 2015</p>,
+        intro: <div>
+          <p>What might resemble a beautiful snowfall is actually a destructive stress response known as coral bleaching, which occurred in Airport Reef in 2015. Prolonged exposure to warmer ocean temperatures can cause corals to expel their symbiotic algae (which gives color to corals and nourishes them through photosynthesis), leaving the corals’ white skeletons visible. Some corals are able to bounce back from a bleaching event if water temperatures decrease fast enough. In a warming ocean, however, <a href="https://www.coralcoe.org.au/media-releases/two-thirds-of-great-barrier-reef-hit-by-back-to-back-mass-coral-bleaching" target="_blank">corals will have less time to recover</a> between bleaching events, and widespread die-off could occur.</p>
+          <p>Photo date: February 2, 2015</p>
+        </div>,
         hotspots: [
           {
             title: 'Coral bleaching on the rise',
@@ -39,14 +42,6 @@ export const PANORAMAS = [
                 <p>
                   Another spike in 2005 was attributed to an underwater heat wave in the Caribbean, leaving <a href="https://www.coris.noaa.gov/activities/caribbean_rpt/SCRBH2005_rpt.pdf" target="_blank">up to 40 percent</a> of reefs bleached there.
                 </p>
-                <h3>
-                  Global coral bleaching events
-                </h3>
-                <ul>
-                  <li><strong>First Event (1998):</strong> In 1997, an unusually strong El Niño followed by a La Niña dramatically changed global ocean surface temperatures. Exacerbated by the general effects of global warming, these temperature changes led to the first mass coral bleaching in 1998. An estimated 16 percent of coral died in 1998 alone.</li>
-                  <li><strong>Second Event (2010):</strong> 2010 set a new record for the hottest year, surpassing 1998. Warm ocean temperatures and a moderate El Niño caused another global bleaching event, affecting reefs in every ocean basin.</li>
-                  <li><strong>Third Event (2014-2017):</strong> This has been the longest and most widespread global coral bleaching event to date. From 2014 to 2017, coral reefs around the world experienced higher-than-normal ocean temperatures, fueled by a partial <a href="https://coralreefwatch.noaa.gov/satellite/analyses_guidance/global_coral_bleaching_2014-17_status.php" target="_blank">El Niño in 2014-15</a> and worsened by a strong El Niño and a La Niña in the following years. Reefs in American Samoa (the location of Airport Reef, seen here) and the Great Barrier Reef were heavily affected in 2015 and again in early 2017, leaving the <a href="http://www.noaa.gov/media-release/us-coral-reefs-facing-warming-waters-increased-bleaching" target="_blank">majority of coral</a> bleached.</li>
-                </ul>
               </div>,
             image: '../../static/images/splash/coral_bleaching_on_the_rise.png',
             imageSelected: '../../static/images/splash/coral_bleaching_on_the_rise_active.png'
@@ -64,6 +59,24 @@ export const PANORAMAS = [
               <p>
                 Normally symbiotic, Symbiodinium actually <a href="https://news.nationalgeographic.com/2016/08/coral-bleaching-video-algae-warming-oceans-environment-science/" target="_blank">becomes toxic to corals</a> at higher temperatures; the process on display in this video (called pulsed inflation) is a defense mechanism. It may protect them in the short term, but corals need this algae to recolonize quickly, because they rely on the sugar the algae produce for nourishment. If temperatures remain high over time, the Symbiodinium will not return, leaving the corals’ white skeletons exposed and the animals defenseless against diseases and other stressors.
               </p>
+            </div>,
+            image: '../../static/images/splash/coral_bleaching_up_close.png',
+            imageSelected: '../../static/images/splash/coral_bleaching_up_close_active.png'
+          },
+          {
+            title: 'Global coral bleaching events',
+            id: 'global_coral_bleaching_events',
+            position: '-14 2 0',
+            rotation: "0 90 0",
+            markup: <div>
+              <h3>
+                Global coral bleaching events
+              </h3>
+              <ul>
+                <li><strong>First Event (1998):</strong> In 1997, an unusually strong El Niño followed by a La Niña dramatically changed global ocean surface temperatures. Exacerbated by the general effects of global warming, these temperature changes led to the first mass coral bleaching in 1998. An estimated 16 percent of coral died in 1998 alone.</li>
+                <li><strong>Second Event (2010):</strong> 2010 set a new record for the hottest year, surpassing 1998. Warm ocean temperatures and a moderate El Niño caused another global bleaching event, affecting reefs in every ocean basin.</li>
+                <li><strong>Third Event (2014-2017):</strong> This has been the longest and most widespread global coral bleaching event to date. From 2014 to 2017, coral reefs around the world experienced higher-than-normal ocean temperatures, fueled by a partial <a href="https://coralreefwatch.noaa.gov/satellite/analyses_guidance/global_coral_bleaching_2014-17_status.php" target="_blank">El Niño in 2014-15</a> and worsened by a strong El Niño and a La Niña in the following years. This is the event that caused the bleaching here at Airport Reef. The Great Barrier Reef was heavily affected in 2015 and again in early 2017, leaving the <a href="http://www.noaa.gov/media-release/us-coral-reefs-facing-warming-waters-increased-bleaching" target="_blank">majority of coral</a> bleached.</li>
+              </ul>
             </div>,
             image: '../../static/images/splash/coral_bleaching_up_close.png',
             imageSelected: '../../static/images/splash/coral_bleaching_up_close_active.png'
