68aec688f4aba69590c6c4349b7fafb82ca09b6e
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-12-18 11:26:51 +0100

    a couple of various fixes

--- pages/app/SplashDetail.js
+++ pages/app/SplashDetail.js
@@ -6,7 +6,7 @@ import classnames from 'classnames';
 // Redux
 import withRedux from 'next-redux-wrapper';
 import { initStore } from 'store';
-import { toggleModal } from 'redactions/modal';
+import { toggleModal, setModalOptions } from 'redactions/modal';
 import { setUser } from 'redactions/user';
 import { setRouter } from 'redactions/routes';
 
