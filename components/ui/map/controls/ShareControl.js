149f702adae4164197b41bb9b60bbfac91694c7c
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-02-13 15:47:26 +0100

    Remove react portal

--- components/ui/map/controls/ShareControl.js
+++ components/ui/map/controls/ShareControl.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 // Components
-import ShareModalExplore from 'components/modal/ShareModalExplore';
 import Icon from 'components/ui/Icon';
 
 // Utils
