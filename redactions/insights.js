d95406cf914d63d5e83a676e8d1c07e40a67d9ea
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2018-02-20 11:57:50 +0100

    Fix RW logos in signal previews

--- redactions/insights.js
+++ redactions/insights.js
@@ -10,7 +10,7 @@ const INSIGHTS_DATA = [
     date: 'Spotlight',
     title: 'Welcome to Resource Watch',
     slug: 'welcome-to-resource-watch',
-    source: { name: 'Resource Watch', path: 'http://www.resourcewatch.org/', img: '/static/images/logo-no-text.png' },
+    source: { name: 'Resource Watch', path: 'http://www.resourcewatch.org/', img: '/static/images/logo-no-text.svg' },
     background: 'url(/static/tempImages/backgrounds/rw_welcome.jpg) top',
     body: ''
   },
@@ -20,7 +20,7 @@ const INSIGHTS_DATA = [
     title: 'Experience a bleached coral reef in 360°',
     slug: 'experience-a-bleached-coral-reef-in-360',
     link: '/splash/coral',
-    source: { name: 'Resource Watch', path: 'http://www.resourcewatch.org/', img: '/static/images/logo-no-text.png' },
+    source: { name: 'Resource Watch', path: 'http://www.resourcewatch.org/', img: '/static/images/logo-no-text.svg' },
     background: 'url(/static/tempImages/backgrounds/BLEACHED-American-Samoa-2-February-2015.jpg)',
     body: ''
   },
@@ -29,7 +29,7 @@ const INSIGHTS_DATA = [
     date: 'April 11, 2018',
     title: "We mapped the world's power plants. Here's what we learned.",
     slug: 'we-mapped-the-worlds-power-plants-heres-what-we-learned',
-    source: { name: 'Resource Watch', path: 'http://www.resourcewatch.org/', img: '/static/images/logo-no-text.png' },
+    source: { name: 'Resource Watch', path: 'http://www.resourcewatch.org/', img: '/static/images/logo-no-text.svg' },
     background: 'url(/static/tempImages/backgrounds/rw_powerwatch.jpg)',
     body: ''
   }
