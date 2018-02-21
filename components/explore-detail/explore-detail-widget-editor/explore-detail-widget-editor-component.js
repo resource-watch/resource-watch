0235871e89f2780e860eacbe52853a9ae49c621c
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-02-21 09:28:06 +0100

    Explore detail: responsive first steps

--- components/explore-detail/explore-detail-widget-editor/explore-detail-widget-editor-component.js
+++ components/explore-detail/explore-detail-widget-editor/explore-detail-widget-editor-component.js
@@ -2,7 +2,8 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 // Utils
-import { getDatasetDefaultEditableWidget } from 'components/explore-detail/explore-detail-helpers';
+import { getDatasetDefaultWidget, getDatasetDefaultEditableWidget } from 'components/explore-detail/explore-detail-helpers';
+import { breakpoints } from 'utils/responsive';
 
 // Components
 import MediaQuery from 'react-responsive';
@@ -17,7 +18,8 @@ import SaveWidgetModal from 'components/modal/SaveWidgetModal';
 // Constants
 class ExploreDetailWidgetEditor extends PureComponent {
   static propTypes = {
-    dataset: PropTypes.object
+    dataset: PropTypes.object,
+    responsive: PropTypes.object
   }
 
   state = {
@@ -29,33 +31,49 @@ class ExploreDetailWidgetEditor extends PureComponent {
   }
 
   render() {
-    const { dataset } = this.props;
+    const { dataset, responsive } = this.props;
+    const defaultWidget = getDatasetDefaultWidget(dataset);
     const defaultEditableWidget = getDatasetDefaultEditableWidget(dataset);
 
     return (
-      <MediaQuery minDeviceWidth={720}>
-        <WidgetEditor
-          datasetId={dataset.id}
-          widgetId={defaultEditableWidget && defaultEditableWidget.id}
-          saveButtonMode="auto"
-          embedButtonMode="auto"
-          titleMode="auto"
-          provideWidgetConfig={(func) => { this.onGetWidgetConfig = func; }}
-          onSave={() => this.handleToggleSaveWidget(true)}
-        />
-
-        <Modal
-          isOpen={this.state.showSaveModal}
-          className="-medium"
-          onRequestClose={() => this.handleToggleSaveWidget(false)}
+      <div className="c-explore-detail-widget-editor">
+        <MediaQuery
+          minDeviceWidth={breakpoints.large}
+          values={{ deviceWidth: responsive.fakeWidth }}
         >
-          <SaveWidgetModal
-            dataset={dataset.id}
-            getWidgetConfig={this.onGetWidgetConfig}
-            onRequestClose={() => this.handleToggleSaveWidget(false)}
+          <WidgetEditor
+            datasetId={dataset.id}
+            widgetId={defaultEditableWidget && defaultEditableWidget.id}
+            saveButtonMode="auto"
+            embedButtonMode="auto"
+            titleMode="auto"
+            provideWidgetConfig={(func) => { this.onGetWidgetConfig = func; }}
+            onSave={() => this.handleToggleSaveWidget(true)}
           />
-        </Modal>
-      </MediaQuery>
+
+          <Modal
+            isOpen={this.state.showSaveModal}
+            className="-medium"
+            onRequestClose={() => this.handleToggleSaveWidget(false)}
+          >
+            <SaveWidgetModal
+              dataset={dataset.id}
+              getWidgetConfig={this.onGetWidgetConfig}
+              onRequestClose={() => this.handleToggleSaveWidget(false)}
+            />
+          </Modal>
+        </MediaQuery>
+
+        {defaultWidget &&
+          <MediaQuery
+            maxDeviceWidth={breakpoints.large - 1}
+            values={{ deviceWidth: responsive.fakeWidth }}
+          >
+            {/* Widget Card */}
+          </MediaQuery>
+        }
+
+      </div>
     );
   }
 }
