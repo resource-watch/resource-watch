64b6fb3184ad8d61d654a337f76d358a4e76b1fb
Tomas Eriksson <tomasmoosee@gmail.com>
2018-02-26 16:27:56 +0100

    redux event to set layer config for preview map

--- components/admin/layers/form/interactions/interactions-actions.js
+++ components/admin/layers/form/interactions/interactions-actions.js
@@ -4,10 +4,12 @@ import { createAction, createThunkAction } from 'redux-tools';
 import LayersService from 'services/LayersService';
 
 // Actions
+import { generateLayerGroups } from 'components/admin/layers/form/layer-preview/layer-preview-actions';
+
 export const toggleLoading = createAction('ADMIN_TOGGLE_INTERACTIONS_LOADING');
 export const setInteractions = createAction('ADMIN_SET_INTERACTIONS');
-
 export const modifyInteractions = createAction('ADMIN_MODIFY_INTERACTION');
+
 
 export const getInteractions = createThunkAction('ADMIN_GET_INTERACTIONS', props => (dispatch) => {
   dispatch(toggleLoading());
@@ -17,11 +19,13 @@ export const getInteractions = createThunkAction('ADMIN_GET_INTERACTIONS', props
   });
   if (form && form.provider !== 'wms') {
     layerService.getColumns({ dataset: form.dataset })
-      .then((interactions) => {
-        dispatch(setInteractions({
+      .then((data) => {
+        const interactions = {
           added: form.interactionConfig.output || [],
-          available: interactions.fields
-        }));
+          available: data.fields
+        };
+        dispatch(setInteractions(interactions));
+        dispatch(generateLayerGroups({ form, interactions }));
         dispatch(toggleLoading());
       });
   }
--- components/admin/layers/form/steps/Step1.js
+++ components/admin/layers/form/steps/Step1.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import findIndex from 'lodash/findIndex';
-
 // Constants
 import { PROVIDER_OPTIONS, FORM_ELEMENTS } from 'components/admin/layers/form/constants';
 
