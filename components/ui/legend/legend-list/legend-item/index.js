83e9a41239c70b72e4a6ce686539fb59e32437a0
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-02-26 18:21:26 +0100

    Explore: legend drag fix

--- components/ui/legend/legend-list/legend-item/index.js
+++ components/ui/legend/legend-list/legend-item/index.js
@@ -3,4 +3,6 @@ import { SortableElement } from 'react-sortable-hoc';
 
 import LegendItemComponent from './legend-item-component';
 
-export default SortableElement(({ value, ...props }) => <LegendItemComponent {...value} {...props} />);
+export default SortableElement(({ value, ...props }) =>
+  <LegendItemComponent key={props.dataset} {...value} {...props} />
+);
