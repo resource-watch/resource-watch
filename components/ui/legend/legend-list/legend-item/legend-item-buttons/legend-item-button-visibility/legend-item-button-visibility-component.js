87bf9b7f85512a8867d7af654be3b26edcd96b8f
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-02-26 17:12:32 +0100

    Legend: visibility

---
+++ components/ui/legend/legend-list/legend-item/legend-item-buttons/legend-item-button-visibility/legend-item-button-visibility-component.js
@@ -0,0 +1,43 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+// Components
+import Icon from 'components/ui/Icon';
+
+// Tooltip
+import Tooltip from 'rc-tooltip/dist/rc-tooltip';
+
+class LegendItemButtonVisibility extends PureComponent {
+  static propTypes = {
+    activeLayer: PropTypes.object,
+    visible: PropTypes.bool,
+    onChangeVisibility: PropTypes.func
+  }
+
+  render() {
+    const { activeLayer, visible } = this.props;
+
+    return (
+      <Tooltip
+        overlay="Visibility"
+        overlayClassName="c-rc-tooltip -default"
+        overlayStyle={{
+          color: '#fff'
+        }}
+        placement="top"
+        trigger="click"
+      >
+        <button
+          type="button"
+          className="toggle"
+          onClick={() => this.props.onChangeVisibility(activeLayer, !visible)}
+          aria-label="Toggle the visibility"
+        >
+          <Icon name={visible ? 'icon-hide' : 'icon-show'} />
+        </button>
+      </Tooltip>
+    );
+  }
+}
+
+export default LegendItemButtonVisibility;
