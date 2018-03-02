78ad6f27f63685cc27f32b1a3cfb8492f5a6487b
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2018-03-02 12:45:23 +0100

    Add selectors file

---
+++ components/app/pulse/globe-cesium/selectors.js
@@ -0,0 +1,18 @@
+import { createSelector } from 'reselect';
+
+const getContextLayers = state => state.contextLayersPulse.contextLayers;
+const getActiveLayers = state => state.contextLayersPulse.activeLayers;
+
+export const activeContextLayers = createSelector(
+  [getContextLayers],
+  [getActiveLayers],
+  (contextLayers, activeLayers) => {
+    debugger;
+    return contextLayers.filter(l => activeLayers.includes(l.attributes.id));
+  }
+);
+
+
+export default {
+  activeContextLayers
+};
