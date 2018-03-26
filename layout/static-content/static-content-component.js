e90c85db491ff2e579a04597d33726d27d366b3c
Ian Denty <ian.denty@vizzuality.com>
2018-03-26 16:41:36 +0200

    Get Involved: Remove additional space caused by empty quill content

--- layout/static-content/static-content-component.js
+++ layout/static-content/static-content-component.js
@@ -3,19 +3,24 @@ import PropTypes from 'prop-types';
 import renderHTML from 'react-render-html';
 
 
-const StaticContentComponent = ({ content }) => (
-  <section className="l-content">
-    <article className="l-content-body">
-      <div className="l-container">
-        <div className="row align-center">
-          <div className="column small-12 medium-8">
-            { renderHTML(content) }
+const StaticContentComponent = ({ content }) => {
+  const emptyContent = '<p><br></p>';
+  if (content === emptyContent) return null;
+
+  return (
+    <section className="l-content">
+      <article className="l-content-body">
+        <div className="l-container">
+          <div className="row align-center">
+            <div className="column small-12 medium-8">
+              { renderHTML(content) }
+            </div>
           </div>
         </div>
-      </div>
-    </article>
-  </section>
-);
+      </article>
+    </section>
+  );
+};
 
 StaticContentComponent.propTypes = {
   content: PropTypes.string
