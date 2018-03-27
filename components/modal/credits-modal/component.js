832b1f0be411c20732707e9126cfb8d02aea5cb0
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2018-03-27 14:46:15 +0200

    add first version of credits modal

---
+++ components/modal/credits-modal/component.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class CreditsModal extends React.Component {
+  render() {
+    return (
+      <div className="credits-modal">
+        <h2>Credits</h2>
+        <div className="upper-container">
+          <div>
+            <div>
+              <strong>Text:</strong> Maria Hart and Will Halicks
+            </div>
+            <div>
+              <strong>Design:</strong> Daniel Caso and Pablo Pareja Tobes
+            </div>
+            <div>
+              <strong>Research:</strong> Rosie Gilroy
+            </div>
+          </div>
+          <div>
+            <div>
+              <strong>Review:</strong> Katie Reytar and Kristian Teleki
+            </div>
+            <div>
+              <strong>Data Preparation:</strong> Nathan Suberi
+            </div>
+            <div>
+              <strong>Additional Design:</strong> [to come]
+            </div>
+          </div>
+        </div>
+        <div className="main-container">
+          <p>
+            360-degree images courtesy The Ocean Agency / XL Catlin Seaview Survey. Watch Chasing Coral, a documentary chronicling their epic underwater campaign to document the disappearance of coral reefs, <a href="https://www.netflix.com/title/80168188" target="_blank" rel="noopener noreferrer">streaming now</a>.
+          </p>
+          <p>
+            <a href="https://www.flickr.com/photos/travels_with_tam/32828868944/in/photolist-S1YwZN-7pTeih-53CDnm-68xUDN-8a1GLP-5eMKt-68Hims-Wcgp3W-u5ckV-bHdAKH-buiNqL-bHdAx6-9SxRHy-aLyYd-audraQ-buiNoo-6dw6bM-dXb1Ym-tJTSh-7XFLEG-52FSAK-zNpnRD-4JBbui-68itCP-5Bt6SY-auaLx4-4JBbL2-dyF4sg-t27QF-dyF4Gz-oJDWwu-buiNyY-bHdACX-bHdAHB-4WVKT5-auaM1r-eSVvJY-7x2bwZ-dGMUxo-9DfWFh-auaLrg-aqUVBZ-7uhhfe-9Dd2Kt-9DfXyW-3fMBfE-9yUnYo-7qHs7q-9Dd3ug-HCSn8v" target="_blank" rel="noopener noreferrer">Humphead wrasse image</a> courtesy Tam Warner Minton and used here under a <a href="https://creativecommons.org/licenses/by/2.0/" target="_blank" rel="noopener noreferrer">CC BY 2.0 license</a>.
+          </p>
+          <p>
+            Scuba audio courtesy HDVideoGuy/<a href="https://freesound.org/people/HDVideoGuy/sounds/156011/" target="_blank" rel="noopener noreferrer">freesound.org</a> and used here under a <a href="https://creativecommons.org/licenses/by-nc/3.0/" target="_blank" rel="noopener noreferrer">CC BY-NC 3.0 license</a>.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CreditsModal;
--- components/splash/layout/Header.js
+++ components/splash/layout/Header.js
@@ -3,35 +3,62 @@ import PropTypes from 'prop-types';
 
 import { Link } from 'routes';
 
-function Header(props) {
-  const { skipAnimation, hideSkip } = props;
+// Modal
+import Modal from 'components/modal/modal-component';
+import CreditsModal from 'components/modal/credits-modal';
 
-  return (
-    <div className="c-splash-header">
-      <Link route="home">
-        <img className="logo" src="/static/images/logo-resource-watch.png" alt="Resource Watch" />
-      </Link>
-      <div className="links">
-        {props.showEarthViewLink &&
-        <div className="earth-view-link">
-          <a href="/splash">
-            <img src="/static/images/splash/globe.svg" alt="Earth view" /><span className="link-text">Earth View</span>
-          </a>
-        </div>
-        }
-        <a
-          role="button"
-          onClick=""
-        >
-          Credits
-        </a>
-        {!hideSkip && <button onClick={skipAnimation} className="c-splash-header-skip-intro">SKIP INTRO</button>}
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showCreditsModal: false
+    };
+  }
+
+  handleToggleCreditsModal = (bool) => {
+    this.setState({ showCreditsModal: bool });
+  }
+
+  render() {
+    const { skipAnimation, hideSkip, showEarthViewLink } = this.props;
+
+    return (
+      <div className="c-splash-header">
         <Link route="home">
-          <a>Go to Resource Watch</a>
+          <img className="logo" src="/static/images/logo-resource-watch.png" alt="Resource Watch" />
         </Link>
+        <div className="links">
+          {showEarthViewLink &&
+          <div className="earth-view-link">
+            <a href="/splash">
+              <img src="/static/images/splash/globe.svg" alt="Earth view" /><span className="link-text">Earth View</span>
+            </a>
+          </div>
+          }
+          <a
+            role="button"
+            tabIndex={0}
+            onKeyDown={() => this.handleToggleCreditsModal(true)}
+            onClick={() => this.handleToggleCreditsModal(true)}
+          >
+            Credits
+            <Modal
+              isOpen={this.state.showCreditsModal}
+              className="-medium"
+              onRequestClose={() => this.handleToggleCreditsModal(false)}
+            >
+              <CreditsModal />
+            </Modal>
+          </a>
+          {!hideSkip && <button onClick={skipAnimation} className="c-splash-header-skip-intro">SKIP INTRO</button>}
+          <Link route="home">
+            <a>Go to Resource Watch</a>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Header.defaultProps = {
@@ -40,7 +67,8 @@ Header.defaultProps = {
 
 Header.propTypes = {
   showEarthViewLink: PropTypes.bool,
-  skipAnimation: PropTypes.func.isRequired
+  skipAnimation: PropTypes.func.isRequired,
+  hideSkip: PropTypes.bool
 };
 
 export default Header;
