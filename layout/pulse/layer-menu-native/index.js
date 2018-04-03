8924596c6ca8667f324b539d560a9579562b18f5
pjosh <jorgegar@gmail.com>
2018-04-03 14:58:47 +0200

    remove unnecessary container

--- layout/pulse/layer-menu-native/index.js
+++ layout/pulse/layer-menu-native/index.js
@@ -1,4 +1,3 @@
-import { Component, createElement } from 'react';
 import { connect } from 'react-redux';
 
 import * as actions from './actions';
@@ -7,18 +6,8 @@ import initialState from './initial-state';
 
 import LayerMenuNativeComponent from './component';
 
-const mapStateToProps = state => ({
-  layerActive: state.pulse.layerActive
-});
-
-class LayerMenuNativeContainer extends Component {
-  render() {
-    return createElement(LayerMenuNativeComponent, {
-      ...this.props
-    });
-  }
-}
+const mapStateToProps = () => ({});
 
 export { actions, reducers, initialState };
 
-export default connect(mapStateToProps, actions)(LayerMenuNativeContainer);
+export default connect(mapStateToProps, actions)(LayerMenuNativeComponent);
