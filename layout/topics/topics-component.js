ea2ac5ad769455bf868fdebe09bc111fc84b4e8a
Tomas Eriksson <tomasmoosee@gmail.com>
2018-04-03 11:50:29 +0200

    topic page now using static page header logic, some style updates

--- layout/topics/topics-component.js
+++ layout/topics/topics-component.js
@@ -1,7 +1,11 @@
 /* eslint max-len: 0 */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Router } from 'routes';
+
+// Redux
+import { connect } from 'react-redux';
 
 // Components
 import Layout from 'layout/layout/layout-app';
@@ -9,21 +13,29 @@ import TopicThumbnailList from 'components/topics/thumbnail-list';
 
 class TopicsComponent extends React.PureComponent {
   render() {
+    const { data } = this.props;
+
+    const styles = {};
+    if (data && data.photo) {
+      styles.backgroundImage = `url(${process.env.STATIC_SERVER_URL}${data.photo.cover})`;
+    }
+
     return (
       <Layout
         title="Topics"
         description="Resource Watch Topics"
-        className="page-topics"
-        pageHeader
+        className="l-static p-topics"
+
       >
-        <div className="l-page-header">
-          <div className="l-container">
-            <div className="row">
-              <div className="column small-12">
-                <div className="page-header-content">
-                  <h1>Topics</h1>
-                  <div className="page-header-info">
-                    <p>Find facts and figures on people and the enviornment, or see the latest data on the world today.</p>
+
+        <div className="l-content">
+          <div className="l-content-header">
+            <div className="cover" style={styles}>
+              <div className="row">
+                <div className="column small-12">
+                  <div className="content">
+                    <h1>{data.title || 'Topics'}</h1>
+                    <p>{data.summary || ''}</p>
                   </div>
                 </div>
               </div>
@@ -31,7 +43,7 @@ class TopicsComponent extends React.PureComponent {
           </div>
         </div>
 
-        <div className="l-section">
+        <div className="l-section -small">
           <div className="l-container">
             <div className="row">
               <div className="column small-12">
@@ -47,10 +59,19 @@ class TopicsComponent extends React.PureComponent {
               </div>
             </div>
           </div>
+
         </div>
       </Layout>
     );
   }
 }
 
-export default TopicsComponent;
+TopicsComponent.propTypes = {
+  data: PropTypes.object
+};
+
+const mapStateToProps = state => ({
+  data: state.staticPages.topics
+});
+
+export default connect(mapStateToProps, null)(TopicsComponent);
