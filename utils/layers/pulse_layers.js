ee50b5601d87ff3403180dc016bfd95b082d9809
fgassert <cowbox314@gmail.com>
2018-04-09 00:35:27 -0400

    one last basemap

--- utils/layers/pulse_layers.js
+++ utils/layers/pulse_layers.js
@@ -375,8 +375,8 @@ export const LAYERS_PLANET_PULSE = [
         contextLayersOnTop: false,
         widgets: [],
         basemap: {
-          name: 'other',
-          url: 'https://api.mapbox.com/styles/v1/wri/cjd56ttip0i1s2rnxv8py2km5/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid3JpIiwiYSI6Ik9TY2w5RTQifQ.0HV7dQTjK40mk7GpNNA64g'
+          name: 'default',
+          url: 'https://api.mapbox.com/styles/v1/resourcewatch/cj1erey1c00ia2rqmvh6htv3x/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmVzb3VyY2V3YXRjaCIsImEiOiJjajFlcXZhNzcwMDBqMzNzMTQ0bDN6Y3U4In0.FRcIP_yusVaAy0mwAX1B8w'
         }
       },
       {
