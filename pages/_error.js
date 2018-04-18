993c242e2368f8e19c4f513430535f08fb312c13
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2018-04-18 14:14:22 +0200

    use logPageView instead

--- pages/_error.js
+++ pages/_error.js
@@ -4,29 +4,38 @@ import { Link } from 'routes';
 import PropTypes from 'prop-types';
 
 // Utils
-import { logEvent } from 'utils/analytics';
+import { initGA, logPageView } from 'utils/analytics';
 
-function Error(props) {
-  // log url requested
-  logEvent('404 page', props.url.pathname);
-  return (
-    <div className="p-error">
-      <HeadNext>
-        <style dangerouslySetInnerHTML={{ __html: require('css/index.scss') }} />
-      </HeadNext>
-      <div className="container">
-        <h1>404</h1>
-        <p>This page could not be found</p>
-        <Link route="home">
-          <a
-            className="c-button -a"
-          >
-            Go to Resource Watch
-          </a>
-        </Link>
+class Error extends React.Component {
+  componentDidMount() {
+    // Google Analytics
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }
+  render() {
+    // log url requested
+    return (
+      <div className="p-error">
+        <HeadNext>
+          <style dangerouslySetInnerHTML={{ __html: require('css/index.scss') }} />
+        </HeadNext>
+        <div className="container">
+          <h1>404</h1>
+          <p>This page could not be found</p>
+          <Link route="home">
+            <a
+              className="c-button -a"
+            >
+              Go to Resource Watch
+            </a>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Error.propTypes = {
