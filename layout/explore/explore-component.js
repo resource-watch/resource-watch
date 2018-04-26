66db54d1cd51e0ed4334b9ec0719f932ca55ae40
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2018-04-26 14:50:24 +0200

    show warning for Explore mobile version

--- layout/explore/explore-component.js
+++ layout/explore/explore-component.js
@@ -9,6 +9,9 @@ import { breakpoints } from 'utils/responsive';
 // Components
 import Layout from 'layout/layout/layout-app';
 
+// Modal
+import Modal from 'components/modal/modal-component';
+
 // Explore components
 import ExploreSidebar from 'layout/explore/explore-sidebar';
 import ExploreHeader from 'layout/explore/explore-header';
@@ -22,8 +25,17 @@ class Explore extends React.Component {
     responsive: PropTypes.object
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mobileWarningOpened: true
+    };
+  }
+
   render() {
     const { responsive } = this.props;
+    const { mobileWarningOpened } = this.state;
     return (
       <Layout
         title="Explore Data Sets — Resource Watch"
@@ -41,6 +53,21 @@ class Explore extends React.Component {
               </div>
             </div>
           </ExploreSidebar>
+
+          {/* Mobile warning */}
+          <MediaQuery
+            maxDeviceWidth={breakpoints.medium}
+            values={{ deviceWidth: responsive.fakeWidth }}
+          >
+            <Modal
+              isOpen={mobileWarningOpened}
+              onRequestClose={() => this.setState({ mobileWarningOpened: false })}
+            >
+              <div>
+                <p>The mobile version of Explore has limited functionality, please check the desktop version to have access to the full list of features available.</p>
+              </div>
+            </Modal>
+          </MediaQuery>
 
           {/* Desktop map */}
           <MediaQuery
