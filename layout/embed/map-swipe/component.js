26d725d450bd7d6ccd0eb4394b7d4b1e65a40290
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-08-22 17:12:27 +0200

    Map-sideBySide: working

--- layout/embed/map-swipe/component.js
+++ layout/embed/map-swipe/component.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import flatten from 'lodash/flatten';
 
 // Components
 import LayoutEmbed from 'layout/layout/layout-embed';
-import Map from 'components/ui/map/Map';
-import { Legend, LegendItemTypes } from 'wri-api-components';
 
-// Utils
-import LayerManager from 'utils/layers/LayerManager';
+import {
+  Map,
+  MapControls,
+  MapSideBySide,
+  ZoomControl,
+  Legend,
+  LegendListItem,
+  LegendItemTypes
+} from 'wri-api-components';
+
+import { LayerManager, Layer } from 'layer-manager/dist/react';
+import { PluginLeaflet } from 'layer-manager';
+
+import { BASEMAPS, LABELS } from 'components/ui/map/constants';
 
 class EmbedMapSwipe extends React.Component {
   static propTypes = {
@@ -48,28 +59,92 @@ class EmbedMapSwipe extends React.Component {
               '-external': isLoadedExternally
             })}
           >
-            <Map
-              swipe
-              mapConfig={{
-                zoom,
-                latLng
-              }}
-              layerGroups={layerGroups}
-              LayerManager={LayerManager}
-            />
+            <div>
+              <div className="c-map">
+                <Map
+                  mapOptions={{
+                    zoom,
+                    center: latLng
+                  }}
+                  basemap={{
+                    url: BASEMAPS.dark.value,
+                    options: BASEMAPS.dark.options
+                  }}
+                  label={{
+                    url: LABELS.light.value,
+                    options: LABELS.light.options
+                  }}
+                >
+                  {map => (
+                    <React.Fragment>
+                      {/* Controls */}
+                      <MapControls
+                        customClass="c-map-controls -embed"
+                      >
+                        <ZoomControl map={map} />
+                      </MapControls>
+
+                      {/* LayerManager */}
+                      <LayerManager map={map} plugin={PluginLeaflet}>
+                        {layerManager => layerGroups && flatten(layerGroups.map(lg => lg.layers.filter(l => l.active === true))).map((l, i) => (
+                          <Layer
+                            layerManager={layerManager}
+                            {...l}
+                            key={l.id}
+                            zIndex={1000 - i}
+
+                            onReady={(layers) => {
+                              const setLayers = {
+                                0: 'setLeftLayers',
+                                1: 'setRightLayers'
+                              };
+
+                              layers.forEach((lm, index) => {
+                                const { mapLayer } = lm;
+                                if (mapLayer.group) {
+                                  mapLayer.getLayers().forEach((ml, j) => {
+                                    if (j === 0) {
+                                      this.sideBySide[setLayers[index]](ml);
+                                    }
+                                  });
+                                } else {
+                                  this.sideBySide[setLayers[index]](mapLayer);
+                                }
+                              });
+                            }}
+                          />
+                        ))}
+                      </LayerManager>
+                      
+                      {/* MapSideBySide */}
+                      <MapSideBySide
+                        map={map}
+                        onReady={(sideBySide) => { this.sideBySide = sideBySide; }}
+                      />
+                    </React.Fragment>
+                  )}
+                </Map>
+              </div>
+            </div>
           </div>
 
           <div className="widget-content-row">
             {layerGroups.map((lg, i) => (
               <Legend
-                key={`${lg.id}${i}`}
+                key={lg.dataset}
                 maxWidth="50%"
+                maxHeight={150}
                 sortable={false}
                 collapsable={false}
-                maxHeight={150}
-                layerGroups={[lg]}
-                LegendItemTypes={<LegendItemTypes />}
-              />
+              >
+                <LegendListItem
+                  index={i}
+                  key={lg.dataset}
+                  layerGroup={lg}
+                >
+                  <LegendItemTypes />
+                </LegendListItem>
+              </Legend>
             ))}
           </div>
 
