6547d35841481155f079f9eed30f48e8c1878b47
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2018-10-08 11:33:29 +0200

    Displays summary and descriptio in dashboard detail page

--- components/dashboards/detail/dashboard-detail.js
+++ components/dashboards/detail/dashboard-detail.js
@@ -5,14 +5,9 @@ import initialState from './dashboard-detail-default-state';
 
 import DashboardDetail from './dashboard-detail-component';
 
-// Mandatory
-export {
-  actions, reducers, initialState
-};
+export { actions, reducers, initialState };
 
 export default connect(
-  state => ({
-    dashboardDetail: state.dashboardDetail
-  }),
+  state => ({ dashboardDetail: state.dashboardDetail }),
   actions
 )(DashboardDetail);
