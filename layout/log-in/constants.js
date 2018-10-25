e17327721d54037ad815aebc6e0b9a7fb2e60d75
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2018-10-25 11:22:32 +0200

    Adds better form validations

---
+++ layout/log-in/constants.js
@@ -0,0 +1,20 @@
+export const FORM_ELEMENTS = {
+  elements: {},
+  validate() {
+    const { elements } = this;
+    Object.keys(elements).forEach((k) => {
+      elements[k].validate();
+    });
+  },
+  isValid() {
+    const { elements } = this;
+    const valid = Object.keys(elements)
+      .map(k => elements[k].isValid())
+      .filter(v => v !== null)
+      .every(element => element);
+
+    return valid;
+  }
+};
+
+export default { FORM_ELEMENTS };
