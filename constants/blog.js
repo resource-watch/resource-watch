9ab6b98bc90f875d481d0820d4e14c436e45d8bf
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2019-06-11 14:55:25 +0200

    Add uncategorized category to constants and update action + fetch accordingly

--- constants/blog.js
+++ constants/blog.js
@@ -1,9 +1,11 @@
 // number of featured posts
 export const SPOTLIGHT_CATEGORY = 15;
+export const UNCATEGORIZED_CATEGORY = 1;
 
 export const ERROR_MESSAGE_FETCH_POSTS = 'Ops, something went wrong getting latest posts. Please, try later.';
 
 export default {
   SPOTLIGHT_CATEGORY,
+  UNCATEGORIZED_CATEGORY,
   ERROR_MESSAGE_FETCH_POSTS
 };
