347f50dc83d78c4a561b66b189763c44d9521401
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2019-06-13 17:58:04 +0200

    Fixes fetching widgets in MyRW

---
+++ components/app/myrw/widgets/tabs/list/index.js
@@ -0,0 +1,109 @@
+import React, { useReducer, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { toastr } from 'react-redux-toastr';
+import debounce from 'lodash/debounce';
+
+// services
+import { fetchWidgets } from 'services/widget';
+
+// store
+import reducer from './reducer';
+import {
+  setDisplay,
+  setPagination,
+  setSearch,
+  setSort,
+  setWidgetState
+} from './actions';
+import initialState from './initial-state';
+
+// components
+import WidgetList from './component';
+
+// helpers
+import { getQueryParams } from './helpers';
+
+const WidgetListTabContainer = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const {
+    search,
+    sort,
+    pagination: { page }
+  } = state;
+  const {
+    subtab,
+    user: { token }
+  } = props;
+
+  const getWidgets = async () => {
+    const queryParams = getQueryParams(state, props);
+
+    dispatch(setWidgetState({
+      loading: true,
+      error: null
+    }));
+    await fetchWidgets(queryParams, { Authorization: token }, true)
+      .then(({ widgets, meta }) => {
+        const {
+          'total-pages': pages,
+          'total-items': size
+        } = meta;
+        const nextPagination = {
+          size,
+          pages
+        };
+
+        dispatch(setWidgetState({
+          loading: false,
+          list: widgets
+        }));
+
+        dispatch(setPagination(nextPagination));
+      })
+      .catch(({ message }) => {
+        dispatch(setWidgetState({
+          loading: false,
+          error: message
+        }));
+      });
+  };
+
+  useEffect(() => {
+    getWidgets();
+  }, [search, sort, page, subtab]);
+
+  return (
+    <WidgetList
+      {...state}
+      routes={{
+        index: 'myrw',
+        detail: 'myrw_detail'
+      }}
+      sideTab={subtab}
+      handlePageChange={(nextPage) => { dispatch(setPagination({ page: nextPage })); }}
+      handleDisplay={(display) => { dispatch(setDisplay(display)); }}
+      handleSortChange={() => {
+        const newSort = sort === 'asc' ? 'desc' : 'asc';
+        dispatch(setSort(newSort));
+      }}
+      handleWidgetRemoved={() => {
+        toastr.success('Success', 'Widget removed');
+        getWidgets();
+      }}
+      handleSearch={debounce((value) => {
+        if (value && value.length < 3) return;
+
+        dispatch(setSearch(value));
+        dispatch(setPagination({ page: 1 }));
+      }, 300)}
+      handleRefresh={() => { getWidgets(); }}
+    />);
+};
+
+export default connect(
+  state => ({
+    user: state.user,
+    subtab: state.routes.query.subtab
+  }),
+  null
+)(WidgetListTabContainer);
