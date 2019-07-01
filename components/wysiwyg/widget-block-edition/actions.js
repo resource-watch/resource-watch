8dee3fd2e4498a5a95fe2b2f4e0a34b1c6eac6be
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2019-07-01 16:17:32 +0200

    change actions format to comply with new syntax

--- components/wysiwyg/widget-block-edition/actions.js
+++ components/wysiwyg/widget-block-edition/actions.js
@@ -3,15 +3,25 @@ import queryString from 'query-string';
 import { createAction, createThunkAction } from 'redux-tools';
 
 // Actions
-export const setWidgets = createAction('WIDGET_BLOCK_EDITION_GET_WIDGETS');
-export const setLoading = createAction('WIDGET_BLOCK_EDITION_LOADING');
-export const setError = createAction('WIDGET_BLOCK_EDITION_ERROR');
-export const setTab = createAction('WIDGET_BLOCK_EDITION_TAB');
-export const setPage = createAction('WIDGET_BLOCK_EDITION_PAGE');
-export const setPages = createAction('WIDGET_BLOCK_EDITION_PAGES');
-export const setPageSize = createAction('WIDGET_BLOCK_EDITION_PAGE_SIZE');
-export const setTotal = createAction('WIDGET_BLOCK_EDITION_TOTAL');
-export const setSearch = createAction('WIDGET_BLOCK_EDITION_SEARCH');
+export const SET_WIDGETS = 'SET_WIDGETS';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_ERROR = 'SET_ERROR';
+export const SET_TAB = 'SET_TAB';
+export const SET_PAGE = 'SET_PAGE';
+export const SET_PAGES = 'SET_PAGES';
+export const SET_PAGE_SIZE = 'SET_PAGE_SIZE';
+export const SET_TOTAL = 'SET_TOTAL';
+export const SET_SEARCH = 'SET_SEARCH';
+
+export const setWidgets = createAction(SET_WIDGETS);
+export const setLoading = createAction(SET_LOADING);
+export const setError = createAction(SET_ERROR);
+export const setTab = createAction(SET_TAB);
+export const setPage = createAction(SET_PAGE);
+export const setPages = createAction(SET_PAGES);
+export const setPageSize = createAction(SET_PAGE_SIZE);
+export const setTotal = createAction(SET_TOTAL);
+export const setSearch = createAction(SET_SEARCH);
 
 
 // Async actions
