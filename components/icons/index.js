8d1615b8ff3d7f27fa127a05497955cc5226ba77
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2019-07-09 11:54:58 +0200

    use svgo to optimize SVGs

--- components/icons/index.js
+++ components/icons/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
 const Icons = () => (
-  <svg
-    aria-hidden="true"
-    style={{ position: 'absolute', width: 0, height: 0, overflow: 'hidden' }}
-  >
+  <svg aria-hidden="true" style={{ position: 'absolute', width: 0, height: 0, overflow: 'hidden' }}>
     <defs>
       <symbol id="icon-share-email" viewBox="0 0 43 32">
         <title>share-email</title>
         <path
           fill="#c32d7b"
-          style={{ fill: 'var(--color1, #c32d7b)' }}
-          d="M0.748 0h41.17l-20.585 16.468-20.585-16.468zM42.667 4.865v23.728l-8.24-10.259-3.326 2.672 8.83 10.994h-37.201l8.781-11.003-3.335-2.661-8.176 10.245v-23.715l21.333 17.067 21.333-17.067z"
+          d="M.748 0h41.17L21.333 16.468.748 0zm41.919 4.865v23.728l-8.24-10.259-3.326 2.672L39.931 32H2.73l8.781-11.003-3.335-2.661L0 28.581V4.866l21.333 17.067L42.666 4.866z"
         />
       </symbol>
     </defs>
