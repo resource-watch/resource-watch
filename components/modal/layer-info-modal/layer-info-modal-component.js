56bd07119527252fd1a907a9364a52b034e2d7b2
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2019-07-11 11:13:56 +0200

    Use slug instead of dataset id in LayerInfoModal component ;

--- components/modal/layer-info-modal/layer-info-modal-component.js
+++ components/modal/layer-info-modal/layer-info-modal-component.js
@@ -1,55 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'routes';
 
 // Redux
 import { connect } from 'react-redux';
 
-class LayerInfoModal extends React.Component {
-  static propTypes = {
-    layer: PropTypes.object,
-    embed: PropTypes.bool
-  };
+import { fetchDataset } from 'services/dataset';
 
-  render() {
-    const { embed, layer } = this.props;
+function LayerInfoModal(props) {
+  const { embed, layer } = props;
+  const [slug, setSlug] = useState(' ');
 
-    return (
-      <div className="layer-info-modal">
-        <div className="layer-info-content">
-          <h2>{layer.name}</h2>
-          <p>{layer.description}</p>
-          <div className="buttons">
-            {embed &&
-              <a
-                className="c-btn -primary"
-                href={`${window.location.origin}/data/explore/${layer.dataset}`}
-                target="_blank"
-              >
-                More info
-              </a>
-            }
+  useEffect(() => {
+    fetchDataset(layer.dataset)
+      .then((response) => {
+        setSlug(response.slug);
+      });
+  }, []);
 
-            {!embed &&
-              <Link
-                route="explore_detail"
-                params={{ id: layer.dataset }}
-              >
-                <a className="c-btn -primary">
-                  More info
-                </a>
-              </Link>
-            }
-          </div>
+  return (
+    <div className="layer-info-modal">
+      <div className="layer-info-content">
+        <h2>{layer.name}</h2>
+        <p>{layer.description}</p>
+        <div className="buttons">
+          {embed && (
+            <a
+              className="c-btn -primary"
+              href={`${window.location.origin}/data/explore/${layer.dataset}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              More info
+            </a>
+          )}
+
+          {!embed && (
+            <Link route="explore_detail" params={{ id: slug }}>
+              <a className="c-btn -primary">More info</a>
+            </Link>
+          )}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
+LayerInfoModal.propTypes = {
+  layer: PropTypes.object.isRequired,
+  embed: PropTypes.bool.isRequired
+};
+
 export default connect(
-  state => ({
-    embed: state.common.embed
-  }),
+  state => ({ embed: state.common.embed }),
   null
 )(LayerInfoModal);
