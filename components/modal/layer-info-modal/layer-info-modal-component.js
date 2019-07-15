b5e8f1c69be92a1012bd538ecbccdb6d8536139b
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2019-07-15 15:36:54 +0200

    unused property embed removed

--- components/modal/layer-info-modal/layer-info-modal-component.js
+++ components/modal/layer-info-modal/layer-info-modal-component.js
@@ -2,19 +2,16 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'routes';
 
-// Redux
-import { connect } from 'react-redux';
-
 import { fetchDataset } from 'services/dataset';
 
 function LayerInfoModal(props) {
-  const { embed, layer } = props;
+  const { layer } = props;
   const [slug, setSlug] = useState(' ');
 
   useEffect(() => {
     fetchDataset(layer.dataset)
-      .then((response) => {
-        setSlug(response.slug);
+      .then((dataset) => {
+        setSlug(dataset.slug);
       });
   }, []);
 
@@ -24,34 +21,15 @@ function LayerInfoModal(props) {
         <h2>{layer.name}</h2>
         <p>{layer.description}</p>
         <div className="c-button-container -j-end">
-          {embed && (
-            <a
-              className="c-btn -primary"
-              href={`${window.location.origin}/data/explore/${slug}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              More info
-            </a>
-          )}
-
-          {!embed && (
-            <Link route="explore_detail" params={{ id: slug }}>
-              <a className="c-btn -primary">More info</a>
-            </Link>
-          )}
+          <Link route="explore_detail" params={{ id: slug }}>
+            <a className="c-btn -primary">More info</a>
+          </Link>
         </div>
       </div>
     </div>
   );
 }
 
-LayerInfoModal.propTypes = {
-  layer: PropTypes.object.isRequired,
-  embed: PropTypes.bool.isRequired
-};
+LayerInfoModal.propTypes = { layer: PropTypes.object.isRequired };
 
-export default connect(
-  state => ({ embed: state.common.embed }),
-  null
-)(LayerInfoModal);
+export default LayerInfoModal;
