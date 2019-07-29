f833e6b22b5330ba5da9cb9ed3da2be4471d59e2
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2019-07-29 16:00:22 +0200

    use a different div for the markdown supported section

--- layout/explore-detail/explore-detail-info/explore-detail-info-component.js
+++ layout/explore-detail/explore-detail-info/explore-detail-info-component.js
@@ -12,11 +12,11 @@ import ShareModal from 'components/modal/share-modal';
 
 // Constants
 class ExploreDetailInfo extends PureComponent {
-  static propTypes = { dataset: PropTypes.object }
+  static propTypes = { dataset: PropTypes.object };
 
-  static defaultProps = { dataset: {} }
+  static defaultProps = { dataset: {} };
 
-  state = { showShareModal: false }
+  state = { showShareModal: false };
 
   getDatasetMetadata() {
     const { dataset } = this.props;
@@ -31,7 +31,7 @@ class ExploreDetailInfo extends PureComponent {
 
   handleToggleShareModal = (bool) => {
     this.setState({ showShareModal: bool });
-  }
+  };
 
   render() {
     const { dataset } = this.props;
@@ -39,7 +39,7 @@ class ExploreDetailInfo extends PureComponent {
     const datasetName = this.getDatasetName();
 
     return (
-      <div className="c-explore-detail-info c-markdown">
+      <div className="c-explore-detail-info">
         <div className="first-row-container">
           {metadata.info && metadata.info.technical_title ? (
             <div className="l-section-mod medium-7">
@@ -47,10 +47,7 @@ class ExploreDetailInfo extends PureComponent {
               <p>{metadata.info.technical_title}</p>
             </div>
           ) : null}
-          <button
-            className="c-button -secondary"
-            onClick={() => this.handleToggleShareModal(true)}
-          >
+          <button className="c-button -secondary" onClick={() => this.handleToggleShareModal(true)}>
             Share dataset
             <Modal
               isOpen={this.state.showShareModal}
@@ -69,114 +66,123 @@ class ExploreDetailInfo extends PureComponent {
             </Modal>
           </button>
         </div>
+        <div className="c-markdown">
+          {metadata.info && metadata.info.cautions && (
+            <div className="l-section-mod medium-7">
+              <h3>Cautions</h3>
+              <ReactMarkdown linkTarget="_blank" source={metadata.info.cautions} />
+            </div>
+          )}
 
-        {metadata.info && metadata.info.cautions &&
-          <div className="l-section-mod medium-7">
-            <h3>Cautions</h3>
-            <ReactMarkdown linkTarget="_blank" source={metadata.info.cautions} />
-          </div>}
+          {metadata.info && metadata.info.citation && (
+            <div className="l-section-mod medium-7">
+              <h3>Suggested citation</h3>
+              <ReactMarkdown linkTarget="_blank" source={metadata.info.citation} />
+            </div>
+          )}
 
-        {metadata.info && metadata.info.citation &&
-          <div className="l-section-mod medium-7">
-            <h3>Suggested citation</h3>
-            <ReactMarkdown linkTarget="_blank" source={metadata.info.citation} />
-          </div>}
+          {dataset && dataset.attributes && dataset.attributes.type && (
+            <div className="l-section-mod">
+              <h3>Data type</h3>
+              <ReactMarkdown linkTarget="_blank" source={dataset.attributes.type} />
+            </div>
+          )}
 
-        {dataset && dataset.attributes && dataset.attributes.type &&
-          <div className="l-section-mod">
-            <h3>Data type</h3>
-            <ReactMarkdown linkTarget="_blank" source={dataset.attributes.type} />
-          </div>}
+          {metadata.info && metadata.info.sources ? (
+            <div className="l-section-mod">
+              <h3>Sources</h3>
+              {metadata.info.sources.map(source => (
+                <div key={source['source-name']}>
+                  {source['source-name']}
+                  {source['source-description']}
+                </div>
+              ))}
+            </div>
+          ) : null}
 
-        {metadata.info && metadata.info.sources ? (
-          <div className="l-section-mod">
-            <h3>Sources</h3>
-            {metadata.info.sources.map(source => (
-              <div
-                key={source['source-name']}
-              >
-                {source['source-name']}
-                {source['source-description']}
+          <div className="l-section-mod row">
+            {metadata.info && metadata.info.geographic_coverage ? (
+              <div className="column small-6 medium-4 large-3">
+                <h3>Geographic coverage</h3>
+                <ReactMarkdown linkTarget="_blank" source={metadata.info.geographic_coverage} />
               </div>
-            ))}
+            ) : null}
+
+            {metadata.info && metadata.info.spatial_resolution ? (
+              <div className="column small-6 medium-4 large-3">
+                <h3>Spatial resolution</h3>
+                <ReactMarkdown linkTarget="_blank" source={metadata.info.spatial_resolution} />
+              </div>
+            ) : null}
+
+            {metadata.info && metadata.info.date_of_content ? (
+              <div className="column small-6 medium-4 large-3">
+                <h3>Date of content</h3>
+                <ReactMarkdown linkTarget="_blank" source={metadata.info.date_of_content} />
+              </div>
+            ) : null}
+
+            {metadata.info && metadata.info.frequency_of_updates ? (
+              <div className="column small-6 medium-4 large-3">
+                <h3>Frequency of updates</h3>
+                <ReactMarkdown linkTarget="_blank" source={metadata.info.frequency_of_updates} />
+              </div>
+            ) : null}
           </div>
-        ) : null}
 
-        <div className="l-section-mod row">
-          {metadata.info && metadata.info.geographic_coverage ? (
-            <div className="column small-6 medium-4 large-3">
-              <h3>Geographic coverage</h3>
-              <ReactMarkdown linkTarget="_blank" source={metadata.info.geographic_coverage} />
-            </div>
-          ) : null}
+          <div className="l-section-mod row">
+            {metadata.info && metadata.info.license ? (
+              <div className="column small-6 medium-4 large-3">
+                <h3>License</h3>
+                {!!metadata.info.license_link && (
+                  <p>
+                    <a href={metadata.info.license_link} target="_blank" rel="noopener noreferrer">
+                      {metadata.info.license}
+                    </a>
+                  </p>
+                )}
+                {!metadata.info.license_link && (
+                  <ReactMarkdown linkTarget="_blank" source={metadata.info.license} />
+                )}
+              </div>
+            ) : null}
 
-          {metadata.info && metadata.info.spatial_resolution ? (
-            <div className="column small-6 medium-4 large-3">
-              <h3>Spatial resolution</h3>
-              <ReactMarkdown linkTarget="_blank" source={metadata.info.spatial_resolution} />
-            </div>
-          ) : null}
+            {metadata.info && metadata.info.summary_of_license ? (
+              <div className="column small-6 medium-4 large-3">
+                <h3>Summary of license</h3>
+                <ReactMarkdown linkTarget="_blank" source={metadata.info.summary_of_license} />
+              </div>
+            ) : null}
 
-          {metadata.info && metadata.info.date_of_content ? (
-            <div className="column small-6 medium-4 large-3">
-              <h3>Date of content</h3>
-              <ReactMarkdown linkTarget="_blank" source={metadata.info.date_of_content} />
-            </div>
-          ) : null}
+            {metadata.info && metadata.info.link_to_license ? (
+              <div className="column small-6 medium-4 large-3">
+                <h3>Link to full license</h3>
+                <a href={metadata.info.link_to_license} target="_blank" rel="noopener noreferrer">
+                  {metadata.info.link_to_license}
+                </a>
+              </div>
+            ) : null}
 
-          {metadata.info && metadata.info.frequency_of_updates ? (
-            <div className="column small-6 medium-4 large-3">
-              <h3>Frequency of updates</h3>
-              <ReactMarkdown linkTarget="_blank" source={metadata.info.frequency_of_updates} />
+            {metadata && metadata.language ? (
+              <div className="column small-6 medium-4 large-3">
+                <h3>Published language</h3>
+                <ReactMarkdown linkTarget="_blank" source={metadata.language} />
+              </div>
+            ) : null}
+          </div>
+
+          {metadata.info &&
+          metadata.info.language &&
+          metadata.info.language.toLowerCase() !== 'en' ? (
+            <div className="l-section-mod">
+              <h3>Translated title</h3>
+              <ReactMarkdown
+                linkTarget="_blank"
+                source={metadata.info && metadata.info.translated_title}
+              />
             </div>
           ) : null}
         </div>
-
-        <div className="l-section-mod row">
-          {metadata.info && metadata.info.license ? (
-            <div className="column small-6 medium-4 large-3">
-              <h3>License</h3>
-              {!!metadata.info.license_link &&
-                <p>
-                  <a href={metadata.info.license_link} target="_blank" rel="noopener noreferrer">{metadata.info.license}</a>
-                </p>
-              }
-              {!metadata.info.license_link &&
-                <ReactMarkdown linkTarget="_blank" source={metadata.info.license} />
-              }
-            </div>
-          ) : null}
-
-          {metadata.info && metadata.info.summary_of_license ? (
-            <div className="column small-6 medium-4 large-3">
-              <h3>Summary of license</h3>
-              <ReactMarkdown linkTarget="_blank" source={metadata.info.summary_of_license} />
-            </div>
-          ) : null}
-
-          {metadata.info && metadata.info.link_to_license ? (
-            <div className="column small-6 medium-4 large-3">
-              <h3>Link to full license</h3>
-              <a href={metadata.info.link_to_license} target="_blank" rel="noopener noreferrer">
-                {metadata.info.link_to_license}
-              </a>
-            </div>
-          ) : null}
-
-          {metadata && metadata.language ? (
-            <div className="column small-6 medium-4 large-3">
-              <h3>Published language</h3>
-              <ReactMarkdown linkTarget="_blank" source={metadata.language} />
-            </div>
-          ) : null}
-        </div>
-
-        {metadata.info && metadata.info.language && metadata.info.language.toLowerCase() !== 'en' ? (
-          <div className="l-section-mod">
-            <h3>Translated title</h3>
-            <ReactMarkdown linkTarget="_blank" source={metadata.info && metadata.info.translated_title} />
-          </div>
-        ) : null}
       </div>
     );
   }
