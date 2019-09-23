703b7f56beb864d821bd99fc6d9e86f92fa62f7b
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2019-09-23 14:22:02 +0200

    filter out null fetches

--- components/modal/subscriptions-modal/actions.js
+++ components/modal/subscriptions-modal/actions.js
@@ -73,7 +73,7 @@ export const getUserSubscriptionsPreview = createThunkAction('SUBSCRIPTIONS__GET
       }
 
       return null;
-    });
+    }).filter(e => e !== null);
 
     dispatch(setSubscriptionsLoadingPreview(true));
 
