abbc68b15fa4d68c32003cb26b814e4fd0a3c363
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2019-10-08 15:18:52 +0200

    use POST instead of GET

--- services/contact-us.js
+++ services/contact-us.js
@@ -4,7 +4,7 @@ import { logger } from 'utils/logs';
 
 export const contactUs = (params) => {
   logger.info('Contact us');
-  return WRIAPI.get('contact-us', { params })
+  return WRIAPI.post('contact-us', params)
     .catch(({ response }) => {
       const { status, statusText } = response;
       logger.error(`Error with contact us endpoint: ${status}: ${statusText}`);
