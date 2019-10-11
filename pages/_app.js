17f686dcea7decd0c07ed23f5a70edc22a032d21
Andrés González <andres.gonzalez@vizzuality.com>
2019-10-11 12:35:18 +0200

    Revomes deprecated Container component

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import React from 'react';
 import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
@@ -88,11 +88,9 @@ class RWApp extends App {
     } = this.props;
 
     return (
-      <Container>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
-      </Container>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     );
   }
 }
