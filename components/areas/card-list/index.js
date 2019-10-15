d89617d78cceea78dbff20ecd1d03bea2dcc7ded
Andrés González <andres.gonzalez@vizzuality.com>
2019-10-15 11:17:21 +0200

    Rebases areas changes

--- components/areas/card-list/index.js
+++ components/areas/card-list/index.js
@@ -1,9 +1,1 @@
-import { connect } from 'react-redux';
-
-import AreaCardList from './component';
-
-
-export default connect(
-  state => ({ query: state.routes.query }),
-  null
-)(AreaCardList);
+export { default } from './component';
