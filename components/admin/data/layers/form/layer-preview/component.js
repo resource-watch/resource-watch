a5512b57ceb7f72b17983d808d40cec271807a70
Sergey Onufrienko <sergey@onufrienko.com>
2019-11-18 09:10:58 +0200

    Replace Leaflet with Mapbox component

--- components/admin/data/layers/form/layer-preview/component.js
+++ components/admin/data/layers/form/layer-preview/component.js
@@ -1,31 +1,18 @@
 import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import isEqual from 'react-fast-compare';
-import { LayerManager, Layer } from 'layer-manager/dist/components';
-import { PluginLeaflet } from 'layer-manager';
 import {
-  Map,
-  MapPopup,
-  MapControls,
-  ZoomControl,
   Legend,
   LegendListItem,
   LegendItemTypes
 } from 'vizzuality-components';
 
 // components
-import LayerPopup from 'components/ui/map/popup/LayerPopup';
+import Map from 'components/map';
+import LayerManager from 'components/map/layer-manager';
 
 // constants
-import { BASEMAPS, LABELS } from 'components/ui/map/constants';
-
-const MAP_CONFIG = {
-  zoom: 3,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-};
+import { MAPSTYLES, BASEMAPS, LABELS } from 'components/map/constants';
 
 class LayerPreviewComponent extends PureComponent {
   static propTypes = {
@@ -78,68 +65,18 @@ class LayerPreviewComponent extends PureComponent {
         <div className="map-container">
           <div className="c-map">
             <Map
-              mapOptions={MAP_CONFIG}
-              basemap={{
-                url: BASEMAPS.dark.value,
-                options: BASEMAPS.dark.options
-              }}
-              label={{
-                url: LABELS.light.value,
-                options: LABELS.light.options
-              }}
-              scrollZoomEnabled={false}
-              onReady={(map) => { this.map = map; }}
+              mapboxApiAccessToken={process.env.RW_MAPBOX_API_TOKEN}
+              mapStyle={MAPSTYLES}
+              basemap={BASEMAPS.dark.value}
+              labels={LABELS.light.value}
+              boundaries
             >
-              {map => (
+              {_map => (
                 <Fragment>
-                  {/* Controls */}
-                  <MapControls
-                    customClass="c-map-controls"
-                  >
-                    <ZoomControl map={map} />
-                  </MapControls>
-
-                  {/* Popup */}
-                  <MapPopup
-                    map={map}
-                    latlng={interactionLatLng}
-                    data={{
-                      layers,
-                      layersInteraction: interaction,
-                      layersInteractionSelected: interactionSelected
-                    }}
-                    onReady={(popup) => { this.popup = popup; }}
-                  >
-                    <LayerPopup
-                      onChangeInteractiveLayer={(selected) => {
-                        this.props.setLayerInteractionSelected(selected);
-                      }}
-                    />
-                  </MapPopup>
-
-                  {/* LayerManager */}
-                  <LayerManager map={map} plugin={PluginLeaflet}>
-                    {layers.map((l, i) => (
-                      <Layer
-                        {...l}
-                        key={l.id}
-                        opacity={l.opacity || 1}
-                        zIndex={1000 - i}
-
-                        // Interaction
-                        {...!!interactions && !!interactions.length && {
-                          interactivity: (l.provider === 'carto' || l.provider === 'cartodb') ? interactions.map(o => o.column) : true,
-                          params: { update: interactions.map(o => o.column) },
-                          events: {
-                            click: (e) => {
-                              setLayerInteraction({ id: l.id, data: e.data });
-                              setLayerInteractionLatLng(e.latlng);
-                            }
-                          }
-                        }}
-                      />
-                    ))}
-                  </LayerManager>
+                  <LayerManager
+                    map={_map}
+                    layers={layers}
+                  />
                 </Fragment>
               )}
             </Map>
