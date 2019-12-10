fee61895b57e24f9c884ec79f788d41ccb058b01
dancer2090 <ivaneichyk.ivan@gmail.com>
2019-12-10 13:57:41 +0200

    remove getStaticPage(topics) from topics page - bug on staging

--- pages/app/topics/index.js
+++ pages/app/topics/index.js
@@ -1,20 +1,9 @@
 import React, { PureComponent } from 'react';
 
-// actions
-import { getStaticPage } from 'modules/static-pages/actions';
-
 // components
 import Topics from 'layout/topics';
 
 class TopicsPage extends PureComponent {
-  static async getInitialProps({ store }) {
-    const { dispatch } = store;
-
-    await dispatch(getStaticPage('topics'));
-
-    return {};
-  }
-
   render() {
     return (<Topics />);
   }
