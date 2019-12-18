f5ab40c37ede9349573e2a0dd79176f4d3cb2bff
dancer2090 <ivaneichyk.ivan@gmail.com>
2019-12-18 16:43:44 +0200

    removed topic layout from dashbard-detail page

--- pages/app/dashboards-detail/index.js
+++ pages/app/dashboards-detail/index.js
@@ -1,36 +1,21 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import isEmpty from 'lodash/isEmpty';
-
 
 // actions
 import { getDashboard } from 'modules/dashboards/actions';
-import { getTopic, setSelected } from 'modules/topics/actions';
 
 // components
 import LayoutDashboardDetail from 'layout/app/dashboard-detail';
-import TopicDetailLayout from 'layout/app/topics-detail';
-import Error from 'pages/_error';
 
 class DashboardsDetailPage extends PureComponent {
-  static propTypes = {
-    topicsDetail: PropTypes.object.isRequired,
-    routes: PropTypes.object.isRequired,
-    setSelected: PropTypes.func.isRequired
-  };
+  static propTypes = { routes: PropTypes.object.isRequired };
 
   static async getInitialProps({ store }) {
     const { dispatch, getState } = store;
-    const { routes: { query: { slug, topic } } } = getState();
+    const { routes: { query: { slug } } } = getState();
 
-    if (topic) {
-      await dispatch(getTopic(slug));
-      // sets current topic in thumbnail list
-      dispatch(setSelected(slug));
-    } else {
-      await store.dispatch(getDashboard(slug));
-    }
+    await dispatch(getDashboard(slug));
 
     return { };
   }
@@ -41,27 +26,12 @@ class DashboardsDetailPage extends PureComponent {
     }
   }
 
-  componentWillUnmount() {
-    this.props.setSelected(null);
-  }
-
   render() {
-    const { routes: { query: { topic } } } = this.props;
-
-    if (topic) {
-      const { topicsDetail } = this.props;
-      if (isEmpty(topicsDetail)) return <Error statusCode={404} />;
-      return <TopicDetailLayout />;
-    }
-
     return <LayoutDashboardDetail />;
   }
 }
 
 export default connect(
-  state => ({
-    topicsDetail: state.topics.detail.data,
-    routes: state.routes
-  }),
-  { setSelected }
+  state => ({ routes: state.routes }),
+  null
 )(DashboardsDetailPage);
