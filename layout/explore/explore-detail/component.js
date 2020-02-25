4f2697b34a00ed134984ed43f509d4908634ff40
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-02-25 12:30:18 +0100

    source and date added to metadata page

--- layout/explore/explore-detail/component.js
+++ layout/explore/explore-detail/component.js
@@ -4,6 +4,11 @@ import PropTypes from 'prop-types';
 // Components
 import Spinner from 'components/ui/Spinner';
 import ReadMore from 'components/ui/ReadMore';
+
+// Utils
+import { getDateConsideringTimeZone } from 'utils/utils';
+
+// Explore detail components
 import ExploreDetailHeader from './explore-detail-header';
 import ExploreDetailFooter from './explore-detail-footer';
 import FurtherInformation from './further-information';
@@ -27,6 +32,8 @@ class ExploreDetailComponent extends React.Component {
     const { dataset, loading } = this.props;
     const metadata = dataset && dataset.metadata && dataset.metadata[0];
     const layers = dataset && dataset.layer;
+    const dateLastUpdated = getDateConsideringTimeZone(dataset && dataset.dataLastUpdated);
+
 
     return (
       <div className="c-explore-detail">
@@ -40,7 +47,12 @@ class ExploreDetailComponent extends React.Component {
                   <h2>{metadata.info && metadata.info.name}</h2>
                 </div>
                 <div className="source-date">
-                  {metadata.source}
+                  <div className="source" title={metadata.source} >
+                    {metadata.source}
+                  </div>
+                  <div className="date">
+                    {dateLastUpdated ? `UPDATED ON ${dateLastUpdated}`.toUpperCase() : ''}
+                  </div>
                 </div>
                 <div className="functions">
                   {metadata.info && metadata.info.functions}
@@ -54,10 +66,8 @@ class ExploreDetailComponent extends React.Component {
                   />
                 </div>
               </div>
-              <div id="layers" className="row">
-                <div className="column small-12">
-                  <DatasetLayers layers={layers} dataset={dataset} />
-                </div>
+              <div id="layers" className="metadata-section">
+                <DatasetLayers layers={layers} dataset={dataset} />
               </div>
               <div id="visualization" className="metadata-section">
                 <h3>Customize visualization</h3>
