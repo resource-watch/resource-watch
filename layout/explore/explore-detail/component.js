3d0bfa12608e3593b82578cf4cbcccecfd032022
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-02-26 10:50:41 +0100

    new icons added

--- layout/explore/explore-detail/component.js
+++ layout/explore/explore-detail/component.js
@@ -37,42 +37,52 @@ class ExploreDetailComponent extends React.Component {
           <Fragment>
             <ExploreDetailHeader dataset={dataset} />
             <div className="content">
-              <div id="overview" className="row">
+              <div className="row">
                 <div className="column small-12">
-                  <div className="title">
-                    <h2>{info && info.name}</h2>
+                  <div id="overview" className="explore-detail-section">
+                    <div className="title">
+                      <h2>{info && info.name}</h2>
+                    </div>
+                    <div className="functions">
+                      {info && info.functions}
+                    </div>
+                    <ExploreDetailButtons dataset={dataset} />
+                    <div className="description">
+                      <ReadMore
+                        markdown
+                        text={metadata.description}
+                        limitChar={DEFAULT_LIMIT_CHAR_FOR_METADATA_FIELDS}
+                      />
+                    </div>
                   </div>
-                  <div className="functions">
-                    {info && info.functions}
-                  </div>
-                  <ExploreDetailButtons dataset={dataset} />
-                  <div className="description">
-                    <ReadMore
-                      markdown
-                      text={metadata.description}
-                      limitChar={DEFAULT_LIMIT_CHAR_FOR_METADATA_FIELDS}
-                    />
+                </div>
+              </div>
+              <div className="row">
+                <div className="column small-12">
+                  <div id="layers" className="explore-detail-section">
+                    <DatasetLayers layers={layers} dataset={dataset} />
                   </div>
                 </div>
               </div>
-              <div id="layers" className="row">
+              <div className="row">
                 <div className="column small-12">
-                  <DatasetLayers layers={layers} dataset={dataset} />
+                  <div id="visualization" className="explore-detail-section">
+                    <h3>Customize visualization</h3>
+                  </div>
                 </div>
               </div>
-              <div id="visualization" className="row">
+              <div className="row">
                 <div className="column small-12">
-                  <h3>Customize visualization</h3>
+                  <div id="further_information" className="explore-detail-section">
+                    <FurtherInformation metadata={metadata} />
+                  </div>
                 </div>
               </div>
-              <div id="further_information" className="row">
+              <div className="row">
                 <div className="column small-12">
-                  <FurtherInformation metadata={metadata} />
-                </div>
-              </div>
-              <div id="related_content" className="row">
-                <div className="column small-12">
-                  <h3>Related content</h3>
+                  <div id="related_content" className="explore-detail-section">
+                    <h3>Related content</h3>
+                  </div>
                 </div>
               </div>
             </div>
