d0b76a467059996d42ddea1333fee45a6f39c345
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-03-04 09:37:11 +0100

    styles for topic buttons fixed

--- layout/explore/explore-topics/component.js
+++ layout/explore/explore-topics/component.js
@@ -7,6 +7,12 @@ import { TOPICS } from './constants';
 import './styles.scss';
 
 function ExploreTopicsComponent(props) {
+  const clickHandler = (id) => {
+    props.setFiltersSelected({ key: 'topics', list: [id] });
+    props.setDatasetsPage(1);
+    props.fetchDatasets();
+  };
+
   return (
     <div className="c-explore-topics">
       <div className="row">
@@ -15,26 +21,29 @@ function ExploreTopicsComponent(props) {
             <div
               id={topic.id}
               className="explore-topic-button"
-              style={{ backgroundImage: `url(${topic.backgroundURL}` }}
               role="button"
               tabIndex={0}
-              onClick={() => {
-                                props.setFiltersSelected({ key: 'topics', list: [topic.id] });
-                                props.setDatasetsPage(1);
-                                props.fetchDatasets();
-                            }}
+              onClick={() => clickHandler(topic.id)}
+              onKeyPress={() => clickHandler(topic.id)}
             >
+              <div className="topic-overlay" style={{ backgroundColor: topic.backgroundColor }} />
+              <div className="topic-gradient" />
+              <div className="topic-image" style={{ backgroundImage: `url(${topic.backgroundURL}` }} />
               <div className="topic-title">
                 {topic.label}
               </div>
             </div>
           </div>
-                ))}
+        ))}
       </div>
     </div>
   );
 }
 
-ExploreTopicsComponent.propTypes = { setFiltersSelected: PropTypes.func.isRequired };
+ExploreTopicsComponent.propTypes = {
+  setFiltersSelected: PropTypes.func.isRequired,
+  setDatasetsPage: PropTypes.func.isRequired,
+  fetchDatasets: PropTypes.func.isRequired
+};
 
 export default ExploreTopicsComponent;
--- layout/explore/explore-topics/constants.js
+++ layout/explore/explore-topics/constants.js
@@ -2,41 +2,49 @@ export const TOPICS = [
   {
     id: 'food_and_agriculture',
     label: 'Food and Agriculture',
-    backgroundURL: '/static/images/components/layout/explore/topic-food.jpg'
+    backgroundURL: '/static/images/components/layout/explore/topics-food.jpg',
+    backgroundColor: 'rgba(181,122,0,0.7)'
   },
   {
     id: 'energy',
     label: 'Energy',
-    backgroundURL: '/static/images/components/layout/explore/topic-energy.jpg'
+    backgroundURL: '/static/images/components/layout/explore/topics-energy.jpg',
+    backgroundColor: 'rgba(44,117,176,0.7)'
   },
   {
     id: 'climate',
     label: 'Climate',
-    backgroundURL: '/static/images/components/layout/explore/topic-climate.jpg'
+    backgroundURL: '/static/images/components/layout/explore/topics-climate.png',
+    backgroundColor: 'rgba(179,21,0,0.7)'
   },
   {
     id: 'forest',
     label: 'Forests',
-    backgroundURL: '/static/images/components/layout/explore/topic-forests.jpg'
+    backgroundURL: '/static/images/components/layout/explore/topics-forests.jpg',
+    backgroundColor: 'rgba(101,182,13,0.7)'
   },
   {
     id: 'water',
     label: 'Water',
-    backgroundURL: '/static/images/components/layout/explore/topic-water.jpg'
+    backgroundURL: '/static/images/components/layout/explore/topics-water.jpg',
+    backgroundColor: 'rgba(59,178,208,0.7)'
   },
   {
     id: 'society',
     label: 'Society',
-    backgroundURL: '/static/images/components/layout/explore/topic-society.jpg'
+    backgroundURL: '/static/images/components/layout/explore/topics-society.jpg',
+    backgroundColor: 'rgba(195,45,123,0.7)'
   },
   {
     id: 'ocean',
     label: 'Oceans',
-    backgroundURL: '/static/images/components/layout/explore/topic-oceans.jpg'
+    backgroundURL: '/static/images/components/layout/explore/topics-oceans.jpg',
+    backgroundColor: 'rgba(44,117,176,0.7)'
   },
   {
     id: 'urban',
     label: 'Cities',
-    backgroundURL: '/static/images/components/layout/explore/topic-cities.jpg'
+    backgroundURL: '/static/images/components/layout/explore/topics-cities.jpg',
+    backgroundColor: 'rgba(5,181,120,0.7)'
   }
 ];
