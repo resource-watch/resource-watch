1650842fc8e8b56a4f4455d26cc912af43045bde
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-03-20 10:06:44 +0100

    use axios instead of XMLHttpRequest

--- services/config.js
+++ services/config.js
@@ -1,18 +1,18 @@
+import axios from 'axios';
+
 // utils
 import { logger } from 'utils/logs';
 
-
 /**
- * Fetch RW config
+ * Fetch Explore config
  */
 export const fetchExploreConfig = () =>
   new Promise((resolve, reject) => {
     logger.info('Fetch RW config');
-    const xhr = new XMLHttpRequest();
-    xhr.open('get', 'https://raw.githubusercontent.com/resource-watch/resource-watch/develop/public/static/data/ExploreConfig.json');
-    xhr.onload = () => resolve(JSON.parse(xhr.response));
-    xhr.onerror = () => reject(new Error('There was an error loading the Explore config'));
-    xhr.send();
+
+    axios.get('https://raw.githubusercontent.com/resource-watch/resource-watch/develop/public/static/data/ExploreConfig.json')
+      .then(response => resolve(response.data))
+      .catch(error => reject(new Error('There was an error loading the Explore config', error)));
   });
 
 export default { fetchExploreConfig };
