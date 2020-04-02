17b8185aa6c41cf0f7a126f623cc3a7aaa71c9e3
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-04-02 16:09:40 +0200

    use a selector for the dataset layers

--- layout/explore/explore-detail/dataset-layers/component.js
+++ layout/explore/explore-detail/dataset-layers/component.js
@@ -8,40 +8,21 @@ import DatasetLayerCard from './dataset-layer-card';
 import './styles.scss';
 
 function DatasetLayers(props) {
-  const { layers, dataset, layerGroups } = props;
-  // Flag indicating if this layer group is of "timeline" type
-  const isATimelineDataset = layers.find(l => l.layerConfig.timeline);
-  // There exists a layer witht the new Mapbox implementation of the timeline for this
-  // In this case this layer should take precedence
-  const timelineDatasetLayer = layers.find(l => !!l.layerConfig.timeline_config);
-  // Layer group for the current dataset
-  const layerGroup = isATimelineDataset
-    && layerGroups.find(lg => lg.dataset === dataset.id);
-  // Active layer - if any
-  const activeLayerValue = layerGroup && layerGroup.layers.find(l => l.active);
-  // We need to use the layer from the "layers" property, instead of the layer object
-  // found in the Redux state
-  const activeLayer = activeLayerValue && layers.find(l => l.id === activeLayerValue.id);
-  // We select the first layer as the default one in the case the metadata section is
-  // open for a dataset that has a timeline with the "old approach" but there's no
-  // active layer at the moment.
-  const defaultTimelineLayer = !!isATimelineDataset && !activeLayer
-    && layers.length > 0 && layers[0];
-
+  const { layers, dataset, isATimeline, timelineLayerMapbox, timelineLayer } = props;
+  const showTimelineMapbox = isATimeline && !!timelineLayerMapbox;
+  const showTimelineOldApproach = isATimeline && !timelineLayerMapbox && timelineLayer;
+  
   return (
     <div className="c-dataset-layers">
       <h3>Dataset layers</h3>
       <div className="layers-container" >
-        {isATimelineDataset && !timelineDatasetLayer && activeLayer &&
-          <DatasetLayerCard layer={activeLayer} dataset={dataset} />
+        {showTimelineOldApproach &&
+          <DatasetLayerCard layer={timelineLayer} dataset={dataset} />
         }
-        {isATimelineDataset && !timelineDatasetLayer && !activeLayer &&
-          <DatasetLayerCard layer={defaultTimelineLayer} dataset={dataset} />
+        {showTimelineMapbox &&
+          <DatasetLayerCard layer={timelineLayerMapbox} dataset={dataset} />
         }
-        {isATimelineDataset && !!timelineDatasetLayer &&
-          <DatasetLayerCard layer={timelineDatasetLayer} dataset={dataset} />
-        }
-        {!isATimelineDataset && layers.map(layer => (
+        {!isATimeline && layers.map(layer => (
           <DatasetLayerCard layer={layer} dataset={dataset} />
           ))}
       </div>
@@ -52,7 +33,9 @@ function DatasetLayers(props) {
 DatasetLayers.propTypes = {
   layers: PropTypes.array.isRequired,
   dataset: PropTypes.object.isRequired,
-  layerGroups: PropTypes.object.isRequired
+  layerGroups: PropTypes.object.isRequired,
+  isATimeline: PropTypes.bool.isRequired,
+  timelineLayerMapbox: PropTypes.object.isRequired
 };
 
 export default DatasetLayers;
--- layout/explore/explore-detail/dataset-layers/index.js
+++ layout/explore/explore-detail/dataset-layers/index.js
@@ -3,7 +3,20 @@ import { connect } from 'react-redux';
 // component
 import DatasetLayersComponent from './component';
 
+// selectors
+import { 
+  isATimelineDataset,
+  getTimelineLayer,
+  getTimelineLayerMapbox
+} from './selectors';
+
 export default connect(
-  state => ({ layerGroups: state.explore.map.layerGroups }),
+  (state, props) => ({
+    isATimeline: isATimelineDataset(state, props),
+    // When there exists a layer witht the new Mapbox implementation of the timeline, this
+    // layer should take precedence
+    timelineLayerMapbox: getTimelineLayerMapbox(state, props),
+    timelineLayer: getTimelineLayer(state, props),
+  }),
   null
 )(DatasetLayersComponent);
