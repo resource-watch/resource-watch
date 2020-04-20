54e253cfbd0e2039ec51e2ede9b00778ba19196d
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-04-20 10:13:15 +0200

    Add GA events for ReadMore buttons click

--- components/ui/read-more/component.js
+++ components/ui/read-more/component.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import truncate from 'lodash/truncate';
 import ReactMarkdown from 'react-markdown';
@@ -7,57 +7,59 @@ import classnames from 'classnames';
 // Styles
 import './styles.scss';
 
-class ReadMore extends PureComponent {
-  static propTypes = {
-    text: PropTypes.string,
-    limitChar: PropTypes.number,
-    markdown: PropTypes.bool
-  };
+function ReadMore(props) {
+  const [visible, setVisible] = useState(false);
+  const { text, limitChar, markdown, readMoreClicked } = props;
 
-  static defaultProps = {
-    text: '',
-    limitChar: 1300,
-    markdown: false
-  };
-
-  state = { visible: false };
-
-  render() {
-    const { text, limitChar, markdown } = this.props;
-    const { visible } = this.state;
-
-    if (text.length <= limitChar) {
-      return (
-        <div className="c-read-more">
-          { markdown && <ReactMarkdown linkTarget="_blank" source={text} />}
-          { !markdown && <p>{text}</p> }
-        </div>
-      );
-    }
-
-    const shortenedText = (visible) ?
-      text :
-      truncate(text, { length: limitChar, separator: '', omission: '...' });
-    const classValue = classnames({
-      'c-read-more': true,
-      '-truncated': !visible
-    });
-
+  if (text.length <= limitChar) {
     return (
-      <div className={classValue}>
-        { markdown && <ReactMarkdown linkTarget="_blank" source={shortenedText} />}
-        { !markdown && <p>{shortenedText}</p> }
-        <div className="overlay" />
-        <button
-          className="c-button -clean"
-          onClick={() => this.setState({ visible: !visible })}
-        >
-          {visible ? 'Read less' : 'Read more'}
-        </button>
+      <div className="c-read-more">
+        {markdown && <ReactMarkdown linkTarget="_blank" source={text} />}
+        {!markdown && <p>{text}</p>}
       </div>
-
     );
   }
+
+  const shortenedText = (visible) ?
+    text :
+    truncate(text, { length: limitChar, separator: '', omission: '...' });
+  const classValue = classnames({
+    'c-read-more': true,
+    '-truncated': !visible
+  });
+
+  return (
+    <div className={classValue}>
+      {markdown && <ReactMarkdown linkTarget="_blank" source={shortenedText} />}
+      {!markdown && <p>{shortenedText}</p>}
+      <div className="overlay" />
+      <button
+        className="c-button -clean"
+        onClick={() => {
+          if (readMoreClicked && !visible) {
+            readMoreClicked();
+          }
+          setVisible(!visible);
+        }}
+      >
+        {visible ? 'Read less' : 'Read more'}
+      </button>
+    </div>
+  );
 }
+
+ReadMore.propTypes = {
+  text: PropTypes.string,
+  limitChar: PropTypes.number,
+  markdown: PropTypes.bool,
+  readMoreClicked: PropTypes.func
+};
+
+ReadMore.defaultProps = {
+  text: '',
+  limitChar: 1300,
+  markdown: false,
+  readMoreClicked: null
+};
 
 export default ReadMore;
