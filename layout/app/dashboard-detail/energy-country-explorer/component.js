47974a8a5e48cc33cbbf24cc506ee0c5728ca714
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-04-30 11:03:36 +0200

    minor updates to energy country explorer

--- layout/app/dashboard-detail/energy-country-explorer/component.js
+++ layout/app/dashboard-detail/energy-country-explorer/component.js
@@ -1,20 +1,33 @@
 import React, { useEffect, useState } from 'react';
 
+// Widget editor
+import Renderer from '@widget-editor/renderer';
+
 // Components
 import Spinner from 'components/ui/Spinner';
 import Select from 'react-select';
 
 // Services
 import { fetchCountries } from 'services/geostore';
+import { fetchWidget } from 'services/widget';
 
 // Styles
 import './styles.scss';
 
-function EnergyCountryExplorer(props) {
+// Constants
+import { ENERGY_COUNTRY_DASHBOARD_DATA } from './constants';
 
+function EnergyCountryExplorer(props) {
     const [countries, setCountries] = useState([]);
     const [loading, setLoading] = useState(true);
     const [selectedCountry, setSelectedCountry] = useState(null);
+    const [dashboardData, setDashboardData] = useState({
+        energy_profile: {
+            map: null,
+            loading: true
+        }
+    });
+    const { energy_profile } = dashboardData;
 
     useEffect(() => {
         fetchCountries()
@@ -28,9 +41,26 @@ function EnergyCountryExplorer(props) {
             });
     }, []);
 
+    useEffect(() => {
+        const energyProfileMapWidget = ENERGY_COUNTRY_DASHBOARD_DATA.energy_profile.map_widget;
+        fetchWidget(energyProfileMapWidget)
+            .then((data) => {
+                setDashboardData({ 
+                    ...dashboardData,
+                    energy_profile: {
+                        map: data,
+                        loading: false
+                    }
+                })
+            });
+    }, [selectedCountry]);
+
     const handleCountrySelected = (value) => {
         setSelectedCountry(value);
     };    
+
+    console.log('energy_profile', energy_profile);
+    
 
     return (
         <div className="c-energy-country-explorer">
@@ -41,6 +71,7 @@ function EnergyCountryExplorer(props) {
                     className="-fluid"
                     onChange={handleCountrySelected}
                     placeholder="Select a country"
+                    value={selectedCountry}
                 />
             </div>
             {selectedCountry &&
@@ -48,9 +79,16 @@ function EnergyCountryExplorer(props) {
                     <div className="country-container section">
                         <h2>{selectedCountry.label}</h2>
                     </div>
-                    <div className="section">
+                    <div className="section energy-profile">
                         <h3>Energy profile</h3>
                         <h5>Energy production, risks, and impacts</h5>
+                        <Spinner isLoading={energy_profile.loading} className="-light" />
+                        {energy_profile.map && 
+                            <div className="energy-profile-map">
+                                {/* <Renderer widgetConfig={energy_profile.map.widgetConfig} /> */}
+                                <iframe src="http://localhost:9000/embed/data/explore?section=All%20data&zoom=3&lat=44.192861132493526&lng=12.216796875010317&pitch=0&bearing=0&basemap=dark&labels=light&layers=%255B%257B%2522dataset%2522%253A%2522a86d906d-9862-4783-9e30-cdb68cd808b8%2522%252C%2522opacity%2522%253A1%252C%2522layer%2522%253A%25222a694289-fec9-4bfe-a6d2-56c3864ec349%2522%257D%255D&page=1&sort=most-viewed&sortDirection=-1" width="100%" height="500px" frameBorder="0" />
+                            </div>
+                        }
                     </div>
                     <div className="section">
                         <h3>Energy mix</h3>
--- layout/app/dashboard-detail/energy-country-explorer/constants.js
+++ layout/app/dashboard-detail/energy-country-explorer/constants.js
@@ -1,3 +1,5 @@
 export const ENERGY_COUNTRY_DASHBOARD_DATA = {
-
+    energy_profile: {
+        map_widget: '693772c2-6fe4-4488-b853-e551a2782de1'
+    }
 };
