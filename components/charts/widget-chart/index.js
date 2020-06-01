e094fe6436ae3769425a606de64d9652c1560a90
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-06-01 13:13:44 +0200

    minor fix for WidgetChart

--- components/charts/widget-chart/index.js
+++ components/charts/widget-chart/index.js
@@ -4,22 +4,16 @@ import PropTypes from 'prop-types';
 // Widget editor
 import Renderer from '@widget-editor/renderer';
 
-// Components
-import ErrorBoundary from 'components/ui/error-boundary';
-
-
 function WidgetChart(props) {
 
   const { thumbnail, widget } = props;
 
   return (
     <div className="c-widget-chart">
-      <ErrorBoundary message="There was an error loading the visualization">
-        <Renderer
-          widgetConfig={widget.widgetConfig}
-          thumbnail={thumbnail}
-        />
-      </ErrorBoundary>
+      <Renderer
+        widgetConfig={widget.widgetConfig}
+        thumbnail={thumbnail}
+      />
     </div>
   );
 }
