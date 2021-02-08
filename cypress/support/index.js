8f2fd731b226c5687061643922e4e9b9f8ddd067
Tiago Garcia <tiagojsag@gmail.com>
2021-02-08 16:53:39 +0100

    Config cypress code coverage

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,9 +15,10 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
+import '@cypress/code-coverage/support'
+
 // Import commands.js using ES2015 syntax:
 import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-import '@cypress/code-coverage/support'
--- utils/logs.js
+++ utils/logs.js
@@ -3,4 +3,6 @@ import pino from 'pino';
 // documentation: http://getpino.io/#/
 export const logger = pino({ level: process.env.LOGGER_LEVEL || 'info' });
 
+logger.info(`Setting logger level to ${logger.level}`);
+
 export default { logger };
