b6d6baa4fc3834a2a8f37af79d5fe0a8f3fc5a10
Tiago Garcia <tiagojsag@gmail.com>
2021-02-08 11:35:20 +0100

    Fix docker-compose for testing

--- utils/logs.js
+++ utils/logs.js
@@ -1,6 +1,6 @@
 import pino from 'pino';
 
 // documentation: http://getpino.io/#/
-export const logger = pino();
+export const logger = pino({ level: process.env.LOGGER_LEVEL || 'info' });
 
 export default { logger };
