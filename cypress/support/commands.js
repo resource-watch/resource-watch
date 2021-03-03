4f2f0774af3d0612708ba42a033daae66a178437
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2021-03-03 20:33:44 +0100

    adds dashboards tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+import { setUser } from '../../redactions/user';
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -10,7 +12,21 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
+Cypress.Commands.add("login", (callbackURL) => {
+  // visiting a page is mandatory in order to initialize the store
+  cy.visit('/sign-in');
+
+  cy.request({
+    method: 'POST',
+    url: '/local-sign-in'
+  })
+  .then((response) => {
+    if (response.status === 200) {
+      cy.window().its('store').invoke('dispatch', setUser(response.body))
+      if (callbackURL) cy.visit(callbackURL);
+    }
+  });
+});
 //
 //
 // -- This is a child command --
