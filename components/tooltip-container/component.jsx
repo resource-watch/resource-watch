e4efb1ea0ca7651e802b4daf040fccac91c08087
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2021-05-11 19:26:55 +0200

    countries call to action

---
+++ components/tooltip-container/component.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import './styles.scss';
+
+export default function TooltipContainer({
+  style,
+  children,
+  direction,
+}) {
+  return (
+    <div
+      className={classnames('c-tooltip-container', {
+        '-direction-down': direction === 'down',
+        '-direction-up': direction === 'up',
+      })}
+      style={style}
+    >
+      {children}
+    </div>
+  );
+}
+
+TooltipContainer.defaultProps = {
+  direction: 'down',
+  style: {},
+};
+
+TooltipContainer.propTypes = {
+  style: PropTypes.shape({}),
+  children: PropTypes.node.isRequired,
+  direction: PropTypes.oneOf([
+    'up',
+    'down',
+  ]),
+};
