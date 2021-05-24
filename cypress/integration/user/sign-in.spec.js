87df84f515756f6f38be95b44081a819ce261c1d
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2021-05-24 13:31:28 +0200

    fixes user sign-in tests

--- cypress/integration/user/sign-in.spec.js
+++ cypress/integration/user/sign-in.spec.js
@@ -1,54 +1,6 @@
-describe('Going to authenticated pages should redirect the user to sign-in page', () => {
-  it('a non-logged user goes to MyRW and gets redirected', () => {
-    cy.visit('/myrw');
-    cy.url().should('match', /sign-in/)
-  });
 
-  it('a non-logged user goes to /admin and gets redirected', () => {
-    cy.visit('/admin');
-    cy.url().should('match', /sign-in/)
-  });
-});
-
-describe('a user fills the login form', () => {
-  it('the user ignores the email format for the email field and an error displays', () => {
-    cy.visit('/sign-in');
-
-    cy.get('form').find('input[name="email"]').type('notAnEmail');
-    cy.get('p.error').should('contain', 'The field should be an email');
-  })
-
-  it('the user keeps empty the email field and a native error displays', () => {
-    cy.visit('/sign-in');
-
-    cy.get('form').find('input[name="email"]').type('{enter}');
-    cy.get('input[name="email"]:invalid').should("have.length", 1);
-  })
-
-  it('the user types something and then keeps empty the email field and an error displays', () => {
-    cy.visit('/sign-in');
-
-    cy.get('form').find('input[name="email"]').type('something').clear();
-    cy.get('p.error').first().should('contain', 'The field is required');
-  })
-
-  it('the user keeps empty the password field and a native error displays', () => {
-    cy.visit('/sign-in');
-
-    cy.get('form').find('input[name="password"]').type('{enter}');
-    cy.get('input[name="password"]:invalid').should("have.length", 1);
-  })
-
-  it('the user types something and then keeps empty the password field and an error displays', () => {
-    cy.visit('/sign-in');
-
-    cy.get('form').find('input[name="password"]').type('something').clear();
-    cy.get('p.error').first().should('contain', 'The field is required');
-  })
-});
-
-describe('a user logs in with user + password', () => {
-  before(() => {
+describe('', () => {
+  beforeEach(() => {
     cy.fixture('auth').then((authPayload) => {
       cy.intercept(
         {
@@ -58,11 +10,36 @@ describe('a user logs in with user + password', () => {
         authPayload,
       ).as('getAuthPayload');
     });
+
+    cy.visit('/sign-in');
   });
 
-  it('the user goes to sign-in page, fills the form and gets logged', () => {
-    cy.visit('/sign-in');
+  it('the user ignores the email format for the email field and an error displays', () => {
+    cy.get('form').find('input[name="email"]').type('notAnEmail');
+    cy.get('p.error').should('contain', 'The field should be an email');
+  })
 
+  it('the user keeps empty the email field and a native error displays', () => {
+    cy.get('form').find('input[name="email"]').type('{enter}');
+    cy.get('input[name="email"]:invalid').should("have.length", 1);
+  })
+
+  it('the user types something and then keeps empty the email field and an error displays', () => {
+    cy.get('form').find('input[name="email"]').type('something').clear();
+    cy.get('p.error').first().should('contain', 'The field is required');
+  })
+
+  it('the user keeps empty the password field and a native error displays', () => {
+    cy.get('form').find('input[name="password"]').type('{enter}');
+    cy.get('input[name="password"]:invalid').should("have.length", 1);
+  })
+
+  it('the user types something and then keeps empty the password field and an error displays', () => {
+    cy.get('form').find('input[name="password"]').type('something').clear();
+    cy.get('p.error').first().should('contain', 'The field is required');
+  })
+
+  it('the user goes to sign-in page, fills the form and gets logged', () => {
     cy.get('form').find('input[name="email"]').type('john@doe.com');
     cy.get('form').find('input[name="password"]').type('password1234');
     cy.get('form').find('button[type="submit"]').click();
