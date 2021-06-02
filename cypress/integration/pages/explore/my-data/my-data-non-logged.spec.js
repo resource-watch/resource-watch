59489c8222c0ef564cefccdb24521cdcce148d06
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2021-06-02 11:41:46 +0200

    updates tests

--- cypress/integration/pages/explore/my-data/my-data-non-logged.spec.js
+++ cypress/integration/pages/explore/my-data/my-data-non-logged.spec.js
@@ -15,23 +15,4 @@ describe('Explore – My Data fake door – Non logged user', () => {
       });
     });
   });
-
-  it ('a non logged user signs up successfully', () => {
-    cy.visit({
-      method: 'GET',
-      url: '/data/explore',
-    });
-
-    cy.get('div[data-cy=\'my-data-tab\']').then(($myDataTab) => {
-      expect($myDataTab).to.have.length(1);
-      $myDataTab.trigger('click');
-
-      cy.get('#my-data-sign-up').find('input[type="email"]').type('test@test.com');
-      cy.get('#my-data-sign-up').find('button[type="submit"]').click();
-
-      cy.get('.toastr')
-        .find('.rrt-text')
-        .should('have.text', 'You signed up successfully.');
-    });
-  });
 });
