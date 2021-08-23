9bccb5822624c10a22e99d948bcc5046d4e19ddb
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2021-08-23 18:57:34 +0200

    skips internal API calls from basic-auth

--- server/app.js
+++ server/app.js
@@ -39,7 +39,11 @@ server.disable('x-powered-by');
 
 function checkBasicAuth(credentials) {
   return function authMiddleware(req, res, nextAction) {
-    if (!/(AddSearchBot)|(HeadlessChrome)/.test(req.headers['user-agent'])) {
+    if (
+      !/(AddSearchBot)|(HeadlessChrome)/.test(req.headers['user-agent'])
+      // unblocks requests from the internal API
+      && !req.url.startsWith('/api')
+    ) {
       const user = basicAuth(req);
       let authorized = false;
       if (user && (user.name === credentials.name && user.pass === credentials.pass)) {
