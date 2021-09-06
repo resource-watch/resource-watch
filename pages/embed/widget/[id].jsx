ae9fb3ea84d6898bcf99ffbfb84c899a6c0ed08b
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2021-09-06 17:41:38 +0200

    refactors embed widget page/layout

--- pages/embed/widget/[id].jsx
+++ pages/embed/widget/[id].jsx
@@ -1,11 +1,9 @@
 // actions
-import { getWidget, checkIfFavorited } from 'redactions/widget';
-import { setEmbed, setWebshotMode } from 'redactions/common';
+import { getWidget } from 'redactions/widget';
 
 // hoc
 import {
   withRedux,
-  withUserServerSide,
 } from 'hoc/auth';
 
 // components
@@ -15,26 +13,18 @@ export default function EmbedWidgetPage(props) {
   return (<LayoutEmbedWidget {...props} />);
 }
 
-export const getServerSideProps = withRedux(withUserServerSide(async ({ store, query }) => {
-  const { dispatch, getState } = store;
-  const {
-    user,
-  } = getState();
+export const getServerSideProps = withRedux(async ({ store, query }) => {
+  const { dispatch } = store;
   const {
     id,
     webshot,
   } = query;
 
-  dispatch(setEmbed(true));
-  if (webshot) dispatch(setWebshotMode(true));
-
   await dispatch(getWidget(id, { includes: ['metadata'].join(',') }));
 
-  if (!webshot) {
-    if (user && user.id) dispatch(checkIfFavorited(id));
-  }
-
   return ({
-    props: ({}),
+    props: ({
+      webshot: Boolean(webshot),
+    }),
   });
-}));
+});
