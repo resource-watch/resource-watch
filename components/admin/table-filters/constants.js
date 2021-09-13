b5cf6c97bf0f72dbe0c326d2a55b222332c45146
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2021-09-13 16:31:56 +0200

    hotfix: replace User content filter by All content

--- components/admin/table-filters/constants.js
+++ components/admin/table-filters/constants.js
@@ -2,12 +2,13 @@ export const USER_TYPES = {
   ADMIN: 'ADMIN',
   USER: 'USER',
   MANAGER: 'MANAGER',
+  ALL: 'ALL',
 };
 
 export const USER_OPTIONS = [
   {
-    value: USER_TYPES.USER,
-    label: 'User content',
+    value: USER_TYPES.ALL,
+    label: 'All content',
   },
   {
     value: USER_TYPES.ADMIN,
