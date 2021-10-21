91fd56fc02b3525e3d0aa201ced6d7d4e965729f
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2021-10-21 12:21:27 +0200

    improvements to new Coral reef dashboard

---
+++ layout/layout/coral-reefs-dashboard/constants.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line import/prefer-default-export
+export const TABS = [
+  {
+    label: 'Value',
+    value: 'value',
+  },
+  {
+    label: 'Threats',
+    value: 'reefs-are-threatened',
+  },
+  {
+    label: 'Condition',
+    value: 'coral-reef-condition',
+  },
+  {
+    label: 'Management & Resilience',
+    value: 'management-resilience-and-other-mitigating-factors',
+  },
+  {
+    label: 'Social Vulnerability',
+    value: 'social-and-economic-vulnerability',
+  },
+  {
+    label: 'Conclusion',
+    value: 'conclusion',
+  },
+];
