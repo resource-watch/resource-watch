4615aff2db78a68d78a0828f940949a9bdff7aca
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2021-10-25 12:35:16 +0200

    progress on key resources section

--- layout/layout/coral-reefs-dashboard/key-resources-section/component.jsx
+++ layout/layout/coral-reefs-dashboard/key-resources-section/component.jsx
@@ -10,12 +10,14 @@ export default function KeyResourcesSection() {
             The
             {' '}
             <a href="https://reefresilience.org/" rel="noopener noreferrer" target="_blank">Reef Resilience Network</a>
+            {' '}
             consolidates information on science and management approaches for coral reefs.&nbsp;
             <a href="https://reefresilience.org/resilience/" rel="noopener noreferrer" target="_blank">Resilience-based management</a>
             is an important management approach which addresses local threats within the context of global threats and the stakeholders / users of resources.
           </li>
           <li>
             <a href="https://coralreefwatch.noaa.gov/" rel="noopener noreferrer" target="_blank">NOAA Coral Reef Watch</a>
+            {' '}
             provides near real-time data on sea surface temperature and modeled outlooks of bleaching risk.
           </li>
           <li>
@@ -36,6 +38,115 @@ export default function KeyResourcesSection() {
             brings together Australia's leading experts to create an innovative suite of safe, acceptable interventions to help the Great Barrier Reef resist, adapt to, and recover from the impacts of climate change.
           </li>
         </ul>
+      </div>
+      <div className="subsection">
+        <h2><strong>Protected Areas</strong></h2>
+        <ul className="bullet-list">
+          <li>
+            {' '}
+            <a href="https://lmmanetwork.org/" rel="noopener noreferrer" target="_blank">Locally Managed Marine Area (LMMA)</a>
+            <span> is an international network of natural resource management practitioners who share knowledge on best practices </span>
+          </li>
+          <li>
+            <a href="https://www.protectedplanet.net/en/thematic-areas/marine-protected-areas" rel="noopener noreferrer" target="_blank">Protect Planet</a>
+            {' '}
+            provides data MPAs updated monthly with submissions from governments, non-governmental organizations, landowners and communities.&nbsp;&nbsp;&nbsp;
+          </li>
+          <li>
+            {' '}
+            <a href="https://mpatlas.org/" rel="noopener noreferrer" target="_blank">Marine Protection Atlas (MPAtlas)</a>
+            {' '}
+            augments spatial data on MPAs to attempt to distinguish MPAs by implementation status and by protection level (such as no-take, fully, and highly protected areas). This ongoing effort (updated monthly) uses (soon to be published), scientifically-based criteria for measuring an MPA’s protection level and associated conservation outcomes.
+          </li>
+          <li>
+            {' '}
+            <a href="https://habitats.oceanplus.org/#coralreef" rel="noopener noreferrer" target="_blank">Ocean + Habitats</a>
+            {' '}
+            is an evolving tool that provides insight into the known extent, protection and other statistics of ecologically and economically important ocean habitats, such as corals, mangroves, seagrasses and saltmarshes.
+          </li>
+        </ul>
+        <div className="row subsection">
+          <div className="image-container column small-12 medium-6">
+            <div>
+              <img src="https://s3.amazonaws.com/wri-api-backups/resourcewatch/staging/temporary_content_images/images/000/000/393/original/CoralReefImageBank_Pacific_HannesKlostermann_03.jpg?1611951799&amp;temp_id=393" alt="CoralReefImageBank_Pacific_HannesKlostermann_03.jpg" />
+            </div>
+            <div>
+              <em>
+                Photo: Hannes Klostermann - Coral Reef Image Bank
+              </em>
+            </div>
+          </div>
+          <div className="image-container column small-12 medium-6">
+            <div>
+              <img src="https://s3.amazonaws.com/wri-api-backups/resourcewatch/staging/temporary_content_images/images/000/000/394/original/IYORBank_ReefAnimals_TheOceanAgency_06.jpg?1611951909&amp;temp_id=394" alt="IYORBank_ReefAnimals_TheOceanAgency_06.jpg" />
+            </div>
+            <div>
+              <em>
+                Photo: The Ocean Agency - Coral Reef Image Bank
+
+              </em>
+            </div>
+          </div>
+        </div>
+        <div className="subsection">
+          <h2><strong>Status and Trends</strong></h2>
+          <ul className="bullet-list">
+            <li>
+              {' '}
+              <a href="https://gcrmn.net/" rel="noopener noreferrer" target="_blank">Global Coral Reef Monitoring Network (GCRMN</a>
+              ) works through a global network of researchers&nbsp;to&nbsp;provide the best available scientific information on the health of coral reef ecosystems, for their conservation and management.
+            </li>
+            <li>
+              <a href="https://www.aims.gov.au/" rel="noopener noreferrer" target="_blank">Australian Institute of Marine Sciences (AIMS)</a>
+              {' '}
+              monitors the condition of coral reefs throughout Australia, including the Great Barrier Reef.
+            </li>
+            <li>
+              <a href="https://www.agrra.org/" rel="noopener noreferrer" target="_blank">Atlantic and Gulf Rapid Reef Assessment (AGGRA</a>
+              ) uses a standardized method to conduct surveys of coral reef health across the Western Atlantic / Caribbean and Gulf of Mexico.
+            </li>
+            <li>
+              <a href="http://www.cordio.org/" rel="noopener noreferrer" target="_blank">Coral Reef Degradation in the Indian Ocean (CORDIO)</a>
+              {' '}
+              is a collaborative program involving researchers in 11 countries in the central and western Indian Ocean focused on assessing degradation of coral reefs, mitigating damage to&nbsp;reefs&nbsp;and promoting alternative livelihoods for people dependent on&nbsp;reefs.
+            </li>
+            <li>
+              <a href="https://www.healthyreefs.org/cms/report-cards/" rel="noopener noreferrer" target="_blank">Healthy Reefs</a>
+              {' '}
+              for Healthy People has developed indicators of reef health and published a series of report cards for the Mesoamerican Reef.
+            </li>
+            <li>
+              {' '}
+              <a href="https://www.livingoceansfoundation.org/global-reef-expedition/" rel="noopener noreferrer" target="_blank">Khaled bin Sultan Living Oceans Foundation</a>
+              {' '}
+              recently
+              {' '}
+              <span>completed field research for one of the largest coral reef studies in history – the Global Reef Expedition – to map and characterize coral reef ecosystems, identify their current status and major threats, and examine factors influencing resilience. </span>
+            </li>
+            <li>
+              <a href="https://www.spc.int/resource-centre" rel="noopener noreferrer" target="_blank">The Pacific Community (SPC)</a>
+              <span> is the principal scientific and technical organization in</span>
+            </li>
+            <li>
+              <span>the Pacific region - governed by its 26 country and territory members. Its </span>
+              <a href="https://pacificdata.org/" rel="noopener noreferrer" target="_blank">Pacific Data Hub</a>
+              <span> includes an ocean portal focused on </span>
+              <a href="http://oceanportal.spc.int/portal/app.html#coral" rel="noopener noreferrer" target="_blank">coral reefs</a>
+              <span>.&nbsp;</span>
+            </li>
+            <li>
+              {' '}
+              <a href="https://www.coris.noaa.gov/monitoring/status_report/" rel="noopener noreferrer" target="_blank">US Coral Reef Information System (CORIS)</a>
+              {' '}
+              <span>provides national coral reef monitoring and status reports for Atlantic and Pacific reefs.</span>
+            </li>
+            <li>
+              {' '}
+              <a href="https://www.wiomsa.org/" rel="noopener noreferrer" target="_blank">Western Indian Ocean Marine Science Association (WIOMSA)&nbsp;</a>
+              <span>is dedicated to promoting the educational, scientific, and technological development of all aspects of coastal and marine sciences (including socioeconomic and management sciences) and to support sustainable development in the Western Indian Ocean region.&nbsp;</span>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
