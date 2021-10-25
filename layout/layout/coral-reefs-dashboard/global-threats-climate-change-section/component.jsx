82b340f10cbd16130ae8e0878f128a5cf62a7e3d
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2021-10-25 17:41:47 +0200

    progress on Global threats section

--- layout/layout/coral-reefs-dashboard/global-threats-climate-change-section/component.jsx
+++ layout/layout/coral-reefs-dashboard/global-threats-climate-change-section/component.jsx
@@ -36,6 +36,49 @@ const GlobalThreatsClimateChangeSection = ({ onShareWidget }) => (
           {' '}
           <strong>Between 1985 and 2018, 87.4% of the world’s reefs experienced bleaching level heat stress, while 42.5% underwent severe heat stress.</strong>
         </p>
+        <MapWidget
+          widgetId="641d5218-be38-4a9a-ba6c-a2c38f084b65"
+          onToggleShare={onShareWidget}
+          style={{
+            height: 550,
+            borderRadius: 4,
+          }}
+        />
+      </div>
+      <div className="subsection">
+        <h2>Current SST and Bleaching Risk</h2>
+        <p>
+          <strong>Bleaching-level conditions occur when the SST exceeds the normal high temperature (for the hottest month) for an extended period (usually several weeks).</strong>
+          {' '}
+          This combines deviation from the normal high and duration.
+          <span> </span>
+          The U.S. National Oceanic and Atmospheric Administration (NOAA)
+          {' '}
+          <a href="https://coralreefwatch.noaa.gov/" rel="noopener noreferrer" target="_blank">Coral Reef Watch (CRW)</a>
+          <span> </span>
+          program has developed a daily global 5km resolution product to monitor thermal stress on coral reefs and alert coral reef managers to impending threats.
+        </p>
+        <MapWidget
+          widgetId="9ea5466d-47f3-4f80-b3bf-d7a54be5d3ba"
+          onToggleShare={onShareWidget}
+          style={{
+            height: 600,
+            borderRadius: 4,
+          }}
+        />
+      </div>
+      <div className="subsection">
+        <h2>Projections of SST and Bleaching Risk</h2>
+        <p>
+          As greenhouse gas emissions continue to rise, the atmosphere and ocean will continue to warm. Projections suggest that
+          {' '}
+          <a href="https://wedocs.unep.org/bitstream/handle/20.500.11822/22048/Coral_Bleaching_Futures.pdf?sequence=1&amp;isAllowed=y" rel="noopener noreferrer" target="_blank"><strong>severe bleaching will occur with increasing frequency in the decades ahead, and virtually all reefs will experience severe bleaching annually by the end of the century</strong></a>
+          <strong>.</strong>
+&nbsp;The time between bleaching events is critical for coral recovery. When
+          {' '}
+          <a href="https://coralreefwatch.noaa.gov/climate/projections/downscaled_bleaching_4km/index.php" rel="noopener noreferrer" target="_blank">bleaching exceeds twice per decade corals are less likely to recover</a>
+          , resulting in increased mortality and reduced structural complexity. These changes will become more sever when bleaching events begin to occur annually. Hence, the continued emission of greenhouse gases gives rise to a dire outlook for coral reefs.
+        </p>
       </div>
     </div>
   </div>
