856da32ab6eb182980c267eb76f2ce3545843b0a
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2022-01-11 17:53:31 +0100

    removes Leaflet leftovers

--- components/areas/AlertWidget.js
+++ components/areas/AlertWidget.js
@@ -1,335 +1,336 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import debounce from 'lodash/debounce';
+// todo: alerts need to be deeply reviewed as they are quite outdated.
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import debounce from 'lodash/debounce';
 
-// Redux
-import { connect } from 'react-redux';
+// // Redux
+// import { connect } from 'react-redux';
 
-// Selectors
-import areaAlerts from 'selectors/user/areaAlerts';
+// // Selectors
+// import areaAlerts from 'selectors/user/areaAlerts';
 
-// Components
-import ShareControls from 'components/map/controls/share';
-import BasemapControl from 'components/ui/map/controls/BasemapControl';
-import { BASEMAPS, LABELS } from 'components/ui/map/constants';
-import DataTable from 'components/ui/DataTable';
+// // Components
+// import ShareControls from 'components/map/controls/share';
+// import BasemapControl from 'components/ui/map/controls/BasemapControl';
+// import { BASEMAPS, LABELS } from 'components/ui/map/constants';
+// import DataTable from 'components/ui/DataTable';
 
-// Modal
-import Modal from 'components/modal/modal-component';
-// import AreaSubscriptionModal from 'components/modal/AreaSubscriptionModal';
-import LayerInfoModal from 'components/modal/layer-info-modal';
+// // Modal
+// import Modal from 'components/modal/modal-component';
+// // import AreaSubscriptionModal from 'components/modal/AreaSubscriptionModal';
+// import LayerInfoModal from 'components/modal/layer-info-modal';
 
-// Services
-import { fetchGeostore } from 'services/geostore';
+// // Services
+// import { fetchGeostore } from 'services/geostore';
 
-// Utils
-import { LayerManager, Layer } from 'layer-manager/dist/components';
-import { PluginLeaflet } from 'layer-manager';
+// // Utils
+// import { LayerManager, Layer } from 'layer-manager/dist/components';
+// import { PluginLeaflet } from 'layer-manager';
 
-// WRI components
-import {
-  Map,
-  MapControls,
-  ZoomControl,
-  Legend,
-  LegendListItem,
-  LegendItemToolbar,
-  LegendItemButtonInfo,
-  LegendItemTypes,
-} from 'vizzuality-components';
+// // WRI components
+// import {
+//   Map,
+//   MapControls,
+//   ZoomControl,
+//   Legend,
+//   LegendListItem,
+//   LegendItemToolbar,
+//   LegendItemButtonInfo,
+//   LegendItemTypes,
+// } from 'vizzuality-components';
 
-class AlertWidget extends React.Component {
-  constructor(props) {
-    super(props);
-    const {
-      dataset,
-      subscriptionData,
-      user,
-      id,
-    } = props;
+// class AlertWidget extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     const {
+//       dataset,
+//       subscriptionData,
+//       user,
+//       id,
+//     } = props;
 
-    const { areas } = user;
+//     const { areas } = user;
 
-    this.state = {
-      area: areas.items.find((a) => a.id === id),
-      alertTable: null,
-      subscriptionData,
-      modalOpen: false,
-      zoom: 0,
-      geostore: null,
-      layer: null,
-      latLng: {
-        lat: 0,
-        lng: 0,
-      },
-      layerGroups: [{
-        dataset: dataset ? dataset.id : null,
-        visible: true,
-        layers: dataset ? dataset.layer.map((d, k) => ({
-          active: k === 0,
-          id: d.id,
-          name: d.name,
-          layerConfig: d.layerConfig,
-          legendConfig: d.legendConfig,
-          provider: d.provider,
-        })) : null,
-      }],
-    };
-  }
+//     this.state = {
+//       area: areas.items.find((a) => a.id === id),
+//       alertTable: null,
+//       subscriptionData,
+//       modalOpen: false,
+//       zoom: 0,
+//       geostore: null,
+//       layer: null,
+//       latLng: {
+//         lat: 0,
+//         lng: 0,
+//       },
+//       layerGroups: [{
+//         dataset: dataset ? dataset.id : null,
+//         visible: true,
+//         layers: dataset ? dataset.layer.map((d, k) => ({
+//           active: k === 0,
+//           id: d.id,
+//           name: d.name,
+//           layerConfig: d.layerConfig,
+//           legendConfig: d.legendConfig,
+//           provider: d.provider,
+//         })) : null,
+//       }],
+//     };
+//   }
 
-  componentDidMount() {
-    const { area, layerGroups } = this.state;
+//   componentDidMount() {
+//     const { area, layerGroups } = this.state;
 
-    getGeostore(area.attributes.geostore)
-      .then(({ data }) => {
-        const geostore = { ...data.attributes, id: data.id, type: data.type };
+//     getGeostore(area.attributes.geostore)
+//       .then(({ data }) => {
+//         const geostore = { ...data.attributes, id: data.id, type: data.type };
 
-        this.setState({ geostore });
-      });
-  }
+//         this.setState({ geostore });
+//       });
+//   }
 
-  UNSAFE_componentWillUpdate(nextProps) {
-    if ((nextProps.subscriptionData !== this.state.subscriptionData) && !this.state.alertTable) {
-      this.getAlertHistory(nextProps).then((table) => this.setState({ alertTable: table }));
-    }
-  }
+//   UNSAFE_componentWillUpdate(nextProps) {
+//     if ((nextProps.subscriptionData !== this.state.subscriptionData) && !this.state.alertTable) {
+//       this.getAlertHistory(nextProps).then((table) => this.setState({ alertTable: table }));
+//     }
+//   }
 
-  // Map params
-  onMapParams = debounce(({ zoom, latLng }) => {
-    this.setState({ zoom, latLng });
-  }, 250);
+//   // Map params
+//   onMapParams = debounce(({ zoom, latLng }) => {
+//     this.setState({ zoom, latLng });
+//   }, 250);
 
-  onChangeLayer = (l) => {
-    const { layerGroups } = this.state;
+//   onChangeLayer = (l) => {
+//     const { layerGroups } = this.state;
 
-    const layers = layerGroups.length && 'layers' in layerGroups[0]
-      ? layerGroups[0].layers.map((layer) => {
-        layer.active = layer.id === l.id;
-        return layer;
-      }) : [];
+//     const layers = layerGroups.length && 'layers' in layerGroups[0]
+//       ? layerGroups[0].layers.map((layer) => {
+//         layer.active = layer.id === l.id;
+//         return layer;
+//       }) : [];
 
-    layerGroups[0] = { ...layerGroups[0], ...layers };
-    this.setState({ layerGroups });
-  }
+//     layerGroups[0] = { ...layerGroups[0], ...layers };
+//     this.setState({ layerGroups });
+//   }
 
-  onChangeInfo = (layer) => {
-    if (layer) {
-      layer = { ...layer, dataset: this.props.dataset.id };
-    }
+//   onChangeInfo = (layer) => {
+//     if (layer) {
+//       layer = { ...layer, dataset: this.props.dataset.id };
+//     }
 
-    this.setState({ layer });
-  }
+//     this.setState({ layer });
+//   }
 
-  // XXX: this is a temporary fix
-  // currently the structure from the alerts return <uid>-<title>
-  // This abstracts the two out of the string
-  getAlertInfoFromKey(key) {
-    return {
-      id: key.substr(0, 36),
-      title: key.substring(37),
-    };
-  }
+//   // XXX: this is a temporary fix
+//   // currently the structure from the alerts return <uid>-<title>
+//   // This abstracts the two out of the string
+//   getAlertInfoFromKey(key) {
+//     return {
+//       id: key.substr(0, 36),
+//       title: key.substring(37),
+//     };
+//   }
 
-  getAlertHistory(props) {
-    const { subscriptionData, dataset } = props;
-    const layer = dataset ? dataset.layer.find((l) => l.default) : null;
+//   getAlertHistory(props) {
+//     const { subscriptionData, dataset } = props;
+//     const layer = dataset ? dataset.layer.find((l) => l.default) : null;
 
-    const o = {
-      columns: [],
-      data: [],
-    };
+//     const o = {
+//       columns: [],
+//       data: [],
+//     };
 
-    return new Promise((resolve) => {
-      if (subscriptionData && subscriptionData.length) {
-        subscriptionData.forEach((sub) => {
-          Object.keys(sub).forEach((subKey) => {
-            const { id } = this.getAlertInfoFromKey(subKey);
-            if (id === layer.dataset && sub[subKey].length) {
-              o.columns = Object.keys(sub[subKey][0]);
-              sub[subKey].forEach((item, key) => {
-                if (key < 11) {
-                  o.data.push(Object.values(item));
-                }
-              });
-              resolve(o);
-            }
-          });
-        });
-      }
-    });
-  }
+//     return new Promise((resolve) => {
+//       if (subscriptionData && subscriptionData.length) {
+//         subscriptionData.forEach((sub) => {
+//           Object.keys(sub).forEach((subKey) => {
+//             const { id } = this.getAlertInfoFromKey(subKey);
+//             if (id === layer.dataset && sub[subKey].length) {
+//               o.columns = Object.keys(sub[subKey][0]);
+//               sub[subKey].forEach((item, key) => {
+//                 if (key < 11) {
+//                   o.data.push(Object.values(item));
+//                 }
+//               });
+//               resolve(o);
+//             }
+//           });
+//         });
+//       }
+//     });
+//   }
 
-  handleEditSubscription(modalOpen = true) {
-    this.setState({ modalOpen });
-  }
+//   handleEditSubscription(modalOpen = true) {
+//     this.setState({ modalOpen });
+//   }
 
-  render() {
-    const { dataset } = this.props;
-    const layer = dataset ? dataset.layer.find((l) => l.default) : null;
-    const {
-      zoom, latLng, layerGroups, alertTable, geostore,
-    } = this.state;
+//   render() {
+//     const { dataset } = this.props;
+//     const layer = dataset ? dataset.layer.find((l) => l.default) : null;
+//     const {
+//       zoom, latLng, layerGroups, alertTable, geostore,
+//     } = this.state;
 
-    return (
-      <div className="c-alerts-page__widget">
-        <div className="c-alerts-page__widget--header">
-          <h2 className="c-alerts-page__widget--heading">{layer && layer.name ? layer.name : 'not defined'}</h2>
-          <button
-            className="c-btn -b"
-            onClick={() => this.handleEditSubscription(true)}
-          >
-            Edit Subscriptions
-          </button>
-        </div>
+//     return (
+//       <div className="c-alerts-page__widget">
+//         <div className="c-alerts-page__widget--header">
+//           <h2 className="c-alerts-page__widget--heading">{layer && layer.name ? layer.name : 'not defined'}</h2>
+//           <button
+//             className="c-btn -b"
+//             onClick={() => this.handleEditSubscription(true)}
+//           >
+//             Edit Subscriptions
+//           </button>
+//         </div>
 
-        {layer
-          && (
-          <div className="c-alerts-page__graph">
-            <Map
-              mapConfig={{ zoom, latLng }}
-              onMapParams={this.onMapParams}
-              onReady={(map) => { this.map = map; }}
-              basemap={{
-                url: BASEMAPS.dark.value,
-                options: BASEMAPS.dark.options,
-              }}
-              label={{
-                url: LABELS.light.value,
-                options: LABELS.light.options,
-              }}
-              {...geostore && {
-                bounds: {
-                  bbox: geostore.bbox,
-                  options: { padding: [20, 20] },
-                },
-              }}
-            >
-              {(map) => (
-                <>
-                  <MapControls
-                    customClass="c-map-controls"
-                  >
-                    <ZoomControl map={map} />
+//         {layer
+//           && (
+//           <div className="c-alerts-page__graph">
+//             <Map
+//               mapConfig={{ zoom, latLng }}
+//               onMapParams={this.onMapParams}
+//               onReady={(map) => { this.map = map; }}
+//               basemap={{
+//                 url: BASEMAPS.dark.value,
+//                 options: BASEMAPS.dark.options,
+//               }}
+//               label={{
+//                 url: LABELS.light.value,
+//                 options: LABELS.light.options,
+//               }}
+//               {...geostore && {
+//                 bounds: {
+//                   bbox: geostore.bbox,
+//                   options: { padding: [20, 20] },
+//                 },
+//               }}
+//             >
+//               {(map) => (
+//                 <>
+//                   <MapControls
+//                     customClass="c-map-controls"
+//                   >
+//                     <ZoomControl map={map} />
 
-                    <ShareControls />
+//                     <ShareControls />
 
-                    <BasemapControl
-                      basemap={BASEMAPS.dark}
-                      labels={LABELS.light}
-                      boundaries={false}
-                    />
+//                     <BasemapControl
+//                       basemap={BASEMAPS.dark}
+//                       labels={LABELS.light}
+//                       boundaries={false}
+//                     />
 
-                  </MapControls>
+//                   </MapControls>
 
-                  <LayerManager map={map} plugin={PluginLeaflet}>
-                    <>
-                      <Layer {...layer} />
-                      {geostore
-                      && (
-                      <Layer
-                        id={geostore.id}
-                        name="Geojson"
-                        provider="leaflet"
-                        layerConfig={{
-                          type: 'geoJSON',
-                          body: geostore.geojson,
-                        }}
-                            // Interaction
-                        interactivity
-                        events={{
-                          mouseover: (e) => {
-                            console.info(e);
-                          },
-                        }}
-                      />
-                      )}
-                    </>
-                  </LayerManager>
+//                   <LayerManager map={map} plugin={PluginLeaflet}>
+//                     <>
+//                       <Layer {...layer} />
+//                       {geostore
+//                       && (
+//                       <Layer
+//                         id={geostore.id}
+//                         name="Geojson"
+//                         provider="leaflet"
+//                         layerConfig={{
+//                           type: 'geoJSON',
+//                           body: geostore.geojson,
+//                         }}
+//                             // Interaction
+//                         interactivity
+//                         events={{
+//                           mouseover: (e) => {
+//                             console.info(e);
+//                           },
+//                         }}
+//                       />
+//                       )}
+//                     </>
+//                   </LayerManager>
 
-                </>
-              )}
-            </Map>
+//                 </>
+//               )}
+//             </Map>
 
-            <div className="c-legend-map">
-              <Legend
-                sortable={false}
-                maxHeight={300}
-                onChangeOrder={this.onChangeOrder}
-              >
-                {layerGroups.map((lg, i) => (
-                  <LegendListItem
-                    index={i}
-                    key={lg.dataset}
-                    layerGroup={lg}
-                    onChangeInfo={this.onChangeInfo}
-                    toolbar={(
-                      <LegendItemToolbar>
-                        <LegendItemButtonInfo />
-                      </LegendItemToolbar>
-                    )}
-                  >
-                    <LegendItemTypes />
-                  </LegendListItem>
-                ))}
-              </Legend>
-            </div>
-          </div>
-          )}
+//             <div className="c-legend-map">
+//               <Legend
+//                 sortable={false}
+//                 maxHeight={300}
+//                 onChangeOrder={this.onChangeOrder}
+//               >
+//                 {layerGroups.map((lg, i) => (
+//                   <LegendListItem
+//                     index={i}
+//                     key={lg.dataset}
+//                     layerGroup={lg}
+//                     onChangeInfo={this.onChangeInfo}
+//                     toolbar={(
+//                       <LegendItemToolbar>
+//                         <LegendItemButtonInfo />
+//                       </LegendItemToolbar>
+//                     )}
+//                   >
+//                     <LegendItemTypes />
+//                   </LegendListItem>
+//                 ))}
+//               </Legend>
+//             </div>
+//           </div>
+//           )}
 
-        {alertTable ? (
-          <DataTable
-            title="10 Most Recent Changes"
-            table={alertTable}
-          />
-        ) : <p>There are no alerts for this subscription yet.</p>}
+//         {alertTable ? (
+//           <DataTable
+//             title="10 Most Recent Changes"
+//             table={alertTable}
+//           />
+//         ) : <p>There are no alerts for this subscription yet.</p>}
 
-        {this.state.modalOpen
-          && (
-          <Modal
-            isOpen
-            onRequestClose={() => this.handleEditSubscription(false)}
-          >
-            {/* <AreaSubscriptionModal
-              area={this.state.area}
-              mode="edit"
-              onRequestClose={() => this.handleEditSubscription(false)}
-              subscriptionDataset
-              subscriptionType
-              subscriptionThreshold
-            /> */}
-          </Modal>
-          )}
+//         {this.state.modalOpen
+//           && (
+//           <Modal
+//             isOpen
+//             onRequestClose={() => this.handleEditSubscription(false)}
+//           >
+//             {/* <AreaSubscriptionModal
+//               area={this.state.area}
+//               mode="edit"
+//               onRequestClose={() => this.handleEditSubscription(false)}
+//               subscriptionDataset
+//               subscriptionType
+//               subscriptionThreshold
+//             /> */}
+//           </Modal>
+//           )}
 
-        {!!this.state.layer
-          && (
-          <Modal
-            isOpen={!!this.state.layer}
-            className="-medium"
-            onRequestClose={() => this.onChangeInfo(null)}
-          >
-            <LayerInfoModal
-              layer={this.state.layer}
-            />
-          </Modal>
-          )}
+//         {!!this.state.layer
+//           && (
+//           <Modal
+//             isOpen={!!this.state.layer}
+//             className="-medium"
+//             onRequestClose={() => this.onChangeInfo(null)}
+//           >
+//             <LayerInfoModal
+//               layer={this.state.layer}
+//             />
+//           </Modal>
+//           )}
 
-      </div>
-    );
-  }
-}
+//       </div>
+//     );
+//   }
+// }
 
-AlertWidget.propTypes = {
-  dataset: PropTypes.object,
-  subscriptionData: PropTypes.array,
-  subscription: PropTypes.object,
-  user: PropTypes.object,
-  id: PropTypes.string.isRequired,
-};
+// AlertWidget.propTypes = {
+//   dataset: PropTypes.object,
+//   subscriptionData: PropTypes.array,
+//   subscription: PropTypes.object,
+//   user: PropTypes.object,
+//   id: PropTypes.string.isRequired,
+// };
 
-const mapStateToProps = (state) => ({
-  user: state.user,
-  locale: state.common.locale,
-  alerts: areaAlerts(state),
-});
+// const mapStateToProps = (state) => ({
+//   user: state.user,
+//   locale: state.common.locale,
+//   alerts: areaAlerts(state),
+// });
 
-export default connect(mapStateToProps, null)(AlertWidget);
+// export default connect(mapStateToProps, null)(AlertWidget);
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,14 +1,8 @@
-import Document, {
-  Html,
-  Main,
-  NextScript,
-  Head,
-} from 'next/document';
+import Document, { Html, Main, NextScript, Head } from 'next/document';
+import Script from 'next/script';
 
 // lib
-import {
-  mediaStyles,
-} from 'lib/media';
+import { mediaStyles } from 'lib/media';
 
 export default class MyDocument extends Document {
   render() {
@@ -20,7 +14,10 @@ export default class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+            rel="stylesheet"
+          />
           <link rel="stylesheet" media="screen" href="/static/styles/add-search-results.css" />
 
           {/* Mobile address background */}
@@ -38,37 +35,11 @@ export default class MyDocument extends Document {
           <meta name="twitter:site" content="@resource_watch" />
           <meta property="fb:app_id" content="Resource Watch" />
 
-          {/* Leaflet CDN */}
-          {/* leaflet script necessary for the Widget Editor */}
-          <script
-            src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-            integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-            crossOrigin=""
-          />
-          <script
-            src="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.2/leaflet.draw.js"
-            crossOrigin=""
-          />
-          <script
-            src="https://unpkg.com/esri-leaflet@2.1.3/dist/esri-leaflet.js"
-            integrity="sha512-pijLQd2FbV/7+Jwa86Mk3ACxnasfIMzJRrIlVQsuPKPCfUBCDMDUoLiBQRg7dAQY6D1rkmCcR8286hVTn/wlIg=="
-            crossOrigin=""
-          />
-          <script
-            src="https://unpkg.com/leaflet-utfgrid/L.UTFGrid-min.js"
-            crossOrigin=""
-          />
-
           {/* Google API */}
-          <script
-            src={`https://maps.googleapis.com/maps/api/js?v=weekly&key=${
-              process.env.NEXT_PUBLIC_RW_GOGGLE_API_TOKEN_SHORTENER
-            }&libraries=places`}
+          <Script
+            src={`https://maps.googleapis.com/maps/api/js?v=weekly&key=${process.env.NEXT_PUBLIC_RW_GOGGLE_API_TOKEN_SHORTENER}&libraries=places`}
           />
 
-          {/* Polifyll */}
-          {/* TO-DO: remove once axios is completely implemented */}
-          <script src="https://cdn.polyfill.io/v2/polyfill.min.js" />
           <style
             type="text/css"
             // eslint-disable-next-line react/no-danger
