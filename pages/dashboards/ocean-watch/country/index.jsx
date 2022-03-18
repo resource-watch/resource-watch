5873138f81f8c1342a49760231b6e13c81a7c16f
Andrés González <andres.gonzalez@vizzuality.com>
2022-03-18 10:53:53 +0100

    ocean watch: displays content despite of warning

--- pages/dashboards/ocean-watch/country/index.jsx
+++ pages/dashboards/ocean-watch/country/index.jsx
@@ -1,6 +1,7 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Select from 'react-select';
+import { useMediaMatch } from 'rooks';
 
 // hooks
 import { useOceanWatchAreas } from 'hooks/ocean-watch';
@@ -12,12 +13,10 @@ import OceanWatchHero from 'layout/layout/ocean-watch/hero';
 import OceanWatchPartners from 'layout/layout/ocean-watch/partners';
 import Modal from 'components/modal/modal-component';
 
-// lib
-import { Media } from 'lib/media';
-
 export default function OceanWatchCountryProfiles() {
   const router = useRouter();
   const [countryValue, setCountryValue] = useState();
+  const [isModalVisible, setModalVisibility] = useState(false);
 
   const { data: areas } = useOceanWatchAreas({
     select: (_areas) =>
@@ -43,86 +42,79 @@ export default function OceanWatchCountryProfiles() {
   );
 
   const onCloseModal = useCallback(() => {
-    router.push('/dashboards/ocean-watch');
-  }, [router]);
+    setModalVisibility((modalVisibility) => !modalVisibility);
+  }, []);
+
+  const shouldWarningModalRender = useMediaMatch('(max-width: 768px)');
+
+  useEffect(() => {
+    if (shouldWarningModalRender) setModalVisibility(shouldWarningModalRender);
+  }, [shouldWarningModalRender]);
 
   return (
     <LayoutOceanWatch
       title="Ocean Watch – Country Profiles Index"
       description="Ocean Watch description" // todo: replace description
     >
-      <Media lessThan="lg">
-        {(className, renderChildren) =>
-          renderChildren ? (
-            <>
-              <Header />
-              <OceanWatchHero className="-ocean-watch" />
-              <Modal isOpen onRequestClose={onCloseModal}>
-                <p>
-                  The mobile version has limited functionality, please check the desktop version to
-                  have access to the full list of features available on the Ocean Watch dashboard.
-                </p>
-              </Modal>
-            </>
-          ) : (
-            <>
-              <Header className="-transparent" />
-              <OceanWatchHero className="-ocean-watch" />
-              <section className="l-section -secondary -small">
-                <div className="l-container">
-                  <div className="row">
-                    <div className="column small-12">
-                      <Select
-                        instanceId="area-selector"
-                        options={areas}
-                        className="-large"
-                        onChange={handleAreaChange}
-                        clearable={false}
-                        value={countryValue}
-                        placeholder="Select a coastline"
-                      />
-                      <div
-                        style={{
-                          display: 'flex',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          minHeight: 550,
-                          width: '100%',
-                          margin: '25px 0 0',
-                          background: 'url(/static/images/ocean-watch/placeholder-map.png)',
-                          backgroundSize: 'contain',
-                          backgroundRepeat: 'no-repeat',
-                        }}
-                      >
-                        <h3
-                          style={{
-                            color: '#393f44',
-                            fontSize: 26,
-                            fontWeight: 300,
-                            textAlign: 'center',
-                          }}
-                        >
-                          Select a coastline country to further explore <br />
-                          land-based pressures upon the ocean.
-                        </h3>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </section>
-              <section className="l-section -medium">
-                <div className="l-container">
-                  <div className="row">
-                    <div className="column small-12">
-                      <OceanWatchPartners />
-                    </div>
-                  </div>
-                </div>
-              </section>
-            </>
-          )
-        }
-      </Media>
+      <Header className="-transparent" />
+      <OceanWatchHero className="-ocean-watch" />
+      <section className="l-section -secondary -small">
+        <div className="l-container">
+          <div className="row">
+            <div className="column small-12">
+              <Select
+                instanceId="area-selector"
+                options={areas}
+                className="-large"
+                onChange={handleAreaChange}
+                clearable={false}
+                value={countryValue}
+                placeholder="Select a coastline"
+              />
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  minHeight: 550,
+                  width: '100%',
+                  margin: '25px 0 0',
+                  background: 'url(/static/images/ocean-watch/placeholder-map.png)',
+                  backgroundSize: 'contain',
+                  backgroundRepeat: 'no-repeat',
+                }}
+              >
+                <h3
+                  style={{
+                    color: '#393f44',
+                    fontSize: 26,
+                    fontWeight: 300,
+                    textAlign: 'center',
+                  }}
+                >
+                  Select a coastline country to further explore <br />
+                  land-based pressures upon the ocean.
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="l-section -medium">
+        <div className="l-container">
+          <div className="row">
+            <div className="column small-12">
+              <OceanWatchPartners />
+            </div>
+          </div>
+        </div>
+      </section>
+      <Modal isOpen={isModalVisible} onRequestClose={onCloseModal}>
+        <p>
+          The mobile version has limited functionality, please check the desktop version to have
+          access to the full list of features available on the Ocean Watch dashboard.
+        </p>
+      </Modal>
     </LayoutOceanWatch>
   );
 }
