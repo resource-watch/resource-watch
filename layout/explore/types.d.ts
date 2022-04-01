eafbe746b747616c8ac0c6cb4f8b88e9f9cdfa6a
Andrés González <andres.gonzalez@vizzuality.com>
2022-04-01 12:41:32 +0200

    adds canonical URL for explore pages

--- layout/explore/types.d.ts
+++
@@ -1,6 +0,0 @@
-import type { ExplorePageProps } from 'pages/data/explore/[[...dataset]]';
-
-export interface ExploreLayoutProps extends ExplorePageProps {
-  stopDrawing: () => void;
-  userIsLoggedIn: boolean;
-}
