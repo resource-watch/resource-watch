08c7325b568ece64cbc723efef63b7f1322e5303
Andrés González <andres.gonzalez@vizzuality.com>
2022-06-23 11:48:56 +0200

    encodes NRT routes

--- layout/footer/footer-links/constants.js
+++ layout/footer/footer-links/constants.js
@@ -10,7 +10,7 @@ export const FOOTER_LINKS = [
       },
       {
         label: 'Near Real-Time Data',
-        href: '/data/explore?section=Near+Real-Time',
+        href: '/data/explore?section=Near%20Real-Time',
       },
       {
         label: 'App Gallery',
